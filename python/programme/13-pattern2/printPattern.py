106ad4ab526e6ffb104e83beffe001ade6f1b1ec
Yacine Zitouni <yac.zitouni1962@gmail.com>
2022-02-23 17:04:37 +0100

    add: python/programme/13-pattern2
    Former-commit-id: cdd00c44602a341b7b78e6438fb4be675c51ef33

---
+++ python/programme/13-pattern2/printPattern.py
@@ -0,0 +1,10 @@
+
+
+def printPattern(n): 
+    for i in range(1,n+1):
+        print((i*"* ").rjust(n+i))
+    for i in reversed(range(1,n)):
+        print((i*"* ").rjust(n+i))
+        
+
+printPattern(3)
