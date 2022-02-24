27c8268e289ea883045afbf6cb859ca2b3f5d818
theanorak <13906654+theanorak@users.noreply.github.com>
2022-02-24 18:39:53 +0800

    Update solution.py
    Former-commit-id: 7491460a390ceb281968d2bd3e8cf3a55c0558dc

--- programme/area-of-circle/solution.py
+++ programme/area-of-circle/solution.py
@@ -1,6 +1,6 @@
 #Write a Python programme to find the area of circle.
 import math
-# Vars
+#Vars
 r = int(input("Enter circle radius: ")) #radius input from user
 AOC = round((math.pi*(r**2)),2) #area of circle
 areaofc = "The Area of the circle is {}."   
