99126d0ad613b73f31dd565f1617582986f10f2d
theanorak <13906654+theanorak@users.noreply.github.com>
2022-02-25 16:39:01 +0800

    Delete old folder for reupload
    new folder with right name created already

--- programme/Compound Interest Calculator/solution.py
+++
@@ -1,7 +0,0 @@
-# Input from User
-p = int(input("Enter Principle Amount: "))
-t = int(input("Enter Time Period: "))
-r = int(input("Enter Rate of Interest: "))
-
-# output
-print ('Compound Interest: ', (round((p*((1+(r/100))**t)) - p,2)))
