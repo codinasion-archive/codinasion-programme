37c707c4d37867f6d25de303f33404dc7f65ea19
Kylian Meulin <59285425+KikiManjaro@users.noreply.github.com>
2022-02-25 13:50:10 +0100

    ASCII Value #240
    Former-commit-id: f461d9451f0933bc88e16a648033d1c80f32c9e9

---
+++ programme/ascii-value/solution.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class ASCIIValue {
+    public static void main(String args[]) {
+        // Input character
+        System.out.print("Enter character: ");
+        Scanner sc = new Scanner(System.in);
+        char chr = sc.next().charAt(0);
+
+        // output ASCII value
+        int asciiValue = chr;
+        System.out.println("ASCII value: " + asciiValue);
+    }
+}  
