e31d9f64efe23bd2c107c9d559a9e857377c0b1b
Neel Patel <98449915+NeelPatel31@users.noreply.github.com>
2022-02-25 16:22:56 +0530

    Added sum of cube in java
    Former-commit-id: 31063305f9f24ffa9302e7671cf7a942cf399a4d

---
+++ programme/Sum-of-cube/SumOfCube.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class SumOfCube{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Input: ");
+        int n = sc.nextInt();
+        int total = 0;
+        for(int i=1;i<=n;i++){
+            total += (i*i*i);
+        }
+        System.out.println("Output: "+total);
+        sc.close();
+    }
+}
