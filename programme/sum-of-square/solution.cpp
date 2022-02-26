ff293d6e989f6190d4a919ed2f088272e4c2eaf6
harshraj8843 <harshraj8843pro@gmail.com>
2022-02-26 23:51:52 +0530

    update #267
    Former-commit-id: 9024459270559d56b88acd22fc42a4a71dd20bf4

---
+++ programme/sum-of-square/solution.cpp
@@ -0,0 +1,16 @@
+// Write a program to print sum of square
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, total = 0;
+    cout << "Input : ";
+    cin >> a;
+    for (int i = 1; i <= a; i++)
+    {
+        total += (i * i);
+    }
+    cout << "Output : " << total << endl;
+    return 0;
+}
