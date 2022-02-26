aa6d468500007684fc00c0a1a5f28a69ed2f1ee8
harshraj8843 <harshraj8843pro@gmail.com>
2022-02-26 23:55:16 +0530

    update programme/sum-of-square/solution.java

--- programme/sum-of-square/solution.java
+++ programme/sum-of-square/solution.java
@@ -1,22 +1,21 @@
+
 // Write a program to print sum of square.
-import java.io.*;
 import java.util.*;
 
 class SumOfSquare {
 
-    public static int squaresum(int n)
-    {
+    public static int squaresum(int n) {
         return (n * (n + 1) / 2) * (2 * n + 1) / 3;
     }
- 
-    public static void main(String[] args)
-    {
-        // Input character
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Input: ");
-        int n = sc.nextInt();
 
-        // output ASCII value
-        System.out.println(squaresum(n));
+    public static void main(String[] args) {
+        try (// Input character
+                Scanner sc = new Scanner(System.in)) {
+            System.out.print("Input: ");
+            int n = sc.nextInt();
+
+            // output ASCII value
+            System.out.println(squaresum(n));
+        }
     }
 }
