61c365a7abdbd6b92e7344898c1eb7e79950903b
Anish Vedantham <f20171228@hyderabad.bits-pilani.ac.in>
2022-02-26 12:35:41 +0530

    fix #234 : create solution.java for area of circle program

---
+++ programme/area-of-circle/solution.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class AreaOfCircle {
+
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the radius");
+		
+		//taking radius input
+		int r = sc.nextInt();
+		
+		//calculating area of circle
+		double area = Math.PI*r*r;
+		
+		//rounding the answer to 2 decimal places
+		double answer = (double)Math.round(area*100)/100;
+		
+		//printing the final answer
+		System.out.println("Area : " + answer);
+		
+		sc.close();
+
+	}
+
+}
