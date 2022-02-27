6cc435f0fd4b97e30e2678e14221258a064579bb
KikiManjaro <kylian.meulin@gmail.com>
2022-02-27 15:34:06 +0100

    Add fractorial for java #186

--- programme/factorial/solution.java
+++ programme/factorial/solution.java
@@ -1,31 +1,5 @@
-## Write a program to get the factorial of a number.
+// Write a program to get the factorial of a number
 
-```
-Input:  5
-Output: 120
-```
-
----
-
-<CodeBlock slots="heading, code" repeat="2" languages="Python, Java" />
-
-#### Python
-
-```python
-# recursive factorial function
-def factorial(n):
-    if n == 1 or n == 0:
-        return 1
-    else:
-        return n*factorial(n-1)
-
-n = int(input("Input:"))
-print("Output: ", factorial(n))
-```
-
-#### Java
-
-```java
 import java.util.Scanner;
 
 class Factorial {
@@ -42,5 +16,4 @@ class Factorial {
         }
         System.out.println("Output: " + fact);
     }
-}  
-```
+}
