36be9da30cf4cb266ad39e87f9477c6201b387b2
Neel Patel <98449915+NeelPatel31@users.noreply.github.com>
2022-03-01 16:18:10 +0530

    Add : Fibonacci series in C
    Former-commit-id: ec461c2f3cd5f731a81f9ee3198485a5fc8bfd7c

---
+++ programme/Fibonacci-nth-C/fibonacci-nth-c.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+    int a=0,b=1,temp=0,n;
+    printf("Input: ");
+    scanf("%d",&n);
+    int i=0;
+    while(i!=n){
+        printf("%d ",a);
+        temp = a;
+        a = b;
+        b = temp + b;
+        i++;
+    }
+    return 0;
+}
