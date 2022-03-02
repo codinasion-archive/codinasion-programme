d09599d89bb02406970811a4a68341511f419d8e
karthik <karthiksbh@gmail.com>
2022-03-02 08:58:45 +0530

    Contributor: Karthik

--- programme/check-prime-number/solution.java
+++ programme/check-prime-number/solution.java
@@ -4,11 +4,11 @@ public class solution {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("\nEnter the number: ");
+        System.out.print("\nInput: ");
         int n = sc.nextInt();
 
         if (n == 0 || n == 1) {
-            System.out.println(n + " is not a prime number");
+            System.out.println("Output: Not prime number");
         } else {
             int temp = 0;
             for (int i = 2; i < n / 2; i++) {
@@ -19,9 +19,9 @@ public class solution {
             }
 
             if (temp == 1) {
-                System.out.println(n + " is not a prime number");
+                System.out.println("Output: Not prime number");
             } else {
-                System.out.println(n + " is a prime number");
+                System.out.println("Output: Prime number");
             }
         }
         sc.close();
