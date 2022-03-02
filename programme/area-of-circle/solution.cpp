bf4bae9d12b6efa9c66c1408166c32fffa7f3727
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-03-02 20:45:50 +0530

    Update solution.cpp

--- programme/area-of-circle/solution.cpp
+++ programme/area-of-circle/solution.cpp
@@ -1,3 +1,4 @@
+// Write a programme to find the area of circle
 #include <bits/stdc++.h>
 using namespace std;
 #define pi 3.141
@@ -5,8 +6,11 @@ using namespace std;
 int main()
 {
     int r;
+    
+    // input radius
     cout << "Enter radius: ";
     cin >> r;
 
-    printf("Area: %.2f", pi * r * r);
+    // output area
+    cout << "Area: " << floor((pi * r * r) * 100) / 100;
 }
