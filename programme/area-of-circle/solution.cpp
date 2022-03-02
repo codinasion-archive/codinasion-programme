b84b23138a909b3ab0be1ba27337db82a8e894dc
karthik <karthiksbh@gmail.com>
2022-03-02 10:23:44 +0530

    Contributed by Karthik

---
+++ programme/area-of-circle/solution.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define pi 3.141
+
+int main()
+{
+    int r;
+    cout << "Enter radius: ";
+    cin >> r;
+
+    printf("Area: %.2f", pi * r * r);
+}
