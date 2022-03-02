64e5a6933b40a67ddc95ccf24409e37532270fb6
harshraj8843 <harshraj8843pro@gmail.com>
2022-03-02 21:25:10 +0530

    update #286

---
+++ programme/fibonaci-series-without-using-recursion/solution.py
@@ -0,0 +1,13 @@
+# Write a program to print fibonacci series without using recursion
+a = 0
+b = 1
+i = 0
+n = int(input("Input  : "))
+print("Output : ", end="")
+while (i != n):
+    print(a, end=" ")
+    temp = a
+    a = b
+    b = temp + b
+    i += 1
+print()
