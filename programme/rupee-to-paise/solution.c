b35056d28a824d1757f7f9a420d20411d0b650a5
Luís <a100549@alunos.uminho.pt>
2022-03-03 01:05:29 +0000

    Create solution.c

---
+++ programme/rupee-to-paise/solution.c
@@ -0,0 +1,9 @@
+// Write a program to convert rupee to paise
+#include <stdio.h>
+
+int main () {
+    float x;
+    scanf ("%g",&x); //reads input in rupee
+    printf ("%g\n",x*100); //prints result in paise
+    return 0;
+}
