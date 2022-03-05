d15d80318e200d86bfe4f186076bdee2c0e5c5d6
Neel Patel <98449915+NeelPatel31@users.noreply.github.com>
2022-03-05 19:35:37 +0530

    Added CPP code for prime number
    Former-commit-id: 5c1e173d2f5a697088a071dd0ea7cba955ff6eee

---
+++ programme/check-prime-number/solution.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b=0;
+    cout<<"Input: ";
+    cin>>a;
+    cout<<"Output: ";
+    if(a==0 || a==1){
+        cout<<"Not prime number\n";
+    }
+    else{
+        for (int i = 2; i < a; i++)
+        {
+            if (a % i == 0)
+            {
+                b = 1;
+                break;
+            }
+        }
+        if (b == 1)
+        {
+            cout<<"Not prime number\n";
+        }
+        else
+        {
+            cout<<"Prime number\n";
+        }
+    }
+    return 0;
+}
