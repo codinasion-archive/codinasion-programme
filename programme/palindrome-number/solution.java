5144c044b65b08240d26136dcaee450d7dd7310b
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-03-05 06:08:10 +0530

    Create solution.java

---
+++ programme/palindrome-number/solution.java
@@ -0,0 +1,26 @@
+// Write a program to check palindrome number
+import java.util.*;
+
+public class solution {
+    public static String checkPalindrome(int a) {
+        int pal = 0, n;
+        int original = a;
+        while (a > 0) {
+            n = a % 10;
+            pal = pal * 10 + n;
+            a = a / 10;
+        }
+        if (original == pal) {
+            return "Palindrome number";
+        }
+        return "Not palindrome number";
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Input  : ");
+        int num = sc.nextInt();
+        System.out.println("Output : " + checkPalindrome(num));
+        sc.close();
+    }
+}
