042e4ba9d680627d5ca053db6a6a0ee513f0f3b7
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-03-06 23:04:44 +0530

    Update solution.cpp

--- programme/check-prime-number/solution.cpp
+++ programme/check-prime-number/solution.cpp
@@ -1,14 +1,18 @@
-#include<iostream>
+// Write a program to check prime number
+#include <iostream>
 using namespace std;
-int main(){
-    int a,b=0;
-    cout<<"Input: ";
-    cin>>a;
-    cout<<"Output: ";
-    if(a==0 || a==1){
-        cout<<"Not prime number\n";
+int main()
+{
+    int a, b = 0;
+    cout << "Input  : ";
+    cin >> a;
+    cout << "Output : ";
+    if (a == 0 || a == 1)
+    {
+        cout << "Not prime number\n";
     }
-    else{
+    else
+    {
         for (int i = 2; i < a; i++)
         {
             if (a % i == 0)
@@ -19,11 +23,11 @@ int main(){
         }
         if (b == 1)
         {
-            cout<<"Not prime number\n";
+            cout << "Not prime number\n";
         }
         else
         {
-            cout<<"Prime number\n";
+            cout << "Prime number\n";
         }
     }
     return 0;
