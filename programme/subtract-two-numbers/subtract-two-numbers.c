07051dbf56060236e76e8cd772bfe8fa58290582
DarkMatter999-dev <darkmatter999official@gmail.com>
2022-03-07 19:21:09 +0530

    Program to subtract two numbers in Python, C, and C++

---
+++ programme/subtract-two-numbers/subtract-two-numbers.c
@@ -0,0 +1,14 @@
+// Programme to subtract two numbers
+#include<stdio.h>
+
+int main() {
+    
+    int x, y;
+
+    printf("Enter two numbers ");
+    scanf("%d %d", &x, &y);
+
+    printf("Difference of numbers : %d\n", (x-y));
+
+    return 0;
+}
---
+++ programme/subtract-two-numbers/subtract-two-numbers.py
@@ -0,0 +1,3 @@
+# Programme to subtract two numbers
+x, y = input("Enter two numbers: ").split()
+print("Difference of numbers : ", int(x)-int(y))
