ffff34cca44505eecc9354ea8468bf80decb8947
DarkMatter999-dev <darkmatter999official@gmail.com>
2022-03-07 19:21:09 +0530

    Program to subtract two numbers in Python, C, and C++
    Former-commit-id: 07051dbf56060236e76e8cd772bfe8fa58290582

---
+++ programme/subtract-two-numbers/subtract-two-numbers.cpp
@@ -0,0 +1,15 @@
+// Programme to subtract two numbers
+#include<iostream>
+
+int main() {
+    
+    int x, y;
+
+    std::cout << "Enter two numbers ";
+    std::cin >> x;
+    std::cin >> y;
+
+    std::cout << "Difference of numbers : " << (x-y) << std::endl;
+    
+    return 0;
+}
