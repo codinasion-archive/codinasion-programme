4239de538fe750037a6b3c29f7cabbe9253b0294
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-03-08 09:39:35 +0530

    Update rs-to-p.cpp

--- programme/rupee-to-paise/rs-to-p.cpp
+++ programme/rupee-to-paise/rs-to-p.cpp
@@ -1,13 +1,14 @@
 // Write a program to convert rupee to paise
 #include <iostream>
 using namespace std;
-int main() {
-	double paise, rupees;
-	cout<< "Please enter desired rupee amount for paise conversion: ";
-	cin >> rupees;
-	paise = rupees * 100; // paise calculation
+int main()
+{
+    double paise, rupees;
+    cout << "Please enter desired rupee amount for paise conversion : ";
+    cin >> rupees;
+    paise = rupees * 100; // paise calculation
 
-	cout << "\nTotal Rupees = " << rupees << " rupees" << endl;
-	cout << "\nTotal Paise = " << paise << " paise" << endl;
+    cout << "\nTotal Rupees : " << rupees << " rupees" << endl;
+    cout << "Total Paise  : " << paise << " paise" << endl;
     return 0;
 }
