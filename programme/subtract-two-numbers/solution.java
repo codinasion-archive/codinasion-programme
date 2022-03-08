0be92912084daec9cded53cc5e8088a3667e7da3
Neel Patel <98449915+NeelPatel31@users.noreply.github.com>
2022-03-08 17:56:17 +0530

    Added java code for subtracting 2 num
    Former-commit-id: 66fc5c3772340f4fe15ea4cf26c355889b50cc58

---
+++ programme/subtract-two-numbers/solution.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class solution{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input: ");
+        // Input 2 numbers
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        // Print difference
+        System.out.println("Output: "+(a-b));
+        sc.close();
+    }
+}
