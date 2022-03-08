c5635ea6e9665c18bd5060db6e22fa859beed189
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-03-08 09:29:49 +0530

    Update subtract-two-numbers.py
    Former-commit-id: b94eb87aa185014baf533adc3dfd1c3edb2e66c7

--- programme/subtract-two-numbers/subtract-two-numbers.py
+++ programme/subtract-two-numbers/subtract-two-numbers.py
@@ -1,3 +1,7 @@
-# Programme to subtract two numbers
+# Write a programme to subtract two numbers
+
+# input two numbers
 x, y = input("Enter two numbers: ").split()
+
+# print difference
 print("Difference of numbers : ", int(x)-int(y))
