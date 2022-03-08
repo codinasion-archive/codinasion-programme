18d4c770adbc29de1e77bfc730566e7948531d61
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-03-08 21:00:08 +0530

    Update and rename solution.java to subtract-two-numbers.java

--- programme/subtract-two-numbers/subtract-two-numbers.java
+++ programme/subtract-two-numbers/subtract-two-numbers.java
@@ -1,13 +1,15 @@
+// Write a programme to subtract two numbers
 import java.util.*;
-public class solution{
-    public static void main(String[] args){
+
+public class solution {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Input: ");
+        System.out.print("Input  : ");
         // Input 2 numbers
         int a = sc.nextInt();
         int b = sc.nextInt();
         // Print difference
-        System.out.println("Output: "+(a-b));
+        System.out.println("Output : " + (a - b));
         sc.close();
     }
 }
