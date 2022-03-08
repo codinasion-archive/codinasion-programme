4f95ce528e2a3944778ef8a143a1136b6a9dad76
Neel Patel <98449915+NeelPatel31@users.noreply.github.com>
2022-03-08 20:31:20 +0530

    Added C,C++,Java code for pattern 2
    Former-commit-id: 3af3534dbae6a5520757d3575b60f853f36c893f

---
+++ programme/pattern-2/solution.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+void pattern(int n){
+    for(int i = 0 ; i<n ; i++){
+        for(int j = n-1 ; j>i ; j--){
+            cout<<" ";
+        }
+        for(int k = 0 ; k<=i ; k++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+    for(int i = 1 ; i<n ; i++){
+        for(int k = 1 ; k<=i ; k++){
+            cout<<" ";
+        }
+        for(int j = n-1 ; j>i-1 ; j--){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+}
+
+int main(){
+    int a;
+    cout<<"Input: ";
+    cin>>a;
+    pattern(a);
+    return 0;
+}
