3af3534dbae6a5520757d3575b60f853f36c893f
Neel Patel <98449915+NeelPatel31@users.noreply.github.com>
2022-03-08 20:31:20 +0530

    Added C,C++,Java code for pattern 2

---
+++ programme/pattern-2/solution.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+void pattern(int n){
+    for(int i = 0; i<n; i++){
+        for(int j = n-1 ; j>i ; j--){
+            printf(" ");
+        }
+        for(int k = 0; k<=i ; k++){
+            printf("* ");
+        }
+        printf("\n");
+    }
+    for(int i = 1; i<n;i++){
+        for(int l = 1; l<=i ;l++){
+            printf(" ");
+        }
+        for(int m = n; m>i ; m--){
+            printf("* ");
+        }
+        printf("\n");
+    }
+}
+
+int main(){
+    int n;
+    printf("Enter num: ");
+    scanf("%d",&n);
+    pattern(n);
+    return 0;
+}
