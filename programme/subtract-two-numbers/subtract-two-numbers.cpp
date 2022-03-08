d914cfe8ceafd800ad4ef33a6a9911f73b367bf0
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-03-08 09:29:41 +0530

    Update subtract-two-numbers.cpp
    Former-commit-id: 82a6a11624c30626b7aeb95a3fd0f3968066ae74

--- programme/subtract-two-numbers/subtract-two-numbers.cpp
+++ programme/subtract-two-numbers/subtract-two-numbers.cpp
@@ -1,15 +1,18 @@
-// Programme to subtract two numbers
-#include<iostream>
+// Write a programme to subtract two numbers
+#include <iostream>
 
-int main() {
-    
+int main()
+{
+    // declare variables
     int x, y;
 
+    // get input
     std::cout << "Enter two numbers ";
     std::cin >> x;
     std::cin >> y;
 
-    std::cout << "Difference of numbers : " << (x-y) << std::endl;
-    
+    // print difference
+    std::cout << "Difference of numbers : " << (x - y) << std::endl;
+
     return 0;
 }
