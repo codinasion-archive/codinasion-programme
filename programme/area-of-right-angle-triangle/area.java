33399faa347ab6833255c4428cceb2297bea48a3
Omar Mabrouk <67244800+SpadeQ22@users.noreply.github.com>
2022-03-09 23:24:27 +0200

    created area-of-right-triangle folder, area.java, README.md, index.md
    Former-commit-id: a553150810f2d1e2fffb624c985e79305d38f685

---
+++ programme/area-of-right-angle-triangle/area.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class area{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //input base
+        System.out.print("base : ");
+        double base = sc.nextDouble();
+        //input height
+        System.out.print("height : ");
+        double height = sc.nextDouble();
+        //calculate height
+        double area = 0.5 * base * height;
+        //print output
+        System.out.print("area : " + area);
+    }
+}
