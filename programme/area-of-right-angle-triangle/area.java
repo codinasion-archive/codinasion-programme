9886b05c38460fa67b6946683413794505b86a96
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-03-10 20:09:02 +0530

    Update area.java
    Former-commit-id: 44b57d1105e0f7c2ae4b10d287dd2d059f639656

--- programme/area-of-right-angle-triangle/area.java
+++ programme/area-of-right-angle-triangle/area.java
@@ -1,17 +1,18 @@
+// Write a programme to find area of right angle triangle
 import java.util.*;
 
-class area{
+class area {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        //input base
+        // input base
         System.out.print("base : ");
         double base = sc.nextDouble();
-        //input height
+        // input height
         System.out.print("height : ");
         double height = sc.nextDouble();
-        //calculate height
+        // calculate height
         double area = 0.5 * base * height;
-        //print output
-        System.out.print("area : " + area);
+        // print output
+        System.out.println("\narea : " + area);
     }
 }
