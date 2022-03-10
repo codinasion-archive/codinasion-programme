b322c40893a9e87757ade4e60a599bed3aed488c
Rafael Fadel <rafaelfadel2@gmail.com>
2022-03-10 17:36:49 -0300

    commited the deleted file

--- programme/area-of-right-angle-triangle/area-of-the-right-triangle.py
+++
@@ -1,3 +0,0 @@
-base = input('Enter the number of the base of the triangle: ')
-height = input('Enter the number of the height of the triangle: ')
-print('The area of the right angle triangle is: ', int(base) * int(height) / 2)
