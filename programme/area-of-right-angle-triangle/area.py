d6c87a2040463c9de9aba2fc284fe4de10492e51
Rafael Fadel <rafaelfadel2@gmail.com>
2022-03-10 17:35:28 -0300

    changing the name of the file
    Former-commit-id: 85c46b7171bdf452877f5e2b2015724121210e38

---
+++ programme/area-of-right-angle-triangle/area.py
@@ -0,0 +1,3 @@
+base = input('Enter the number of the base of the triangle: ')
+height = input('Enter the number of the height of the triangle: ')
+print('The area of the right angle triangle is: ', int(base) * int(height) / 2)
