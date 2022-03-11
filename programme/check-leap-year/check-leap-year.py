51ea430542c44f12c8addb6ef1970abd9f00bf03
Saikat Das <saikat.g347@gmail.com>
2022-03-11 18:49:56 +0530

    Leap year checking program added #333

---
+++ programme/check-leap-year/check-leap-year.py
@@ -0,0 +1,14 @@
+year = int(input("Input : "))
+
+print("Output : ", end="")
+
+if year % 100 == 0:
+    if year % 400 == 0:
+        print("Leap Year")
+    else:
+        print("Not a Leap Year")
+else:    
+    if year % 4 == 0:
+        print("Leap Year")
+    else:
+        print("Not a Leap Year")
