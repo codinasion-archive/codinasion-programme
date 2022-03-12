81f9354df2c5c67e4da0ebeaed1a466486c0da5d
Neel Patel <98449915+NeelPatel31@users.noreply.github.com>
2022-03-12 10:41:26 +0530

    Added cpp and java code for number-to-string

---
+++ programme/number-to-string/numbers_to_string.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class numbers_to_string{
+    public static void main(String[] args){
+        String[] word = {"Zero","One","Two","Three","Four","Five","Six","Seven","Eight","Nine"};
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Input: ");
+        String a = sc.nextLine();
+        System.out.print("Output: ");
+        for(int i = 0 ; i<a.length() ; i++){
+            System.out.print(word[(int)a.charAt(i)-48]+" ");
+        }
+        sc.close();
+    }
+}
