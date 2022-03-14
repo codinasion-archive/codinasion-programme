ef288c2db1468f153f33c5072da88d4ab5a59b2c
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-03-14 11:11:02 +0530

    Update area-of-right-angle-triangle.c

--- programme/area-of-right-angle-triangle/area-of-right-angle-triangle.c
+++ programme/area-of-right-angle-triangle/area-of-right-angle-triangle.c
@@ -3,32 +3,35 @@
  */
 
 #include <stdio.h>
-#include <assert.h>         // for the assert function
+#include <assert.h> // for the assert function
 
 double area_of_right_angle_triangle(double base, double height);
 void test(void);
 
-int main(void) {
+int main(void)
+{
     double area, base, height;
 
     test();
 
-    printf("base: ");
+    printf("base : ");
     scanf("%lf", &base);
-    printf("height: ");
+    printf("height : ");
     scanf("%lf", &height);
     area = area_of_right_angle_triangle(base, height);
-    printf("area: %.1lf\n", area);
+    printf("\narea : %.1lf\n", area);
 
     return 0;
 }
 
 // calculates the area
-double area_of_right_angle_triangle(double base, double height) {
+double area_of_right_angle_triangle(double base, double height)
+{
     return 0.5 * base * height;
 }
 
 // checks correctness of the area calculation
-void test(void) {
+void test(void)
+{
     assert(area_of_right_angle_triangle(2, 3) == 3);
 }
