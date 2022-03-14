9027c2e435ab94db0d233a12fd95f09ba453444b
RuiL1904 <rui.lilokas2003@hotmail.com>
2022-03-14 13:42:48 +0000

    Add ascii-value.c
    Former-commit-id: 7e6db2b128ebd4de643bd1311da7e29c8e077ee0

---
+++ programme/ascii-value/ascii-value.c
@@ -0,0 +1,10 @@
+// Write a programme to print ASCII Value of a character.
+#include <stdio.h>
+
+int main() {
+    char a;
+    printf("Enter the character: ");
+    scanf("%c", &a);
+    printf("ASCII value is: %d\n", (int)(a));
+    return 0;
+}
