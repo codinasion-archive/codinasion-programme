6d0fbb2979044aa37b6d5b51ed51757f91bdb6c4
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-03-15 20:07:18 +0530

    Update check-leap-year.c
    Former-commit-id: ecb9efe368418174b2ed3ec5756c12e26eed3d0e

--- programme/check-leap-year/check-leap-year.c
+++ programme/check-leap-year/check-leap-year.c
@@ -1,26 +1,36 @@
 // Write a programme to check leap year
 #include <stdio.h>
 
-int main() {
+int main()
+{
     int year;
-    
-    printf("Input: ");
+
+    // input year
+    printf("Input  : ");
     scanf("%d", &year);
 
-    if (year % 100 == 0) {
-        if (year % 400 == 0) {
-            printf("Leap year.\n");
+    // output result
+    printf("Output : ");
+    if (year % 100 == 0)
+    {
+        if (year % 400 == 0)
+        {
+            printf("Leap year\n");
         }
-        else {
-            printf("Not a leap year.\n");
+        else
+        {
+            printf("Not a leap year\n");
         }
     }
-    else {
-        if (year % 4 == 0) {
-            printf("Leap year.\n");
+    else
+    {
+        if (year % 4 == 0)
+        {
+            printf("Leap year\n");
         }
-        else {
-            printf("Not a leap year.\n");
+        else
+        {
+            printf("Not a leap year\n");
         }
     }
 }
