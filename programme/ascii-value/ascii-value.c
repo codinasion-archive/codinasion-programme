0c1e404673aa083e8c78eac8c8ac32de919a8b15
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-03-15 20:01:50 +0530

    Update ascii-value.c
    Former-commit-id: 73c0bf23ceb1ae63d205f30bd97683255221f3ca

--- programme/ascii-value/ascii-value.c
+++ programme/ascii-value/ascii-value.c
@@ -1,10 +1,11 @@
 // Write a programme to print ASCII Value of a character.
 #include <stdio.h>
 
-int main() {
+int main()
+{
     char a;
-    printf("Enter the character: ");
+    printf("Enter the character : ");
     scanf("%c", &a);
-    printf("ASCII value is: %d\n", (int)(a));
+    printf("ASCII value is : %d\n", (int)(a));
     return 0;
 }
