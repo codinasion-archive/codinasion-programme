e836e39955c1cdea47445912e0793f7f04efbb31
Luís <a100549@alunos.uminho.pt>
2022-03-15 23:16:17 +0000

    Code simplification and optimization

--- programme/minimum-of-numbers/min.c
+++ programme/minimum-of-numbers/min.c
@@ -3,17 +3,18 @@
 #include <limits.h>
 
 int main () {
-    // Initializes the variable as the maximum integer possible
-    int min= INT_MAX;
-    int test;
-
-    printf ("Enter numbers (enter 0 to stop)\n");
+    // Initializes the min variable as the maximum integer possible
+    int min= INT_MAX,n,test;
+    printf ("Enter amount of numbers: \n");
+    scanf ("%i",&n);
+    printf ("Enter numbers: \n");
     // Input one number at a time
-    while (test !=0) {     
+    while (n > 0) {     
         scanf ("%i",&test); 
         if (test < min) min = test;
+        n--;
     }; 
     // Output
-    printf ("%i\n",min); 
+    printf ("The minimum is: %i\n",min); 
     return 0;
 }
