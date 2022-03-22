0e0d55fd513761dcdb3ac9603192c9cadfbebfbf
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-03-22 12:38:30 +0530

    Create number-triangle-pattern.c

---
+++ programme/number-triangle-pattern/number-triangle-pattern.c
@@ -0,0 +1,37 @@
+// Write a program to print number triangle pattern
+#include <stdio.h>
+
+void pattern(int a)
+{
+    printf("Output :\n");
+    for (int i = 1; i <= a; i++)
+    {
+        int c = 1;
+        for (int k = a; k > i; k--)
+        {
+            printf(" ");
+        }
+        for (int j = 1; j <= 2 * i - 1; j++)
+        {
+            if (j < i)
+            {
+                printf("%d", c);
+                c++;
+            }
+            else
+            {
+                printf("%d", c);
+                c--;
+            }
+        }
+        printf("\n");
+    }
+}
+int main()
+{
+    int a;
+    printf("Input  : ");
+    scanf("%d", &a);
+    pattern(a);
+    return 0;
+}
