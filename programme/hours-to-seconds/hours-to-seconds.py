c3f897d617d94a8335d1902db4b32293131ee256
GodKimba <rafaelfadel2@gmail.com>
2022-03-22 10:13:11 -0300

    correcting grammar and adding more comments
    Former-commit-id: 3921b667b6bcf01f1b21651f5b4d7bc7b1655a29

--- programme/hours-to-seconds/hours-to-seconds.py
+++ programme/hours-to-seconds/hours-to-seconds.py
@@ -1,5 +1,13 @@
-
-hours = input("Type how many hours you want to convert into seconds: ")
+# Hours to seconds converter program
+# Input the hours from the user
+hours = int(input("How many hours do you want to convert into seconds: "))
+# Convert the number in hours to seconds (convert the variable to integer because input gets input as string by default)
 seconds = int(hours) * 3600
 
-print(f"{hours} hour is equal to {seconds} seconds!")
+# Print the result
+# If function just to correct the grammar. If it is less than 1 hour, keep it singular, else turn into plural.
+if hours <= 1:
+    # Using f string to insert the variables
+    print(f"{hours} hour is equal to {seconds} seconds!")
+else:
+    print(f"{hours} hours is equal to {seconds} seconds!")
