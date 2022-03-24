bc4363834336d755f2e40eba5c4523a3a5e49867
Ramanand Thakur <80584888+ramanand-rv@users.noreply.github.com>
2022-03-25 01:22:35 +0530

    Create area-of-square.cpp

---
+++ programme/area-of-square/area-of-square.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    double side;
+  
+    // input 
+    cout << "Enter side length: ";
+    cin >> side;
+  
+  double area = side*side;
+
+    // output area
+    cout << "Area of Square: " << area;
+  
+  
+  return 0;
+}
