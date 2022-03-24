867969c927c6adc28c6324ec3c9d973d135cb930
Ramanand Thakur <80584888+ramanand-rv@users.noreply.github.com>
2022-03-25 01:17:59 +0530

    Create area-of-rectangle.cpp
    Former-commit-id: 0a41c2b4abd1d2e3438ae5a00e2f2b3853c9a3db

---
+++ programme/area-of-rectangle/area-of-rectangle.cpp
@@ -0,0 +1,21 @@
+// Write a programme to find the area of Rectangular
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    double length, width;
+    
+    // input radius
+    cout << "Enter length: ";
+    cin >> length;
+  
+    cout << "Enter breadth: ";
+    cin >> width;
+
+  double result = length* width;
+    // output area
+    cout << "Area: " << result;
+  
+  return 0;
+}
