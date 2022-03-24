15e8795eeb54e837e41fd2b4feb2b594419ba9e0
Ramanand Thakur <80584888+ramanand-rv@users.noreply.github.com>
2022-03-25 01:11:44 +0530

    Create add-two-numbers.cpp

---
+++ programme/add-two-numbers/add-two-numbers.cpp
@@ -0,0 +1,19 @@
+// Write a programme to add two numbers 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    
+   // input
+  cin>>a>>b;
+  
+  // Calculate
+  int sum = a+b;
+  
+   // output area
+    cout << "Sum: " << sum;
+  
+  return 0;
+}
