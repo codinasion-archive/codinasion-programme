d79b83d9365c072c0c32953ceaa994683338946c
Neel Patel <98449915+NeelPatel31@users.noreply.github.com>
2022-03-25 20:25:12 +0530

    Add files via upload
    Former-commit-id: 193ef56fa04ceb0c0646593d29828268fa2ba9f5

---
+++ programme/number-triangle-pattern/number-triangle-pattern.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+void pattern(int a){
+    cout<<"Output:\n";
+    for(int i= 1; i<=a ; i++){
+        int c = 1;
+        for(int k = a ; k>i ; k--){
+            cout<<" ";
+        }
+        for(int j = 1; j<=2*i-1; j++){
+            if(j<i){
+                cout<<c;
+                c++;
+            }
+            else{
+                cout<<c;
+                c--;
+            }
+
+        }
+        cout<<"\n";
+    }
+}
+int main(){
+    int a;
+    cout<<"Input: ";
+    cin>>a;
+    pattern(a);
+    return 0;
+}
