ad5236824b92be8644bf76bee3c887beeb8d5aef
Neel Patel <98449915+NeelPatel31@users.noreply.github.com>
2022-03-25 19:59:11 +0530

    Add : number triangle pattern in python
    Former-commit-id: 96b8f0ef5d24f6dbce916c44b3a714a452dd0c1f

---
+++ programme/number-triangle-pattern/number-triangle-pattern.py
@@ -0,0 +1,17 @@
+def pattern(a):
+    print("Output:")
+    for i in range(1,a+1):
+        c = 1
+        for k in range(a,i,-1):
+            print(" ",end="")
+        for j in range(1,2*i):
+            if j<i:
+                print(c,end="")
+                c+=1
+            else:
+                print(c,end="")
+                c-=1
+        print()
+
+a = int(input("Input: "))
+pattern(a)
