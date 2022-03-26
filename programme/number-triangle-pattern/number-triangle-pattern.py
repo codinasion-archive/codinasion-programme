e16682da36e20c8a0d3da6019b4db147f2d2d588
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-03-26 21:05:03 +0530

    Update number-triangle-pattern.py

--- programme/number-triangle-pattern/number-triangle-pattern.py
+++ programme/number-triangle-pattern/number-triangle-pattern.py
@@ -1,17 +1,19 @@
+# Write a program to print the pattern
 def pattern(a):
-    print("Output:")
-    for i in range(1,a+1):
+    print("Output :")
+    for i in range(1, a+1):
         c = 1
-        for k in range(a,i,-1):
-            print(" ",end="")
-        for j in range(1,2*i):
-            if j<i:
-                print(c,end="")
-                c+=1
+        for k in range(a, i, -1):
+            print(" ", end="")
+        for j in range(1, 2*i):
+            if j < i:
+                print(c, end="")
+                c += 1
             else:
-                print(c,end="")
-                c-=1
+                print(c, end="")
+                c -= 1
         print()
 
-a = int(input("Input: "))
+
+a = int(input("Input  : "))
 pattern(a)
