ac2ef11ce7f903f393a027a4229ab26e3ba1cc81
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-03-26 21:05:06 +0530

    Update number_triangle_pattern.java
    Former-commit-id: 3d87d2d8c3fa537595817d657c009f51dd1d3cea

--- programme/number-triangle-pattern/number_triangle_pattern.java
+++ programme/number-triangle-pattern/number_triangle_pattern.java
@@ -1,30 +1,31 @@
+// Write a program to print the pattern
 import java.util.Scanner;
 
-public class number_triangle_pattern{
+public class number_triangle_pattern {
     private static void pattern(int a) {
-        System.out.print("Output:\n");
-        for(int i= 1; i<=a ; i++){
+        System.out.print("Output :\n");
+        for (int i = 1; i <= a; i++) {
             int c = 1;
-            for(int k = a ; k>i ; k--){
+            for (int k = a; k > i; k--) {
                 System.out.print(" ");
             }
-            for(int j = 1; j<=2*i-1; j++){
-                if(j<i){
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                if (j < i) {
                     System.out.print(c);
                     c++;
-                }
-                else{
+                } else {
                     System.out.print(c);
                     c--;
                 }
 
             }
             System.out.print("\n");
-        }        
+        }
     }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Input: ");
+        System.out.print("Input  : ");
         int a = sc.nextInt();
         pattern(a);
         sc.close();
