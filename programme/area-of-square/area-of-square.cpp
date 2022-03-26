a6fb1bf3874ca887c4279b0d36beb7e38f31c842
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-03-26 20:38:17 +0530

    Update area-of-square.cpp

--- programme/area-of-square/area-of-square.cpp
+++ programme/area-of-square/area-of-square.cpp
@@ -1,19 +1,19 @@
+// Write a program to find the area of a square
 #include <bits/stdc++.h>
 using namespace std;
 
 int main()
 {
-    double side;
-  
-    // input 
-    cout << "Enter side length: ";
-    cin >> side;
-  
-  double area = side*side;
+  double side;
 
-    // output area
-    cout << "Area of Square: " << area;
-  
-  
+  // input
+  cout << "Enter side length : ";
+  cin >> side;
+
+  double area = side * side;
+
+  // output area
+  cout << "Area of Square : " << area << endl;
+
   return 0;
 }
