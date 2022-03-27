317d5fbcc1fd6651f3245ffc3bd8667e7de6ff30
Neel Patel <98449915+NeelPatel31@users.noreply.github.com>
2022-03-27 16:44:58 +0530

    add : alphabet-triangle-pattern programme

---
+++ programme/alphabet-triangle-pattern/alphabet-triangle-pattern.py
@@ -0,0 +1,18 @@
+def pattern(a):
+    print("Output:")
+    for i in range(1, a + 1):
+        c = 65
+        for k in range(a, i, -1):
+            print(" ", end="")
+        for j in range(1, 2 * i):
+            if j < i:
+                print(chr(c), end="")
+                c += 1
+            else:
+                print(chr(c), end="")
+                c -= 1
+        print()
+
+
+a = int(input("Input: "))
+pattern(a)
