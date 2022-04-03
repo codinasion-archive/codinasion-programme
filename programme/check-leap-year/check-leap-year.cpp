111f5c26be155adddff7dee58c07fc228da0943a
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-04-03 19:00:44 +0530

    Update check-leap-year.cpp
    Former-commit-id: da352573685dab41d0df5ba920de185f976c3cf5

--- programme/check-leap-year/check-leap-year.cpp
+++ programme/check-leap-year/check-leap-year.cpp
@@ -1,14 +1,16 @@
-#include<bits/stdc++.h>
+// Write a programme to check leap year
+#include <bits/stdc++.h>
 
 using namespace std;
 
-int main() {
-    cout << "Input : ";
-    int year;
-    cin >> year;
-    cout << "Output : ";
-    if (year % 4 == 0 && (year % 400 == 0 || year % 100 != 0))
-        cout << "Leap Year\n";
-    else
-        cout << "Not a Leap Year\n";
+int main()
+{
+  cout << "Input  : ";
+  int year;
+  cin >> year;
+  cout << "Output : ";
+  if (year % 4 == 0 && (year % 400 == 0 || year % 100 != 0))
+    cout << "Leap Year\n";
+  else
+    cout << "Not a Leap Year\n";
 }
