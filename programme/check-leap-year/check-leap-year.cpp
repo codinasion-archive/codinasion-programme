081f0649384517edc6901e1153286646d01ee238
rootzer00 <16ucs127@lnmiit.ac.in>
2022-04-03 16:37:30 +0530

    Added check-leap-year.cpp

---
+++ programme/check-leap-year/check-leap-year.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    cout << "Input : ";
+    int year;
+    cin >> year;
+    cout << "Output : ";
+    if (year % 4 == 0 && (year % 400 == 0 || year % 100 != 0))
+        cout << "Leap Year\n";
+    else
+        cout << "Not a Leap Year\n";
+}
