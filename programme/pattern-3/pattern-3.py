bd970e14136f8f49328c0334df4b7794fa60a070
Seraph★776 <72005563+seraph776@users.noreply.github.com>
2022-04-05 04:47:57 -0400

    Update pattern-3.py
    Updating Documentation
    
    Former-commit-id: bfc2eb33f3c4588bdd06a86cec72c8cce57f12e4

--- programme/pattern-3/pattern-3.py
+++ programme/pattern-3/pattern-3.py
@@ -1,5 +1,5 @@
 def print_pattern(n: int) -> None:
-    """Prints a nxn box outline pattern"""
+       """Prints a square outline pattern"""
     for col in range(n):
         for row in range(n):
             if col == 0 or col == n - 1 or row == 0 or row == n - 1:
