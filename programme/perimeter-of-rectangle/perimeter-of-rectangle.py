34aef18fb145d7040d4303617c20f190a3f1cfb1
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-04-06 08:10:53 +0530

    Update perimeter-of-rectangle.py

--- programme/perimeter-of-rectangle/perimeter-of-rectangle.py
+++ programme/perimeter-of-rectangle/perimeter-of-rectangle.py
@@ -1,6 +1,6 @@
 # Computes the perimeter of a rectangle
 
-length = int(input("length: "))
-breadth = int(input("breadth: "))
+length = int(input("length  : "))
+breadth = int(input("breadth : "))
 
-print(f"perimeter: {2*(length + breadth)}")
+print(f"\nperimeter : {2*(length + breadth)}")
