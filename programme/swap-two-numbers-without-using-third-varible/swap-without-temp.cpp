a22cfcd26a08c973205f469dd6fb02bff227b1f7
wkktoria <abcwiktoria00@gmail.com>
2022-04-07 10:30:26 +0200

    Add swap-without-temp.cpp

---
+++ programme/swap-two-numbers-without-using-third-varible/swap-without-temp.cpp
@@ -0,0 +1,22 @@
+﻿// Write a program to swap two numbers without using third variable.
+#include <iostream>
+
+void swap(int &x, int &y) {
+  x = x + y;
+  y = x - y;
+  x = x - y;
+}
+
+int main() {
+  int a, b;
+  std::cout << "a:";
+  std::cin >> a;
+  std::cout << "b:";
+  std::cin >> b;
+
+  swap(a, b);
+
+  std::cout << "a=" << a << " b=" << b << "\n";
+
+  return 0;
+}
