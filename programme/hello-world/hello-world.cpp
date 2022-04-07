4a212819e05ab6142bf93cf990addcebcc8624b6
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-04-07 09:50:43 +0530

    Update hello-world.cpp

--- programme/hello-world/hello-world.cpp
+++ programme/hello-world/hello-world.cpp
@@ -1,7 +1,8 @@
 // Write a program to print Hello World
 #include <iostream>
 
-int main() {
-    std::cout << "Hello World !!!\n";
-    return 0;
+int main()
+{
+  std::cout << "Hello World !!!\n";
+  return 0;
 }
