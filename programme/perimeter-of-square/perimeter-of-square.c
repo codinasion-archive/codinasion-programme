f61157b5d8ea2230fd69d1e38511b7259730c9f6
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-04-07 09:46:50 +0530

    Update perimeter-of-square.c
    Former-commit-id: b8ae373f3cfe2a845d9921ca105a7d97a4580a48

--- programme/perimeter-of-square/perimeter-of-square.c
+++ programme/perimeter-of-square/perimeter-of-square.c
@@ -1,23 +1,26 @@
+// Write a programme to find perimeter of square
 #include <stdio.h>
 #include <stdlib.h> /* for atof function */
 
 int main()
 {
-    double side, perimeter;
-    char sidestr[32];
+  double side, perimeter;
+  char sidestr[32];
 
-    printf("Enter length of side: ");
-    for (;;) { /* get user input until input is valid */
-        scanf("%s", sidestr);
-        side = atof(sidestr);
-        if (side <= 0) {
-            printf("Invalid input.\nEnter length of side: ");
-            continue;
-        }
-        else
-            break;
+  printf("Enter length of side : ");
+  for (;;)
+  { /* get user input until input is valid */
+    scanf("%s", sidestr);
+    side = atof(sidestr);
+    if (side <= 0)
+    {
+      printf("\nInvalid input.\n\nEnter length of side : ");
+      continue;
     }
-    perimeter = 4 * side;
-    printf("Perimeter: %g\n", perimeter);
-    return 0;
+    else
+      break;
+  }
+  perimeter = 4 * side;
+  printf("Perimeter : %g\n", perimeter);
+  return 0;
 }
