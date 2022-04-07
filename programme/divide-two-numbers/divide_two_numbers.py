e7dbf13b088d557402676b0a3f3327381789b7c5
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-04-07 10:29:29 +0530

    Update divide_two_numbers.py

--- programme/divide-two-numbers/divide_two_numbers.py
+++ programme/divide-two-numbers/divide_two_numbers.py
@@ -4,14 +4,14 @@ import sys
 def divide_two_numbers(n1: int, n2: int) -> None:
     """Divides two numbers"""
     try:
-        print(f'Output: {n1 / n2}')
+        print(f'\nOutput : {n1 / n2}')
     except ZeroDivisionError:
-        print('Cannot divide by zero!', file=sys.stderr)
+        print('\nCannot divide by zero!', file=sys.stderr)
         pass
 
 
 if __name__ == '__main__':
-    ht = {1: 'dividend', 2: 'divisor'}
+    ht = {1: 'dividend', 2: 'divisor '}
 
-    n1, n2 = [int(input(f'Input the {ht.get(i)}:\n> ')) for i in range(1, 3)]
+    n1, n2 = [int(input(f'Input the {ht.get(i)} : ')) for i in range(1, 3)]
     divide_two_numbers(n1, n2)
