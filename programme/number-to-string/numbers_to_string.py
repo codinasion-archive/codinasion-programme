de57bc4e7260e5b529f11407b7c4afb4ab3d8c3c
Seraph★776 <72005563+seraph776@users.noreply.github.com>
2022-04-07 13:56:49 -0400

    Create numbers_to_string.py
    Adding numbers_to_string.py alternative solution
    
    Former-commit-id: 267f02148dbc753a9ae00527717761caf6b39055

---
+++ programme/number-to-string/numbers_to_string.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+def numbers_to_strings(n: int) -> str:
+    """Converts numbers to string"""
+    d = {0: 'zero', 1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five', 6: 'six', 7: 'seven', 8: 'eight', 9: 'nine'}
+    n = [int(i) for i in str(n)]
+    output = ' '.join([d.get(i) for i in n])
+
+    return output
+
+
+if __name__ == '__main__':
+    print(numbers_to_strings(776))  # seven seven six
+    print(numbers_to_strings(237))  # two three seven
+    print(numbers_to_strings(13))  # one three
