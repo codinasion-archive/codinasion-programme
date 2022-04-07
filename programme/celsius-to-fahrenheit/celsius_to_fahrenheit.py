a8fb20c6aff589823736c87066606136f315c743
Seraph★776 <72005563+seraph776@users.noreply.github.com>
2022-04-07 14:15:39 -0400

    Create celsius_to_fahrenheit.py

---
+++ programme/celsius-to-fahrenheit/celsius_to_fahrenheit.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+def celsius_to_fahrenheit(celsius: int) -> float:
+  """Converts celsius to farenheit
+    fahrenheit = (celsius * 9 / 5) + 32
+    return fahrenheit
+
+
+if __name__ == '__main__':
+    while user_input := input('Enter celsius:\n> '):
+        print(f'{celsius_to_fahrenheit(int(user_input))}\n')
