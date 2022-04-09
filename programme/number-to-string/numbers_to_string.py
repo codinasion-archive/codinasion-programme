7ecc77aad767d574452d6f77a0d0b769a178fc4f
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-04-09 14:07:04 +0530

    Update numbers_to_string.py
    Former-commit-id: 863065e7db7cf79e42c9296daf152473f659eda8

--- programme/number-to-string/numbers_to_string.py
+++ programme/number-to-string/numbers_to_string.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
+# Write a program to convert number to strings
 
 def numbers_to_strings(n: int) -> str:
     """Converts numbers to string"""
-    d = {0: 'zero', 1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five', 6: 'six', 7: 'seven', 8: 'eight', 9: 'nine'}
+    d = {0: 'zero', 1: 'one', 2: 'two', 3: 'three', 4: 'four',
+         5: 'five', 6: 'six', 7: 'seven', 8: 'eight', 9: 'nine'}
     n = [int(i) for i in str(n)]
     output = ' '.join([d.get(i) for i in n])
 
