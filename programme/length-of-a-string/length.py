46fef27bad9d0ffe7922cdfe0e81ad3731d8540c
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-04-09 13:22:06 +0530

    Update length.py
    Former-commit-id: f83c0c1bdb6ac0b90549fea23c6576866685ea0a

--- programme/length-of-a-string/length.py
+++ programme/length-of-a-string/length.py
@@ -1,3 +1,4 @@
 # Write a programme to find the length of a string
-n = input("Input Enter a text: ")
-print(f"Length of the string is {len(n)}")
+n = input("Input Enter a text : ")
+
+print(f"\nLength of the string is {len(n)}")
