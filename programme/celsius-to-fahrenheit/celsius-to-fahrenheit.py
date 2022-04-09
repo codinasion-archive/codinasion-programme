b5afc0cf23e367e2cc5a485df5d1b9546f7b6636
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-04-09 14:01:52 +0530

    Update celsius-to-fahrenheit.py
    Former-commit-id: 56a7b93a36fcd4058dfa0aef7b77f336cb6bdec2

--- programme/celsius-to-fahrenheit/celsius-to-fahrenheit.py
+++ programme/celsius-to-fahrenheit/celsius-to-fahrenheit.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
+# Write a program to convert the temperature from Celsius to Fahrenheit
 
 def celsius_to_fahrenheit(celsius: int) -> float:
-  """Converts celsius to farenheit
+    """Converts celsius to farenheit"""
     fahrenheit = (celsius * 9 / 5) + 32
     return fahrenheit
 
 
 if __name__ == '__main__':
-    while user_input := input('Enter celsius:\n> '):
-        print(f'{celsius_to_fahrenheit(int(user_input))}\n')
+    user_input = input('Enter celsius : ')
+    print(f"\nFahrenheit : {celsius_to_fahrenheit(int(user_input))}")
