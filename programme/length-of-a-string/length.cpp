98639d01ebe12454e16d771960f54cadfdeb13a1
superpthegreat <anthony.soriano453@gmail.com>
2022-04-09 09:52:35 -0700

    Added C++ version of length of string

---
+++ programme/length-of-a-string/length.cpp
@@ -0,0 +1,19 @@
+//Write a program to find the length of a string
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+int main()
+{   
+    // string variable
+    string str;
+
+    // input
+    cout << "Please enter a string : ";
+    getline(cin, str);
+
+    // output
+    cout << "Length of string is : " << str.size();
+
+    return 0;
+}
