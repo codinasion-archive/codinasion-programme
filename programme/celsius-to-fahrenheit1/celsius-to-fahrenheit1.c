a16a54d40010f13bfb2f14b1b059b61e6cb1ef83
Khushi369 <khushichauhan1706@gmail.com>
2022-04-10 13:39:15 +0530

    Conversion from celsius to fahrenheit
    Former-commit-id: edc666280132146d8c330c35a3194ed16e2b0b39

---
+++ programme/celsius-to-fahrenheit1/celsius-to-fahrenheit1.c
@@ -0,0 +1,11 @@
+//C# program to convert the temperature from Celsius to Fahrenheit
+
+#include<stdio.h>
+void main()
+{
+  float c,f;
+  printf("Input ( C ) : ");
+  scanf("%f", &c);
+  f = ((9 * c) / 5) + 32;
+  printf("Output ( F )  : %f\n", f);
+}
