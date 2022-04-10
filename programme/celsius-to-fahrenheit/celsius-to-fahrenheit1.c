4ec41b17519099574968a31b5be21c005f4b0952
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-04-10 23:52:06 +0530

    Update celsius-to-fahrenheit1.c

--- programme/celsius-to-fahrenheit/celsius-to-fahrenheit1.c
+++ programme/celsius-to-fahrenheit/celsius-to-fahrenheit1.c
@@ -1,11 +1,11 @@
-//C# program to convert the temperature from Celsius to Fahrenheit
+// Write a program to convert the temperature from celsius to fahrenheit
 
-#include<stdio.h>
+#include <stdio.h>
 void main()
 {
-  float c,f;
-  printf("Input ( C ) : ");
+  float c, f;
+  printf("Input ( C )  : ");
   scanf("%f", &c);
   f = ((9 * c) / 5) + 32;
-  printf("Output ( F )  : %f\n", f);
+  printf("Output ( F ) : %.2f\n", f);
 }
