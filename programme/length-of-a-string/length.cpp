282c45a99476734bbe029bf0cbc5d736cac6ea48
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-04-10 20:57:06 +0530

    Update length.cpp
    Former-commit-id: a8add5da457391eb8c0dc85ab3dd484fc4dbb749

--- programme/length-of-a-string/length.cpp
+++ programme/length-of-a-string/length.cpp
@@ -1,19 +1,19 @@
-//Write a program to find the length of a string
+// Write a program to find the length of a string
 #include <iostream>
 #include <string.h>
 using namespace std;
 
 int main()
-{   
-    // string variable
-    string str;
+{
+  // string variable
+  string str;
 
-    // input
-    cout << "Please enter a string : ";
-    getline(cin, str);
+  // input
+  cout << "Please enter a string : ";
+  getline(cin, str);
 
-    // output
-    cout << "Length of string is : " << str.size();
+  // output
+  cout << "Length of string is : " << str.size() << endl;
 
-    return 0;
+  return 0;
 }
