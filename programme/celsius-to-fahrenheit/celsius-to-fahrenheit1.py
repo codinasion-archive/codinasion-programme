618f1af6f204230d30bb5260fd305ace966a8e88
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-04-10 23:46:29 +0530

    Update celsius-to-fahrenheit1.py
    Former-commit-id: 1492f5f5893aa36eb46b9d73600d45a57f2715a5

--- programme/celsius-to-fahrenheit/celsius-to-fahrenheit1.py
+++ programme/celsius-to-fahrenheit/celsius-to-fahrenheit1.py
@@ -1,11 +1,8 @@
-celsius_1 = float(input("Input ( C ): " ))  
-  
-# For Converting the temperature to degree Fahrenheit by using the above  
-# given formula  
-Fahrenheit_1 = (celsius_1 * 1.8) + 32  
-    
-# print the result  
-print('Output ( F ): %.2f'  
-      %(Fahrenheit_1))  
-  
- 
+# Write a program to convert the temperature from Celsius to Fahrenheit
+celsius_1 = float(input("Input ( C )  : "))
+
+# For Converting the temperature to Fahrenheit
+Fahrenheit_1 = (celsius_1 * 1.8) + 32
+
+# print the result
+print('Output ( F ) : %.2f' % (Fahrenheit_1))
