8351b666de9b12de69b3060bfbfaa90bd676c5f9
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-04-11 19:33:30 +0530

    Update perimeter-of-square.cpp

--- programme/perimeter-of-square/perimeter-of-square.cpp
+++ programme/perimeter-of-square/perimeter-of-square.cpp
@@ -1,20 +1,22 @@
+// Write a programme to find perimeter of square
 #include <iostream>
-//this namespace function allows you to skip
-//over the "std::".
+// this namespace function allows you to skip
+// over the "std::".
 using namespace std;
 
-int main(){
-    //set up the variables for the two values
+int main()
+{
+    // set up the variables for the two values
     int side;
     int perimeter;
-    
-    //getting the user input for the size of their side
-    cout << "Side : ";
-    cin  >> side;
 
-    //calculating the perimeter
+    // getting the user input for the size of their side
+    cout << "Side : ";
+    cin >> side;
+
+    // calculating the perimeter
     perimeter = side * 4;
 
-    //printing out the result
+    // printing out the result
     cout << "Perimeter : " << perimeter << endl;
 }
