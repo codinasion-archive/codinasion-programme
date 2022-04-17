a5a2bffaddff5262234a09c86d33e580f1351c40
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-04-17 20:18:53 +0530

    Update eulerNumber.py

--- programme/euler-number/eulerNumber.py
+++ programme/euler-number/eulerNumber.py
@@ -1,6 +1,6 @@
+# Write a programme to compute the value of Euler's Number
 from math import factorial
 
+
 def e(n):
-    return sum((
-        1/factorial(i) for i in range(1, n+1)
-    ))
+    return sum((1 / factorial(i) for i in range(1, n + 1)))
