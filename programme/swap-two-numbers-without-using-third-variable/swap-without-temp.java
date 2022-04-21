553fefddef55c89966af0f0bd850b304c73094c7
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-04-21 12:50:00 +0530

    Update swap-without-temp.java
    Former-commit-id: baf27dd1f05d05c5f45e90e732e3b2238c010bf6

--- programme/swap-two-numbers-without-using-third-variable/swap-without-temp.java
+++ programme/swap-two-numbers-without-using-third-variable/swap-without-temp.java
@@ -3,17 +3,18 @@ import java.util.Scanner;
 
 public class Swap {
     private static void swap(int a, int b) {
-        System.out.println("Numbers before swap: " + a + " and " + b);
+        System.out.println("\nNumbers before swap : " + a + " and " + b);
         a = a + b;
         b = a - b;
         a = a - b;
-        System.out.println("Numbers after swap: " + a + " and " + b);
+        System.out.println("\nNumbers after swap  : " + a + " and " + b);
     }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a number: ");
+        System.out.print("Enter a number : ");
         int a = sc.nextInt();
-        System.out.print("Enter another number: ");
+        System.out.print("Enter another number : ");
         int b = sc.nextInt();
 
         swap(a, b);
