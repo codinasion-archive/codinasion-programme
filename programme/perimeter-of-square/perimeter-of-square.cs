4fcc09b2776e2700addda02bd96f4ab90c35f270
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-04-26 21:06:01 +0530

    Update perimeter-of-square.cs

--- programme/perimeter-of-square/perimeter-of-square.cs
+++ programme/perimeter-of-square/perimeter-of-square.cs
@@ -1,21 +1,21 @@
-﻿using System;
+// Write a programme to find perimeter of square
+using System;
 
 public class PerimeterOfSquare
 {
     public static void Main(string[] args)
     {
-
         Console.Write("Side : ");
         string input = Console.ReadLine();
         int side = 0;
-        //verifies valid # input and passes converted int to side variable
+        // verifies valid input and passes converted int to side variable
         if (int.TryParse(input, out side))
         {
             Console.WriteLine("Perimeter : " + (side * 4));
         }
         else
         {
-            Console.WriteLine("Invalid input.");
+            Console.WriteLine("\n*** Invalid input !!! ***");
         }
     }
 }
