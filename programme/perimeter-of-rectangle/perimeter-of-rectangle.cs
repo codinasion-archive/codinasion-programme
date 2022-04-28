6966848325b686732425b75309386aff9a162143
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-04-28 09:24:18 +0530

    Update perimeter-of-rectangle.cs
    Former-commit-id: d75159260c45103d45a19838fce2bc2353643c45

--- programme/perimeter-of-rectangle/perimeter-of-rectangle.cs
+++ programme/perimeter-of-rectangle/perimeter-of-rectangle.cs
@@ -5,21 +5,21 @@ public class PerimeterOfRectangle
 {
     public static void Main(string[] args)
     {
-        Console.Write("Length : ");
+        Console.Write("Length    : ");
         string input = Console.ReadLine();
         int length, breadth;
         // verifies valid input and passes converted int to length variable
         if (int.TryParse(input, out length))
         {
-            Console.Write("Breadth : ");
+            Console.Write("Breadth   : ");
             input = Console.ReadLine();
-            //gets 2nd user input and passes converted int to breadth variable
-            if (int.TryParse(input, out breadth)) {
-                Console.WriteLine("Perimeter : " + (2 * (length + breadth)));
+            // gets 2nd user input and passes converted int to breadth variable
+            if (int.TryParse(input, out breadth))
+            {
+                Console.WriteLine("\nPerimeter : " + (2 * (length + breadth)));
                 return;
             }
         }
-
         Console.WriteLine("\n*** Invalid input !!! ***");
     }
 }
