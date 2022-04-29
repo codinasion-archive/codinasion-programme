02728049b20ee90bb6d48fc5d401dac0c93a8c68
Eliza Migdal <20emigdal@castilleja.org>
2022-04-29 18:20:40 -0500

    Update AreaOfRhombus.java
    Former-commit-id: 81d0e1a1cab42822d7c2ebdfb002a7a23f460b67

---
+++ programme/area-of-rhombus/AreaOfRhombus.java
@@ -0,0 +1,17 @@
+import java.util.Scanner; // Import the Scanner class
+
+public class AreaOfRhombus {
+    public static void main(String[] args) {
+        float diag1, diag2, area;
+        Scanner diag = new Scanner(System.in);
+        System.out.print("Diagonal 1 : ");
+        diag1 = diag.nextInt();
+        
+        System.out.print("Diagonal 2 : ");
+        diag2 = diag.nextInt();
+        
+        area = diag1 * diag2 / 2;
+        System.out.println("");
+        System.out.println("Area : " + area);
+    }
+}
