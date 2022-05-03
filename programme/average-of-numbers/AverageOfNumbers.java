ddece90adf54812d4914b2b0d158a5128d3eb816
Anzo52 <andy@zollnersolutions.com>
2022-05-02 23:52:59 -0600

    Add AverageOfNumbers.java

--- programme/average-of-numbers/AverageOfNumbers.java
+++ programme/average-of-numbers/AverageOfNumbers.java
@@ -1,19 +1,45 @@
+
 import java.util.Scanner;
 
-public class AverageOfNumbers {   
-    static Scanner sc = new Scanner (System.in);
+public class AverageOfNumbers {
 
-    public static void main (String[] args) {
-        double i;
-        double sum = 0;
-        int count = 0;
-    
-        while(sc.hasNextDouble()) {
-            System.out.println("Input: ");
-            i = sc.nextDouble();
-            sum = sum + i;
-            count++;
+    private Scanner in;
+
+    public AverageOfNumbers() {
+        String getStr = getUserNums();
+        double result = userAvg(getStr);
+        printAverage(result, getStr);
+
+    }
+
+    public String getUserNums() {
+        in = new Scanner(System.in);
+        System.out.print("Input: ");
+        return in.nextLine();
+    }
+
+    public static double userAvg(String str) {
+        String[] arr = str.split(" ");
+        double sum = 0.0;
+        double average = 0.0;
+
+        for (int i = 0; i < arr.length; i++) {
+            sum += Integer.parseInt(arr[i]);
         }
-        System.out.println("Average: " + sum / (count));
+        if (arr.length > 0) {
+            average = sum / arr.length;
+        }
+
+        return average;
+    }
+
+    public static void printAverage(double average, String userNumInput) {
+        System.out.println("Output: " + average);
+
+    }
+
+    public static void main(String[] args) {
+        new AverageOfNumbers();
+
     }
 }
