20ddd23ea50608bad89cf38d4aabd90d789a09f1
gilazani <gilazani1@gmail.com>
2022-05-03 15:42:47 +0300

    adding volume-of-cylinder (java)
    Former-commit-id: 4f092f60f7c4419cc7294f2801e00166ef88d035

---
+++ programme/volume-of-cylinder/volume-of-cylinder.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class cylinder {
+
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the radius : ");
+
+        // taking radius input
+        int r = sc.nextInt();
+        
+        System.out.print("\nEnter the height : ");
+        
+        //taking height input
+        int h = sc.nextInt();
+
+        // calculating Volume of cylinder
+        double volume = Math.PI * r * r * h;
+
+        // rounding the answer to 2 decimal places
+        double answer = (double) Math.round(volume * 100) / 100;
+
+        // printing the final answer
+        System.out.println("Area of circle : " + answer);
+
+        sc.close();
+
+	}
+
+}
