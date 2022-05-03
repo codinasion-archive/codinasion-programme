91d8cc90043ab6e1b18eed5971767fbd3d5bb9cf
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-05-03 10:54:49 +0530

    Update area-of-trapezoid.c

--- programme/area-of-trapezoid/area-of-trapezoid.c
+++ programme/area-of-trapezoid/area-of-trapezoid.c
@@ -1,23 +1,23 @@
-/* C Program to Find the area of trapezoid */
+/* Program to Find the area of trapezoid */
 
 #include <stdio.h>
- 
+
 int main()
 {
     float base1, base2, height;
     float area;
- 
-    printf("Base 1 : "); 
+
+    printf("Base 1 : ");
     scanf("%f", &base1);
 
-    printf("\nBase 2 : "); 
+    printf("Base 2 : ");
     scanf("%f", &base2);
 
-    printf("\nHeight : "); 
+    printf("Height : ");
     scanf("%f", &height);
 
-    area = (base1+base2)*height/2;
+    area = (base1 + base2) * height / 2;
 
-    printf("\nArea: %.2f", area);
+    printf("\nArea: %.2f\n", area);
     return 0;
 }
