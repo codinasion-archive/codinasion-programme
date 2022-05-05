6fb7fea3e90018fcfbce3f2d27b226e3bd629cf1
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-05-05 13:51:27 +0530

    Update volume-of-cylinder.cpp
    Former-commit-id: 1c3b5d173d1302e86e84134bf0e595139e3de3f7

--- programme/volume-of-cylinder/volume-of-cylinder.cpp
+++ programme/volume-of-cylinder/volume-of-cylinder.cpp
@@ -1,22 +1,24 @@
-// Write a program to find the Volume of a Cylinder. 
+// Write a program to find the Volume of a Cylinder
 
-#include<iostream>
+#include <iostream>
+#include <math.h>
+
 using namespace std;
 
 int main()
 {
-	float radius, height, volume;
+    float radius, height, volume;
 
-	cout << "Enter Radius of Cylinder : ";
-	cin >> radius;
-	cout << "Enter Height of Cylinder :";
-	cin >> height;
+    cout << "Enter Radius of Cylinder : ";
+    cin >> radius;
+    cout << "Enter Height of Cylinder : ";
+    cin >> height;
 
-	volume = 3.14 * radius * radius * height;
+    // value of PI is defined in M_PI in math.h
+    volume = M_PI * radius * radius * height;
 
-	cout << "Radius: " << radius << endl;
-	cout << "Height: " << height << endl;
-	cout << "Volume of Cylinder is : " << volume;
+    cout << endl
+         << "Volume of Cylinder is : " << volume << endl;
 
-	return 0;
+    return 0;
 }
