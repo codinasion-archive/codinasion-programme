8cd31ac2e112bbeb4a3f8494476b3019625b09d2
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-05-05 13:24:26 +0530

    Update hourstoseconds.java

--- programme/hours-to-seconds/hourstoseconds.java
+++ programme/hours-to-seconds/hourstoseconds.java
@@ -1,13 +1,16 @@
+// Write a programme to convert hours to seconds
+
 import java.util.Scanner;
+
 class hoursToSeconds {
-    public static void main(String args[]){
-        
+    public static void main(String args[]) {
+
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("hours: ");
+        System.out.print("Hours   : ");
         int s = Integer.parseInt(sc.nextLine());
-        s = s*60*60;
-        System.out.println("Seconds: " + s);
+        s = s * 60 * 60;
+        System.out.println("Seconds : " + s);
         sc.close();
 
     }
