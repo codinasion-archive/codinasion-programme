d2c26238aa9ff89c7bce60828ade7ff2907aa10a
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-05-05 13:15:21 +0530

    Update volume-of-cylinder.py

--- programme/volume-of-cylinder/volume-of-cylinder.py
+++ programme/volume-of-cylinder/volume-of-cylinder.py
@@ -1,3 +1,5 @@
+# Write a program to compute the volume of a Cylinder
+
 import math
 
 '''
@@ -5,12 +7,12 @@ import math
     Date: 03/05/2022
 '''
 
-#Get radius and height from user
+# Get radius and height from user
 enter_radius = int(input('Radius : '))
 enter_height = int(input('Height : '))
 
-#Calculate volume
+# Calculate volume
 volume = enter_radius * enter_radius * enter_height * math.pi
 
-#Print result
+# Print result
 print("\nVolume : {:0.2f}".format((volume)))
