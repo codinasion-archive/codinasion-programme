fdd7c245d9ff08773f157416bf98e8d5527d0e57
Anzo52 <azollner2014@gmail.com>
2022-05-06 11:22:20 -0600

    Update PrintEven.java
    Add method (for real this time).
    
    Former-commit-id: 6cf943a7c36169defe2fc91e3e2c6c9d8e3ca5ca

--- programme/print-even-numbers/PrintEven.java
+++ programme/print-even-numbers/PrintEven.java
@@ -2,6 +2,24 @@ import java.util.Scanner;
 
 public class PrintEven {
   public static int[] printEven(int num) {
-      
+      int[] arr = new int[num];
+      int i = 0;
+      while (i < num) {
+          arr[i] = i * 2;
+          i++;
+      }
+      return arr;
+  }
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Input: ");
+    int num = sc.nextInt();
+    int[] arr = printEven(num);
+    System.out.println("Output:");
+    for (int i = 0; i < arr.length; i++) {
+      System.out.print(arr[i] + " ");
+    }
+    sc.close();
   }
 }
