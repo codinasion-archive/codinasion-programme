c5a8ee88a091ab4d188aeca99f3ce5dc1b989ed4
Anzo52 <azollner2014@gmail.com>
2022-05-07 23:10:00 -0600

    Create SimpleInterestCalculator.java
    Former-commit-id: 5549caadde81b0f14afb4cc2b49e0ad111753711

---
+++ programme/simple-interest-calculator/SimpleInterestCalculator.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+
+public class SimpleInterestCalculator {
+
+    public static int interestCalculator(int principle, int time, int rate) {
+        int interest = (principle * time * rate) / 100;
+        return interest;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter P: ");
+        int principle = sc.nextInt();
+        System.out.print("Enter T: ");
+        int time = sc.nextInt();
+        System.out.print("Enter R: ");
+        int rate = sc.nextInt();
+        int interest = interestCalculator(principle, time, rate);
+        System.out.println("Simple interest: " + interest);
+        sc.close();
+    }
+}
