1827f118a738d404b699f4c2a27fbf8ddf774c59
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-05-10 21:36:53 +0530

    Update perimeter-of-rectangle.cpp
    Former-commit-id: b60f422079c1566c0c7836d28212a3f5ed4f22a1

--- programme/perimeter-of-rectangle/perimeter-of-rectangle.cpp
+++ programme/perimeter-of-rectangle/perimeter-of-rectangle.cpp
@@ -1,15 +1,18 @@
-#include<iostream>
+// Write a programme to find perimeter of rectangle
+
+#include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
     double len, wid;
 
-    cout<<"Enter the length: ";
-    cin>>len;
-    cout<<"Enter the width: ";
-    cin>>wid;
+    cout << "Enter the length : ";
+    cin >> len;
+    cout << "Enter the width  : ";
+    cin >> wid;
 
-    double perimeter = 2*(len+wid);
-    cout<<"Perimeter of the rectangle: "<<perimeter;
+    double perimeter = 2 * (len + wid);
+    cout << "\nPerimeter of the rectangle: " << perimeter << endl;
     return 0;
 }
