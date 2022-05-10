d6ba60f29aca391c85b5bd9722ef0fd582df3f70
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-05-10 21:43:39 +0530

    Update PrintEven.java
    Former-commit-id: 9f45964c305c02cb3c93dab0dec18a483759ea0f

--- programme/print-even-numbers/PrintEven.java
+++ programme/print-even-numbers/PrintEven.java
@@ -1,25 +1,28 @@
+// Write a programme to print first n even numbers
+
 import java.util.Scanner;
 
 public class PrintEven {
   public static int[] printEven(int num) {
-      int[] arr = new int[num];
-      int i = 0;
-      while (i < num) {
-          arr[i] = i * 2;
-          i++;
-      }
-      return arr;
+    int[] arr = new int[num];
+    int i = 0;
+    while (i < num) {
+      arr[i] = i * 2;
+      i++;
+    }
+    return arr;
   }
 
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    System.out.print("Input: ");
+    System.out.print("Input  : ");
     int num = sc.nextInt();
     int[] arr = printEven(num);
-    System.out.println("Output:");
+    System.out.print("Output : ");
     for (int i = 0; i < arr.length; i++) {
       System.out.print(arr[i] + " ");
     }
+    System.out.println();
     sc.close();
   }
 }
