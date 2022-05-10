d67c8efd4e433b0abeb3e27993e86dc4429b15e8
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-05-10 21:48:13 +0530

    Update SimpleInterestCalculator.java

--- programme/simple-interest-calculator/SimpleInterestCalculator.java
+++ programme/simple-interest-calculator/SimpleInterestCalculator.java
@@ -1,5 +1,6 @@
-import java.util.Scanner;
+// Write a programme to calculate simple interest
 
+import java.util.Scanner;
 
 public class SimpleInterestCalculator {
 
@@ -10,14 +11,14 @@ public class SimpleInterestCalculator {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter P: ");
+        System.out.print("Enter P : ");
         int principle = sc.nextInt();
-        System.out.print("Enter T: ");
+        System.out.print("Enter T : ");
         int time = sc.nextInt();
-        System.out.print("Enter R: ");
+        System.out.print("Enter R : ");
         int rate = sc.nextInt();
         int interest = interestCalculator(principle, time, rate);
-        System.out.println("Simple interest: " + interest);
+        System.out.println("\nSimple interest: " + interest);
         sc.close();
     }
 }
