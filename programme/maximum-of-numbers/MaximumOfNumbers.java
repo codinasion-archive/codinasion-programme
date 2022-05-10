8e87ed5c0130e472eed24a916174e2661939684b
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-05-10 21:11:19 +0530

    Update MaximumOfNumbers.java
    Former-commit-id: 70408be854eefc45e8dee85d961e4fdb40d237df

--- programme/maximum-of-numbers/MaximumOfNumbers.java
+++ programme/maximum-of-numbers/MaximumOfNumbers.java
@@ -1,21 +1,25 @@
+// Write a program to print the maximum of given numbers
 
-import java.io.IOException;
 import java.util.Scanner;
 import java.util.*;
+
 public class MaximumOfNumbers {
-    
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-         String input = sc.nextLine();
-         String[] inputArr = input.split(" ");
-         Integer[] numbers = new Integer[inputArr.length];
-         for(int i = 0; i < numbers.length; i++)
-         {
-             numbers[i] = Integer.parseInt(inputArr[i]);
-         }
-         List<Integer> list = Arrays.asList(numbers);
-         list.stream().reduce((x, y) -> x > y ? x : y).ifPresent(System.out::println);
-         
+        System.out.print("Enter numbers : ");
+        String input = sc.nextLine();
+
+        String[] inputArr = input.split(" ");
+        Integer[] numbers = new Integer[inputArr.length];
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = Integer.parseInt(inputArr[i]);
+        }
+        List<Integer> list = Arrays.asList(numbers);
+
+        System.out.print("\nMaximum Number : ");
+        list.stream().reduce((x, y) -> x > y ? x : y).ifPresent(System.out::println);
+        sc.close();
     }
-    
+
 }
