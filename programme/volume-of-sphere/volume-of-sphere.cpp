4d481cbb9273222a22bbba6a2a3db8728c70191e
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-05-14 02:02:50 +0530

    Update volume-of-sphere.cpp

--- programme/volume-of-sphere/volume-of-sphere.cpp
+++ programme/volume-of-sphere/volume-of-sphere.cpp
@@ -1,11 +1,15 @@
-#include<iostream>
+// Write a program to compute the volume of a Sphere
+
+#include <iostream>
+#include <math.h>
 using namespace std;
 
-int main(){
+int main()
+{
     double radius;
-    cout << "Enter the radius: " << endl;
+    cout << "Enter the radius : ";
     cin >> radius;
-    double area = (4/3) * 3.141 * radius * radius * radius;
-    cout << "The volume of the sphere: " << area << endl;
+    double area = (4 / 3.0) * M_PI * radius * radius * radius;
+    cout << "\nThe volume of the sphere: " << area << endl;
     return 0;
 }
