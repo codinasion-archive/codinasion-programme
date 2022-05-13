cc160df42c268b64dd85c1149c0162a98e2789c7
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-05-14 02:02:49 +0530

    Update volume-of-sphere.c

--- programme/volume-of-sphere/volume-of-sphere.c
+++ programme/volume-of-sphere/volume-of-sphere.c
@@ -1,10 +1,14 @@
-#include<stdio.h>
+// Write a program to compute the volume of a Sphere
 
-int main(){
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
     double radius;
-    printf("Enter the radius: \n");
+    printf("Enter the radius : ");
     scanf("%lf", &radius);
-    double area = (4/3) * 3.141 * radius * radius * radius;
-    printf("The volume of sphere: %lf\n", area);
+    double area = (4 / 3.0) * M_PI * radius * radius * radius;
+    printf("\nThe volume of sphere : %.2lf\n", area);
     return 0;
 }
