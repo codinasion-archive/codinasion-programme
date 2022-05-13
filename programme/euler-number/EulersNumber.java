0ad76fae098b53f5add39baab7303eceacda1144
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-05-14 02:22:10 +0530

    Update EulersNumber.java
    Former-commit-id: 92ff1a9fc3a927e91542bc2d6562a62ab02d4655

--- programme/euler-number/EulersNumber.java
+++ programme/euler-number/EulersNumber.java
@@ -1,21 +1,22 @@
-// Java program to compute the value of Euler's number
+// program to compute the value of Euler's number
 
 import java.util.Scanner;
-
 
 public class EulersNumber {
 
     public static double eulersNumber(int n) {
 
-       /* double e = 1;
-        double factorial = 1;
-
-        for (int i = 1; i <= n; i++) {
-            factorial = factorial * i;
-            e = e + 1 / factorial;
-        }
-
-        return e; */
+        /*
+         * double e = 1;
+         * double factorial = 1;
+         * 
+         * for (int i = 1; i <= n; i++) {
+         * factorial = factorial * i;
+         * e = e + 1 / factorial;
+         * }
+         * 
+         * return e;
+         */
 
         return Math.exp(n);
     }
@@ -23,9 +24,9 @@ public class EulersNumber {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
-        System.out.print("Input: ");
+        System.out.print("Input  : ");
         int n = input.nextInt();
-        System.out.println("Output: " + eulersNumber(n));
-        input.close(); 
+        System.out.println("Output : " + eulersNumber(n));
+        input.close();
     }
 }
