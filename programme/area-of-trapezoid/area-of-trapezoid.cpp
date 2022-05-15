5bc0bbac2bafbaf5186a09bda33078a70b104c44
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-05-15 19:53:19 +0530

    Update area-of-trapezoid.cpp
    Former-commit-id: cc93db5ca92ad7c5ef6a6a06b96bf7d0cd3843b5

--- programme/area-of-trapezoid/area-of-trapezoid.cpp
+++ programme/area-of-trapezoid/area-of-trapezoid.cpp
@@ -1,22 +1,25 @@
+// Write a program to compute area of trapezoid
+
 #include <iostream>
 
 using namespace std;
 
-int main(){
+int main()
+{
 
     int base1, base2, height;
 
-    cout << "Enter base1 : ";
+    cout << "Enter base1  : ";
     cin >> base1;
 
-    cout << "Enter base2 : ";
+    cout << "Enter base2  : ";
     cin >> base2;
 
     cout << "Enter height : ";
     cin >> height;
 
     float area = (float)(base1 + base2) * height / 2;
-    cout << "Area : " << area << endl;
+    cout << "\nArea of trapezoid : " << area << endl;
 
     return 0;
 }
