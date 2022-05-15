8ffdfd35c5692c2f8da0513a35ec0d66e94bdbae
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-05-15 18:42:31 +0530

    Update hello_world.java
    Former-commit-id: ac837548d58d179d8aad55667649478ea8977065

--- programme/hello-world/hello_world.java
+++ programme/hello-world/hello_world.java
@@ -1,5 +1,7 @@
-public class hello_world{
-    public static void main(String args[]){
+// Write a program to print Hello World
+
+public class hello_world {
+    public static void main(String args[]) {
         System.out.println("Hello World !!!");
     }
 }
