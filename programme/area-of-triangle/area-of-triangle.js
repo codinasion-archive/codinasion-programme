32bc3e0a3e03e6f08d3220f4da1e63fda3aa3983
tiarabcodes1 <tiaradominique.b@gmail.com>
2022-05-19 19:48:58 -0400

    Adds area of triangle in JavaScript file.
    Former-commit-id: d39eba78737ff76f5266bdd7ec3af3192b713f3e

---
+++ programme/area-of-triangle/area-of-triangle.js
@@ -0,0 +1,17 @@
+"use strict";
+
+function area (a, b, c) {
+  let s = (a + b + c) / 2;
+  let product = Math.sqrt( s * (s - a) * (s - b) * (s - c) );
+  
+  return product;
+}
+
+let a = prompt("a : ");
+let b = prompt("b : ");
+let c = prompt("c : ");
+let s = ( +a + +b + +c ) / 2;
+
+console.log("\ns:", s);
+
+console.log("\narea:", area(+a, +b, +c)); 
