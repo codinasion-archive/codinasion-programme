79445f342af9199aa71e45cd24042d6508f5fd0a
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-05-20 19:55:57 +0530

    Update compound-interest.js
    Former-commit-id: f6de12645e9056703357e07d85aea44d445c7283

--- programme/compound-interest-calculator/compound-interest.js
+++ programme/compound-interest-calculator/compound-interest.js
@@ -1,13 +1,18 @@
-function calculateCompoundInterest(amount, time, rate)
-{
+// Write a programme to calculate compound interest
 
-    const finalAmount = amount * ((1+(rate/100))**time);
-    const compoundInterest = finalAmount - amount;
-    return compoundInterest.toFixed(2);
+function calculateCompoundInterest(amount, time, rate) {
+  const finalAmount = amount * (1 + rate / 100) ** time;
+  console.log("Amount :", finalAmount.toFixed(2));
+
+  const compoundInterest = finalAmount - amount;
+  return compoundInterest.toFixed(2);
 }
 
 let amount = 100;
 let time = 10;
 let rate = 10;
 
-console.log(calculateCompoundInterest(amount, time, rate));
+console.log(
+  "Compound Interest :",
+  calculateCompoundInterest(amount, time, rate)
+);
