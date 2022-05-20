561503138c0e93455ba440f165acfe0c7b34dfb1
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-05-21 03:41:55 +0530

    Update table.py
    Former-commit-id: 3a013915bc6f2c9fb430a02fd783a7893c633c4c

--- programme/multiplication-table/table.py
+++ programme/multiplication-table/table.py
@@ -1,10 +1,10 @@
 # Write a program to output the following multiplication table
 
 def table_of(n):
-    for i in range(1, 11): # get first 10 result
+    for i in range(1, 11):  # get first 10 result
         print(f"{n} x {i} = {n*i}")
 
 
 n = int(input("Enter number: "))
 
-table_of(n) 
+table_of(n)
