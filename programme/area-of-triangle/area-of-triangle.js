65c204e1771584a7d62d626bb52352019a79fe2f
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-05-20 14:26:19 +0530

    Update area-of-triangle.js
    Former-commit-id: 62f3564b0cf1aaea60166ba862fd3676dadede27

--- programme/area-of-triangle/area-of-triangle.js
+++ programme/area-of-triangle/area-of-triangle.js
@@ -1,17 +1,19 @@
+// Write a programme to find area of triangle
+
 "use strict";
 
-function area (a, b, c) {
+function area(a, b, c) {
   let s = (a + b + c) / 2;
-  let product = Math.sqrt( s * (s - a) * (s - b) * (s - c) );
-  
+  let product = Math.sqrt(s * (s - a) * (s - b) * (s - c));
+
   return product;
 }
 
 let a = prompt("a : ");
 let b = prompt("b : ");
 let c = prompt("c : ");
-let s = ( +a + +b + +c ) / 2;
+let s = (+a + +b + +c) / 2;
 
-console.log("\ns:", s);
+console.log("\ns :", s);
 
-console.log("\narea:", area(+a, +b, +c)); 
+console.log("\narea :", area(+a, +b, +c));
