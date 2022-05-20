49435f45561853eca8184890afa1fee03570d61e
Archana <a@gmail.com>
2022-05-20 14:56:01 +0530

    added divide-two-numbers.js

---
+++ programme/divide-two-numbers/divide-two-numbers.js
@@ -0,0 +1,10 @@
+//Write a program to divide two given numbers
+
+function divide(a,b){
+    return ((a/b).toFixed(2));
+}
+
+let num1 = prompt("Enter First Number = ");
+let num2 = prompt("Enter Second Number = ");
+
+console.log(divide(num1,num2));
