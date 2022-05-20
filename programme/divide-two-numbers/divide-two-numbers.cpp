171353b968ba94c761dd5480fa7c5dc32a68dacc
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-05-21 01:49:47 +0530

    Update divide-two-numbers.cpp
    Former-commit-id: 4fedc91162fc4c092e1ed44646d790c01e9156cd

--- programme/divide-two-numbers/divide-two-numbers.cpp
+++ programme/divide-two-numbers/divide-two-numbers.cpp
@@ -1,27 +1,33 @@
+// Write a program to divide two numbers
+
 #include <iostream>
 using namespace std;
 
 double Divide(int num1, int num2)
 {
-    if(num2==0)
+    if (num2 == 0)
     {
         return -1;
     }
-    double result = (double)num1/(double)num2;
+    double result = (double)num1 / (double)num2;
     return result;
 }
 
-int main() {
+int main()
+{
     int num1, num2;
-    cin>>num1>>num2;
-    double result = Divide(num1,num2);
-    if(result==-1)
+    cout << "Input the Dividend : ";
+    cin >> num1;
+    cout << "Input the Divisor  : ";
+    cin >> num2;
+    double result = Divide(num1, num2);
+    if (result == -1)
     {
-        cout<<"Divide by Zero is not possible";
+        cout << "\n!!!  Divide by Zero is not possible  !!!\n";
     }
     else
     {
-        cout<<result;
+        cout << "\nOutput : " << result << endl;
     }
 
     return 0;
