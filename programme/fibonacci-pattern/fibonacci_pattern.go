33c8149c8eea60b5354f697ed2f65d9354222833
Paula Boyano <paulaboyanoivars@gmail.com>
2022-05-20 12:42:32 +0200

    fibonacci pattern in go

---
+++ programme/fibonacci-pattern/fibonacci_pattern.go
@@ -0,0 +1,39 @@
+/* program to print Fibonacci pattern using recursion.
+
+Example:
+Input: n = 5
+Output:
+1
+1 1
+1 1 2
+1 1 2 3
+1 1 2 3 5
+*/
+
+package main
+
+import (
+	"fmt"
+	"strconv"
+  )
+
+
+func fibonacci(n int) int {
+    if n <= 1 {
+        return n
+    }
+    return fibonacci(n-1) + fibonacci(n-2)
+}
+
+func main() {
+	var n int
+	fmt.Print("Enter an integer : ")
+	fmt.Scan(&n)
+
+	for i := 1; i <= n; i++ {
+		for j := 1; j <= i; j++ {
+			fmt.Print(strconv.Itoa(fibonacci(j)) + " ");
+		}
+		fmt.Println();
+	}
+}
