c69c5e4a2759c87762e84c8600c70727d3bc23bf
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-05-22 14:23:34 +0530

    Update area-of-circle.c
    Former-commit-id: 2c90a89f4056e69d8936d198960a508d1c995b20

--- programme/area-of-circle/area-of-circle.c
+++ programme/area-of-circle/area-of-circle.c
@@ -1,13 +1,16 @@
+// Write a programme to find the area of circle
+
 #include <stdio.h>
 
 #define PI 3.141f
 
-int main(void) {
+int main(void)
+{
     float r;
-    printf("Enter r: ");
+    printf("Enter r : ");
     scanf("%f", &r);
 
     float area = PI * r * r;
 
-    printf("Area: %.2f\n", area);
+    printf("\nArea : %.2f\n", area);
 }
