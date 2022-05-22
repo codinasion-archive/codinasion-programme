cceb10a6f36e2dea7308ebfb7e9089a8171c47f5
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-05-22 14:10:47 +0530

    Update add-two-numbers.c

--- programme/add-two-numbers/add-two-numbers.c
+++ programme/add-two-numbers/add-two-numbers.c
@@ -1,12 +1,15 @@
+// Write a program to add two numbers
+
 #include <stdio.h>
 
-int main(void) {
+int main(void)
+{
     int a, b;
-    printf("Enter two numbers: ");
+    printf("Enter two numbers : ");
     scanf("%d %d", &a, &b);
 
     int sum = a + b;
 
-    printf("%d + %d = %d\n", a, b, sum);
+    printf("\n%d + %d = %d\n", a, b, sum);
     return 0;
 }
