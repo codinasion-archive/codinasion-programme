a3ac44ad1e2cfb4de1b0ce1ad1a229af664b1ccd
Dentyr <74077896+Dentyr@users.noreply.github.com>
2022-05-22 10:16:32 -0700

    Create AlphabetTrianglePattern.cs
    Former-commit-id: dd4802574fe1447b3a096d5f479ce1d4cbda26ed

---
+++ programme/alphabet-triangle-pattern/AlphabetTrianglePattern.cs
@@ -0,0 +1,29 @@
+// Write a program to print alphabet triangle pattern
+using System;
+
+public class AlphabetTrianglePattern 
+{
+    private static void pattern(int n) {
+        Console.WriteLine("Output :");
+        for (int i = 1; i <= n; i++) {
+            int c = (int)('A');
+            for (int k = n; k > i; k--) {
+                Console.Write(" ");
+            }
+            for (int j = 1; j < 2 * i; j++) {
+                Console.Write(((char)c));
+                if (j < i) {
+                    c++;
+                } else {
+                    c--;
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+    public static void Main(String[] args) {
+        Console.WriteLine("Input  : ");
+        int n = int.Parse(Console.ReadLine());
+        pattern(n);
+    }
+}
