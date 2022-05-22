23f0c4f42d0793a5e07eb1b824147953bb81f4e8
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-05-22 14:26:59 +0530

    Update multiply.c
    Former-commit-id: 49768cb39e2950e97a8c2d8b8234ded07f8d1180

--- programme/multiply-two-numbers/multiply.c
+++ programme/multiply-two-numbers/multiply.c
@@ -1,12 +1,15 @@
+// Write a programme to multiply two numbers
+
 #include <stdio.h>
 
-int main(void) {
+int main(void)
+{
     int a, b;
-    printf("Enter two numbers: ");
+    printf("Enter two numbers : ");
     scanf("%d %d", &a, &b);
 
     int product = a * b;
 
-    printf("%d * %d = %d\n", a, b, product);
+    printf("\n%d * %d = %d\n", a, b, product);
     return 0;
 }
