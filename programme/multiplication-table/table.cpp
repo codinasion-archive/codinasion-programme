a195b0462e3fa896cb4c1e2db847875922e05851
Enoch <adesanyaenoch@gmail.com>
2022-05-22 09:29:50 +0100

    program to find multiplicatiom table
    Former-commit-id: 7467d3a7b6c4e51985e2cd545fadba7279e18528

---
+++ programme/multiplication-table/table.cpp
@@ -0,0 +1,13 @@
+// Write a program to output the following multiplication table
+#include <iostream>
+
+int main()
+{
+    int n;
+    std::cout << "Enter number to find its table: ";
+    std::cin >> n;
+    for (int i = 1; i < 11; i++)
+    {
+        std::cout << n << " x " << i << " = " << n * i << "\n";
+    }
+}
