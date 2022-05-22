e4d9dee0baef9eec20069e69f842854d56ae483f
Prajwal Borkar <48290911+PrajwalBorkar@users.noreply.github.com>
2022-05-22 12:43:52 +0530

    Added C++ program
    Former-commit-id: 865e9fe595128688abcedca29ab5e09ecef22119

---
+++ programme/multiplication-table/table.cpp
@@ -0,0 +1,12 @@
+# Write a C++ program to output the multiplication table
+
+#include <iostream>
+
+using namespace std;
+int main() {
+   int n = 7, i;
+   cout<<"The multiplication table for "<< n <<" is as follows:"<< endl;
+   for (i = 1; i <= 10; i++)
+   cout << n << " * " << i << " = " << n * i << endl;
+   return 0;
+}
