e7bbd75af60b244a1ed8c4911eb3f7708f899a7d
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-05-22 14:18:33 +0530

    Update factorial.cpp
    Former-commit-id: e6a6fa2526770cb1faca14acd36d53f6060daa21

--- programme/factorial/factorial.cpp
+++ programme/factorial/factorial.cpp
@@ -1,15 +1,18 @@
+// Write a program to find the factorial of a number
+
 #include <iostream>
 using namespace std;
 
 int main()
 {
     int number;
-    cin>>number;
+    cout << "Input : ";
+    cin >> number;
     int factorial_of_number = 1;
-    for(int i=1;i<=number;i++)
+    for (int i = 1; i <= number; i++)
     {
-        factorial_of_number = factorial_of_number*i;
+        factorial_of_number = factorial_of_number * i;
     }
-    cout<<"Factorial of "<<number<<" is "<<factorial_of_number;
+    cout << "\nFactorial of " << number << " is " << factorial_of_number << endl;
     return 0;
 }
