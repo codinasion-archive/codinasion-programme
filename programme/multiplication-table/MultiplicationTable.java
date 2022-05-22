d1a38016ccce12b83241071aceb1ff0366006705
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-05-22 14:33:21 +0530

    Update and rename table.java to MultiplicationTable.java

--- programme/multiplication-table/MultiplicationTable.java
+++ programme/multiplication-table/MultiplicationTable.java
@@ -1,12 +1,11 @@
-# Write a program to output the following multiplication table
+// Write a program to output the following multiplication table
 
 public class MultiplicationTable {
 
     public static void main(String[] args) {
 
         int num = 5;
-        for(int i = 1; i <= 10; ++i)
-        {
+        for (int i = 1; i <= 10; ++i) {
             System.out.printf("%d * %d = %d \n", num, i, num * i);
         }
     }
