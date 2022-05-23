e7dc573602ae586929fb0863ee29724efae5ed63
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-05-23 15:36:36 +0530

    Update MultiplicationTable.cs

--- programme/multiplication-table/MultiplicationTable.cs
+++ programme/multiplication-table/MultiplicationTable.cs
@@ -1,11 +1,15 @@
+// Write a program to output the multiplication table
+
 using System;
 public class MultiplicationTable
 {
-    public static void Main(String[] args) {
+    public static void Main(String[] args)
+    {
 
-        Console.WriteLine("Enter Number:");
+        Console.Write("Enter Number : ");
         int num = int.Parse(Console.ReadLine());
-        for (int i = 1; i < 11; i++) {
+        for (int i = 1; i < 11; i++)
+        {
             Console.WriteLine("{0} * {1} = {2}", num, i, num * i);
         }
     }
