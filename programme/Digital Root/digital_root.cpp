2e47c7822b296b163a0f61212f03ad2c81d2afb7
arth2002 <kingap12ka4@gmail.com>
2022-05-24 00:20:16 +0530

    Digital Root Added

---
+++ programme/Digital Root/digital_root.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int digital_root(int n){
+    if (n==0)return 0;
+    else{
+        return 1 + ((n-1)%9);
+    }
+}
+int main() {
+    int n;
+    cout << "Enter Number: ";
+    cin >> n;
+    cout <<endl;
+    cout << "Digital Root Value :"  << digital_root(n);
+    return 0;
+}
