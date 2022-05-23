7ff038661c6075ab7b8792c3c48f134fee7196b8
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-05-23 15:51:41 +0530

    Update solution.c
    Former-commit-id: 8b2ba5e1cda65111ea1c438630c89a1ea2e8f2a9

--- programme/swap-two-numbers/solution.c
+++ programme/swap-two-numbers/solution.c
@@ -1,14 +1,16 @@
+// Write a program to swap two numbers
+
 #include <stdio.h>
 int main()
 {
     int a, b;
-    printf("Enter the value of a: ");
+    printf("Enter the value of a : ");
     scanf("%d", &a);
-    printf("Enter the value of b: ");
+    printf("Enter the value of b : ");
     scanf("%d", &b);
     int temp = a;
     a = b;
     b = temp;
-    printf("Output: a = %d , b = %d", a, b);
+    printf("\nOutput: a = %d , b = %d\n", a, b);
     return 0;
 }
