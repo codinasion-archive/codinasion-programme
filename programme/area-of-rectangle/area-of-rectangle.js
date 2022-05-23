0e6b5311be3244a8ef2679a9655402cb3ca22bb8
Jon Christie <jonpchristie@gmail.com>
2022-05-23 10:04:42 -0400

    Create area-of-rectangle.js

---
+++ programme/area-of-rectangle/area-of-rectangle.js
@@ -0,0 +1,7 @@
+// Write a program to find the area of Rectangular given the length and the breadth.
+
+const rectangleArea = (length, breadth) => {
+  let area = length*breadth
+  
+  return area
+}
