4a3b8e68769afdbbf05ce50dd86eaeba471e718b
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-05-24 23:02:01 +0530

    Update sum-of-digits.cs
    Former-commit-id: 425379ab8d7e4c066fe32d9aa57d9eb005eb0799

--- programme/sum-of-digits/sum-of-digits.cs
+++ programme/sum-of-digits/sum-of-digits.cs
@@ -1,19 +1,19 @@
 // Write a program to print sum of digits.
 
-using System;  
-  public class SumExample  
-   {  
-     public static void Main(string[] args)  
-      {  
-       int  n,sum=0,m;         
-       Console.Write("Enter a number: ");      
-       n= int.Parse(Console.ReadLine());     
-       while(n>0)      
-       {      
-        m=n%10;      
-        sum=sum+m;      
-        n=n/10;      
-       }      
-       Console.Write("Sum is= "+sum);       
-     }  
-  }  
+using System;
+public class SumExample
+{
+    public static void Main(string[] args)
+    {
+        int n, sum = 0, m;
+        Console.Write("Enter a number : ");
+        n = int.Parse(Console.ReadLine());
+        while (n > 0)
+        {
+            m = n % 10;
+            sum = sum + m;
+            n = n / 10;
+        }
+        Console.WriteLine("\nSum : " + sum);
+    }
+}
