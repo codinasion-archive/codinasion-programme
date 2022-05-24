68c1a8cd04dcc8aa4a08ccf9accbf868d62f1fcf
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-05-24 23:08:44 +0530

    Update fibonacci_pattern.py
    Former-commit-id: 6f1bbf86b0ec460e35c7699b21f8cd667855e788

--- programme/fibonacci-pattern/fibonacci_pattern.py
+++ programme/fibonacci-pattern/fibonacci_pattern.py
@@ -1,3 +1,5 @@
+# Write a program to print Fibonacci pattern
+
 def fibonacci_pattern(n: int) -> int:
     assert n > 0
     a, b = 0, 1
@@ -9,6 +11,6 @@ def fibonacci_pattern(n: int) -> int:
 
 if __name__ == "__main__":
     pattern_lenght = int(input(
-        "Enter the lenght of the Fibonacci pattern to generate: "))
+        "Enter the lenght of the Fibonacci pattern to generate : "))
     for number in range(1, pattern_lenght + 1):
         fibonacci_pattern(number)
