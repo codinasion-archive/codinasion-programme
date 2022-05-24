396196e416b48a58fa4c7f31c2b4ec46ce26387a
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-05-24 23:27:45 +0530

    Update and rename minutes-to-seconds.c++ to minutes-to-seconds.cpp

---
+++ programme/minutes-to-seconds/minutes-to-seconds.cpp
@@ -0,0 +1,18 @@
+/*
+ * program to convert minutes to seconds
+ * @author: Siddhant Chimankar
+ */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int n;
+    cout << "Enter Minutes : ";
+    cin >> n;
+    cout << "\nSeconds : " << n * 60 << "\n";
+
+    return 0;
+}
