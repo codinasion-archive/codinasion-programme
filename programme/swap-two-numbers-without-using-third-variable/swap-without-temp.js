ac2fcfda0477dd130e4403974b770031abe21303
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-05-24 23:13:53 +0530

    Update swap-without-temp.js
    Former-commit-id: 049660c91002f19373e98e7c75a94394e1aa673d

--- programme/swap-two-numbers-without-using-third-variable/swap-without-temp.js
+++ programme/swap-two-numbers-without-using-third-variable/swap-without-temp.js
@@ -1,8 +1,10 @@
+// Write a program to swap two numbers without using third variable
+
 let a = 10;
 let b = 20;
 
-console.log(`Input:  a = ${a}, b = ${b}`);
+console.log(`Input  : a = ${a}, b = ${b}`);
 
 [a, b] = [b, a];
 
-console.log(`Output: a = ${a}, b = ${b}`);
+console.log(`Output : a = ${a}, b = ${b}`);
