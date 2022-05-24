510eb815a3198de0670275d524fdbd68b4128b72
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-05-24 22:51:06 +0530

    Update area-of-rectangle.js

--- programme/area-of-rectangle/area-of-rectangle.js
+++ programme/area-of-rectangle/area-of-rectangle.js
@@ -1,7 +1,11 @@
 // Write a program to find the area of Rectangular given the length and the breadth.
 
 const rectangleArea = (length, breadth) => {
-  let area = length*breadth
-  
-  return area
-}
+  let area = length * breadth;
+
+  return area;
+};
+
+console.log("length  : 2");
+console.log("breadth : 3");
+console.log("\narea : ", rectangleArea(2, 3));
