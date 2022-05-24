b1dd5a2d8f7a80dadc3320228c2406c151dabe12
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-05-25 00:09:28 +0530

    Update digital_root.cpp

--- programme/digital-root/digital_root.cpp
+++ programme/digital-root/digital_root.cpp
@@ -1,16 +1,24 @@
+// Write a programme for Digital Root of a number
+
 #include <iostream>
 using namespace std;
-int digital_root(int n){
-    if (n==0)return 0;
-    else{
-        return 1 + ((n-1)%9);
+
+int digital_root(int n)
+{
+    if (n == 0)
+        return 0;
+    else
+    {
+        return 1 + ((n - 1) % 9);
     }
 }
-int main() {
+
+int main()
+{
     int n;
-    cout << "Enter Number: ";
+    cout << "Enter Number : ";
     cin >> n;
-    cout <<endl;
-    cout << "Digital Root Value :"  << digital_root(n);
+    cout << endl;
+    cout << "Digital Root Value : " << digital_root(n) << endl;
     return 0;
 }
