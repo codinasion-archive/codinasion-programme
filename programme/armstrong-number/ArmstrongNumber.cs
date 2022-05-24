4c7d5a2c4d640cbe7d921413c9c47d63fbb9634b
Dentyr <74077896+Dentyr@users.noreply.github.com>
2022-05-23 18:10:35 -0700

    Create ArmstrongNumber.cs
    Former-commit-id: a376ddf0086196e2e489b88014662e18a7d5c28d

---
+++ programme/armstrong-number/ArmstrongNumber.cs
@@ -0,0 +1,27 @@
+using System;
+public class ArmstrongNumber
+{
+    public static bool IsArmstrongNumber(int num)
+    {
+        int sum = 0; 
+        for (int temp = num; temp > 0; temp /= 10)
+        {
+            sum += ((int)Math.Pow(temp % 10, num.size));
+            Console.Out.WriteLine(temp + "  " + sum);
+        }
+        if (num == sum) return true; return false;
+    }
+    public static int numDigits(int num)
+    {
+        if (num == 0) return 1;
+        return Math.Floor(Math.Log10(num)) + 1;
+    }
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Input  : ");
+        Console.WriteLine(
+            IsArmstrongNumber(int.Parse(Console.ReadLine())) ?
+            "Is an Armstrong number" : "Not an Armstrong number"
+        );
+    }
+}
