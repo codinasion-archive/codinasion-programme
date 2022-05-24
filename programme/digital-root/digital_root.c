3790236af80c3f4900affe2428af197dc169044b
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-05-25 00:09:26 +0530

    Update digital_root.c
    Former-commit-id: 8dd3d07b27dda39afeea1e2152d6afd0917d6397

--- programme/digital-root/digital_root.c
+++ programme/digital-root/digital_root.c
@@ -1,17 +1,24 @@
+// Write a programme for Digital Root of a number
+
 #include <stdio.h>
-int digital_root(int n){
-    if (n==0)return 0;
-    else{
-        return 1 + ((n-1)%9);
+
+int digital_root(int n)
+{
+    if (n == 0)
+        return 0;
+    else
+    {
+        return 1 + ((n - 1) % 9);
     }
 }
-int main() {
+
+int main()
+{
     int n;
-    printf("Enter Number: ");
-    scanf("%d",&n);
+    printf("Enter Number : ");
+    scanf("%d", &n);
     int ans = digital_root(n);
     printf("\n");
-    printf("Digital Root Value : %d",ans);
-    printf("\n");
+    printf("Digital Root Value : %d\n", ans);
     return 0;
 }
