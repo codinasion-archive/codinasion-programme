ba05c52d138b3e72ce20e2617b43a502a07d860a
Jon Rutter <contact@jonrutter.io>
2022-05-25 12:57:26 -0400

    Implement check-prime in JavaScript

---
+++ programme/check-prime-number/check-prime.js
@@ -0,0 +1,29 @@
+/*
+Write a JavaScript program to check if a number is a prime
+
+Example:
+Input: n = 8
+Output: Not a prime number.
+
+Example:
+Input: n = 7
+Output: Prime number
+
+*/
+
+const isPrime = (n) => {
+  // edge cases: 1 and 0 are not prime
+  if (n <= 1) return false;
+  // n is a prime if n is not evenly divisible by any integer from 2 to the square root of n
+  for (let i = 2; i <= Math.sqrt(n); i++) {
+    if (n % i === 0) return false;
+  }
+  return true;
+};
+
+const printIsPrime = (n) => {
+  console.log(isPrime(n) ? 'Prime number' : 'Not a prime number');
+};
+
+printIsPrime(8);
+printIsPrime(7);
