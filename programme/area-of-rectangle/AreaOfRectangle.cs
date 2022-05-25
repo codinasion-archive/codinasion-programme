e84ce4fc59bd5870b2f2c310c813ea8e9bdf6aa0
Dentyr <74077896+Dentyr@users.noreply.github.com>
2022-05-25 16:22:38 -0700

    Update AreaOfRectangle.cs
    Former-commit-id: da4ef04cc0a51a0bd967533feca10d48dcc2f4aa

--- programme/area-of-rectangle/AreaOfRectangle.cs
+++ programme/area-of-rectangle/AreaOfRectangle.cs
@@ -3,10 +3,11 @@ public class AreaOfRectangle
 {
     public static void Main(String[] args)
     {
+        int length, width;
         Console.WriteLine("Enter Length:");
-        int length = int.Parse(Console.ReadLine());
+        length = int.Parse(Console.ReadLine());
         Console.WriteLine("Enter Width");
-        int width = int.Parse(Console.ReadLine());
+        width = int.Parse(Console.ReadLine());
         Console.WriteLine("The area is {0}", length * width);
     }
 }
