317dd502cb3fa7ef86e84e592f20c5a5d1142933
Lorenzo Rossi <rossilor95@outlook.it>
2022-05-26 12:09:43 +0200

    Added armstrongnumber.go
    Former-commit-id: f68d4c307e26bc25e59be82cb0c0f4d6c8c812de

---
+++ programme/armstrong-number/armstrongnumber.go
@@ -0,0 +1,54 @@
+// Write a program to check armstrong number
+
+package main
+
+import "fmt"
+
+func countDigits(number int) int {
+	var numberOfDigits int = 0
+	for number > 0 {
+		number /= 10
+		numberOfDigits++
+	}
+	return numberOfDigits
+}
+
+// math.Pow only accepts float64 inputs and returns a float64 number
+// Therefore, we write our own Integer Power function
+func intPow(base, exponent int) int {
+	if exponent == 0 {
+		return 1
+	}
+	var result int = base
+	for i := 2; i <= exponent; i++ {
+		result *= base
+	}
+	return result
+}
+
+func requiredSum(number int) int {
+	var numberOfDigits int = countDigits(number)
+	var sum int = 0
+	for number > 0 {
+		var digit int = number % 10
+		number /= 10
+		sum += intPow(digit, numberOfDigits)
+	}
+	return sum
+}
+
+func isArmstrong(number int) bool {
+	return number == requiredSum(number)
+}
+
+func main() {
+	var num int
+	fmt.Println("Enter an integer: ")
+	fmt.Scan(&num)
+	fmt.Println("Input: ", num)
+	if isArmstrong(num) {
+		fmt.Println("Output: armstrong number")
+	} else {
+		fmt.Println("Output: not an armstrong number")
+	}
+}
