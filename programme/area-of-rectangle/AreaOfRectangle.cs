8627ecbf7d72d68f358846252fe8072e2df56af7
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-05-29 10:48:16 +0530

    Update AreaOfRectangle.cs
    Former-commit-id: 0c313fc4f59a540c6aa18038a82232c92f9f881d

--- programme/area-of-rectangle/AreaOfRectangle.cs
+++ programme/area-of-rectangle/AreaOfRectangle.cs
@@ -1,13 +1,16 @@
+// Write a program to compute the area of a rectangle
 using System;
+
 public class AreaOfRectangle
 {
     public static void Main(String[] args)
     {
-        int length, width;
-        Console.WriteLine("Enter Length:");
+        int length;
+        int width;
+        Console.Write("Enter Length : ");
         length = int.Parse(Console.ReadLine());
-        Console.WriteLine("Enter Width");
+        Console.Write("Enter Width  : ");
         width = int.Parse(Console.ReadLine());
-        Console.WriteLine("The area is {0}", length * width);
+        Console.WriteLine("\nThe area is {0}", length * width);
     }
 }
