c7bd6d73be1a8bb8d46ce537ff82777c3dd6726d
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-05-29 10:00:16 +0530

    Update digital_root.java
    Former-commit-id: 61fc1e09d616306257f8f156ea89af066f62da0b

--- programme/digital-root/digital_root.java
+++ programme/digital-root/digital_root.java
@@ -1,13 +1,14 @@
-//Converts a number to its digital root
-//The digital root of a number is defined as the sum of each digit defining the original number
-//example 127 -> 1 + 2 + 7 -> 10
-//        10 -> 1 + 0 -> 1
+// Converts a number to its digital root
+// The digital root of a number is defined as the sum of each digit defining the original number
+// example 127 -> 1 + 2 + 7 -> 10
+//          10 -> 1 + 0 -> 1
+
 import java.util.Scanner;
 
 public class digital_root {
     public static void convert(int a) {
-        //Ramans formula for calcuating the digital root
-        System.out.println(1+(a-1)%9);
+        // Ramans formula for calcuating the digital root
+        System.out.println(1 + (a - 1) % 9);
     }
 
     public static void main(String[] args) {
@@ -15,9 +16,8 @@ public class digital_root {
         Scanner sc = new Scanner(System.in);
         System.out.print("Input  : ");
         a = sc.nextInt();
-        System.out.print("The digital root is : ");
+        System.out.print("\nThe digital root is : ");
         convert(a);
-        System.out.println("");
         sc.close();
     }
 }
