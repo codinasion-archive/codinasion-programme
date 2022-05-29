162207c07e36d9d3527c2d17680b8c5cef60b12d
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-05-29 12:52:14 +0530

    Update euler_numbers.c

--- programme/euler-number/euler_numbers.c
+++ programme/euler-number/euler_numbers.c
@@ -1,13 +1,17 @@
+// Write a programme to compute the value of Euler's Number
+
 #include <stdio.h>
 
-int main(){
+int main()
+{
     int N;
-    printf("Enter N: ");
+    printf("Enter N : ");
     scanf("%d", &N);
     float eNumber = 1, fact = 1;
-    for(int i=1; i<=N; i++){
+    for (int i = 1; i <= N; i++)
+    {
         fact = fact * i;
         eNumber += (1.0 / fact);
     }
-    printf("Euler Number of %d: %f\n", N, eNumber);
+    printf("Euler Number of %d : %f\n", N, eNumber);
 }
