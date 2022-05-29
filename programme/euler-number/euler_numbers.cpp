8c40fc2f020c0c59dd744d7b3a91195b554fdadd
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-05-29 12:52:16 +0530

    Update euler_numbers.cpp
    Former-commit-id: 84037ee0371eaa7426196aaf7f9a494659236d26

--- programme/euler-number/euler_numbers.cpp
+++ programme/euler-number/euler_numbers.cpp
@@ -1,15 +1,19 @@
+// Write a programme to compute the value of Euler's Number
+
 #include <iostream>
 
 using namespace std;
 
-int main(){
+int main()
+{
     int N;
-    cout <<"Enter N: ";
+    cout << "Enter N : ";
     cin >> N;
     float eNumber = 1, fact = 1;
-    for(int i=1; i<=N; i++){
+    for (int i = 1; i <= N; i++)
+    {
         fact = fact * i;
         eNumber += (1.0 / fact);
     }
-    cout <<"Euler Number of "<<N<<": "<<eNumber<<endl;
+    cout << "Euler Number of " << N << " : " << eNumber << endl;
 }
