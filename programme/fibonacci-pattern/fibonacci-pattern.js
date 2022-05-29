22fa305706678fc8d58b145b7ea7dc9b4697d758
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-05-29 10:19:40 +0530

    Update fibonacci-pattern.js

--- programme/fibonacci-pattern/fibonacci-pattern.js
+++ programme/fibonacci-pattern/fibonacci-pattern.js
@@ -1,24 +1,28 @@
 // Write a program to print Fibonacci pattern
 
-const fibonacciPattern = function (n) {
-    let a = 0;
-    let b = 1;
-    for (let i = 0; i < n; i++) {
-        process.stdout.write(`${b} `);
-        [a, b] = [b, a + b];
-    }
-    process.stdout.write("\n");
-}
-
 const readline = require("readline");
+
+const fibonacciPattern = function (n) {
+  let a = 0;
+  let b = 1;
+  for (let i = 0; i < n; i++) {
+    process.stdout.write(`${b} `);
+    [a, b] = [b, a + b];
+  }
+  process.stdout.write("\n");
+};
+
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+  input: process.stdin,
+  output: process.stdout,
 });
 
-rl.question("Enter the lenght of the Fibonacci pattern to generate : ", (patternLength) => {
+rl.question(
+  "Enter the lenght of the Fibonacci pattern to generate : ",
+  (patternLength) => {
     for (let number = 1; number <= patternLength; number++) {
-        fibonacciPattern(number)
+      fibonacciPattern(number);
     }
     rl.close();
-});
+  }
+);
