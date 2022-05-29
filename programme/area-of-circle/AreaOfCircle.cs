fc8ee3ec2bf705b659bce4023b194321d6f645fe
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-05-29 10:44:22 +0530

    Update AreaOfCircle.cs
    Former-commit-id: 33afe56d701fa6e040556c615f50b98a8051fcff

--- programme/area-of-circle/AreaOfCircle.cs
+++ programme/area-of-circle/AreaOfCircle.cs
@@ -1,12 +1,15 @@
+// Write a programme to find the area of circle
 using System;
-public class AreaOfCircle 
-{
-	public static void Main(String[] args) {
-		Console.WriteLine("Enter the radius : ");
-		int r = int.Parse(Console.ReadLine());
-		double area = Math.PI * r * r;
-		double answer = Math.Round(area, 2);
 
-		Console.WriteLine("Area of circle : " + answer);
-	}
+public class AreaOfCircle
+{
+    public static void Main(String[] args)
+    {
+        Console.Write("Enter the radius : ");
+        int r = int.Parse(Console.ReadLine());
+        double area = Math.PI * r * r;
+        double answer = Math.Round(area, 2);
+
+        Console.WriteLine("Area of circle   : " + answer);
+    }
 }
