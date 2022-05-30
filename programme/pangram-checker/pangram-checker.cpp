b95d1f7fc258b7bc20b99d2403fc3b6708bb4b2a
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-05-30 08:24:05 +0530

    Update pangram-checker.cpp
    Former-commit-id: 59894fff97ac4f5122c601568dceaa2e04c4260c

--- programme/pangram-checker/pangram-checker.cpp
+++ programme/pangram-checker/pangram-checker.cpp
@@ -1,33 +1,66 @@
+// Write a programme to check Pangram sentence
+
 #include <iostream>
 
 using namespace std;
 
 int main()
 {
-    char str[100];                              // Taking an array of character of max size 100
-    scanf("%[^\n]s",str);                       // Scanning the input line
-    char isPangram;                             // isPangram is a variable that store if a sentence is Pangram or not
-    for(char i='a'; i<='z'; i++){               // As per description, we are checking if a-z is present or not
-        int j;                                  // Iterating variable for given string
-        for(j=0; str[j]!='\0'; j++){            // Iterating through given string
-            if(str[j]>='A' && str[j]<='Z'){     // Checking the character in the given sentence in Uppercase (A-Z) or not
-                str[j] += 32;                   // Change the character into Lowercase (a-z) if it is Uppercase (A-Z)
+    // Taking an array of character of max size 100
+    char str[100];
+
+    // Scanning the input line
+    cout << "Input  : ";
+    scanf("%[^\n]s", str);
+
+    // isPangram is a variable that store if a sentence is Pangram or not
+    char isPangram;
+
+    // As per description, we are checking if a-z is present or not
+    for (char i = 'a'; i <= 'z'; i++)
+    {
+        // Iterating variable for given string
+        int j;
+
+        // Iterating through given string
+        for (j = 0; str[j] != '\0'; j++)
+        {
+
+            // Checking the character in the given sentence in Uppercase (A-Z) or not
+            if (str[j] >= 'A' && str[j] <= 'Z')
+            {
+
+                // Change the character into Lowercase (a-z) if it is Uppercase (A-Z)
+                str[j] += 32;
             }
-            if(str[j] == i){                    // We found ith alphabet
-                break;                          // Breaking from the loop as we 
-            }                                   // do not have to check more after one alphabet at least once
+
+            // We found ith alphabet
+            if (str[j] == i)
+            {
+                // Breaking from the loop as we
+                break;
+            }
+            // do not have to check more after one alphabet at least once
         }
-        if(str[j]=='\0') {                      // If we reach to the end of the string and still we do not locate an
-            isPangram = 'n';                    // alphabet in the string, then the sentence will not be a pangram
-            break;                              // As we are sure it is not a pangram sentence, then just getting out from the loop
+        // If we reach to the end of the string and still we do not locate an
+        if (str[j] == '\0')
+        {
+            // alphabet in the string, then the sentence will not be a pangram
+            isPangram = 'n';
+
+            // As we are sure it is not a pangram sentence, then just getting out from the loop
+            break;
         }
     }
-    
-    if(isPangram == 'n'){                      
-        cout<< "Not a pangram sentence\n";
+
+    cout << "Output : ";
+    if (isPangram == 'n')
+    {
+        cout << "Not a pangram sentence\n";
     }
-    else{
-        cout<< "Pangram Sentence\n";
+    else
+    {
+        cout << "Pangram Sentence\n";
     }
 
     return 0;
