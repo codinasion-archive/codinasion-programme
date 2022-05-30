ef6686ed6ae96d4046f289c44e5914487f9db7bd
Lorenzo Rossi <rossilor95@outlook.it>
2022-05-30 17:03:47 +0200

    Added area of circle, rectangle, triangle and right triangle

---
+++ programme/area-of-triangle/areaoftriangle.go
@@ -0,0 +1,36 @@
+// Write a programme to find area of triangle.
+
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func areaOfTriangle(a, b, c, s float64) float64 {
+	return math.Sqrt(s * (s - a) * (s - b) * (s - c))
+}
+
+func roundFloat(val float64, precision uint) float64 {
+	var ratio float64 = math.Pow(10, float64(precision))
+	return math.Round(val*ratio) / ratio
+}
+
+func main() {
+	var a, b, c, s, area float64
+
+	fmt.Print("a : ")
+	fmt.Scan(&a)
+
+	fmt.Print("b : ")
+	fmt.Scan(&b)
+
+	fmt.Print("c : ")
+	fmt.Scan(&c)
+
+	s = (a + b + c) / 2
+	fmt.Print("\ns : ", s)
+
+	area = areaOfTriangle(a, b, c, s)
+	fmt.Print("\n\narea : ", roundFloat(area, 2))
+}
