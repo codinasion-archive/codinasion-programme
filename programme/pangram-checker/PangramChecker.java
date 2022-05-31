5e045a70f1197804d42765a98619e3de73f6595d
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-05-31 18:27:05 +0530

    Update PangramChecker.java
    Former-commit-id: 47127dfd69f8f43e7f4a6cb21f97298cabb920c8

--- programme/pangram-checker/PangramChecker.java
+++ programme/pangram-checker/PangramChecker.java
@@ -1,4 +1,4 @@
-package pangram;
+// Write a programme to check Pangram sentence
 
 import java.util.HashMap;
 import java.util.Scanner;
@@ -10,27 +10,30 @@ public class PangramChecker {
 		HashMap<Character, Boolean> letters = new HashMap<Character, Boolean>();
 		boolean pangram = true;
 		Scanner sc = new Scanner(System.in);
-		
-		//read input
-        System.out.print("\nInput: ");
-        String input = sc.nextLine();
-        
-        //convert string to lower case
-        input = input.toLowerCase();
-		
-        //iterate for each character in input
-		for(int i=0; i<input.length(); i++) {
+
+		// read input
+		System.out.print("Input  : ");
+		String input = sc.nextLine();
+
+		// convert string to lower case
+		input = input.toLowerCase();
+
+		// iterate for each character in input
+		for (int i = 0; i < input.length(); i++) {
 			letters.put(input.charAt(i), true);
 		}
-		
-		//iterate to check if every alphabet character is in the hashmap
-		for(int i=0; i<alphabet.length(); i++) {
-			//if an alphabet character is missing then set pangam to false
-			if(letters.get(alphabet.charAt(i)) == null) {
+
+		// iterate to check if every alphabet character is in the hashmap
+		for (int i = 0; i < alphabet.length(); i++) {
+			// if an alphabet character is missing then set pangam to false
+			if (letters.get(alphabet.charAt(i)) == null) {
 				pangram = false;
 			}
 		}
-		
-		System.out.println(pangram);
+
+		System.out.print("Output : ");
+		System.out.println(pangram ? "Pangram Sentence" : "Not a pangram sentence");
+
+		sc.close();
 	}
 }
