78a03de229eeaf708ffb1a0cbdbc4653f754b904
julyvjo <julyvjo@gmail.com>
2022-05-30 23:56:55 -0300

    Added pangram chekcer files for java, javascript and python

---
+++ programme/pangram-checker/PangramChecker.java
@@ -0,0 +1,36 @@
+package pangram;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class PangramChecker {
+
+	public static void main(String[] args) {
+		String alphabet = "abcdefghijklmnopqrstuvwxyz";
+		HashMap<Character, Boolean> letters = new HashMap<Character, Boolean>();
+		boolean pangram = true;
+		Scanner sc = new Scanner(System.in);
+		
+		//read input
+        System.out.print("\nInput: ");
+        String input = sc.nextLine();
+        
+        //convert string to lower case
+        input = input.toLowerCase();
+		
+        //iterate for each character of input
+		for(int i=0; i<input.length(); i++) {
+			letters.put(input.charAt(i), true);
+		}
+		
+		//iterate to check if every alphabet character is in the hashmap
+		for(int i=0; i<alphabet.length(); i++) {
+			//if an alphabet character is missing we set pangam to false
+			if(letters.get(alphabet.charAt(i)) == null) {
+				pangram = false;
+			}
+		}
+		
+		System.out.println(pangram);
+	}
+}
---
+++ programme/pangram-checker/pangram-checker.py
@@ -0,0 +1,29 @@
+
+def isPangram(_input):
+    alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    pangram = 1
+    letters = {}
+    #convert _input to lower case
+    _input = _input.lower()
+
+    #iterate for each character in input
+    for c in _input:
+        letters[c] = 1
+    
+    #iterate for each character in alphabet
+    #and checks if each one is in letters
+    for c in alphabet:
+        #if not in letters then pangram is set to 0 (false)
+        if c not in letters:
+            pangram = 0
+    
+    return pangram
+
+#ask for input
+print('input: ')
+_input = input()
+
+if isPangram(_input):
+    print('pangram!')
+else:
+    print('not pangram!')
