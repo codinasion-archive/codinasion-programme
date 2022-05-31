441203b3345b8a299b9018809a922a8acf4fc298
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-05-31 17:50:10 +0530

    Update matrix_multiplication.cpp
    Former-commit-id: 193b5e275d9780063b07efd68f92044617ec4def

--- programme/matrix-multiplication/matrix_multiplication.cpp
+++ programme/matrix-multiplication/matrix_multiplication.cpp
@@ -1,55 +1,68 @@
-#include <stdio.h>
+// Write a program to print multiplication of 2 matrices
+
+#include <iostream>
+
+using namespace std;
 
 int matrix1[10][10], matrix2[10][10], result[10][10];
- 
+
 int main()
 {
-    
-        int m, n, p, q;
-        
-        printf("Enter the number of rows and columns of first matrix\n");
-        scanf("%d%d", &m, &n);
-        printf("Enter Matrix1\n");
-        
-        for ( int  i = 0 ; i < m ; i++ ){
-            for ( int j = 0 ; j < n ; j++ ){
-                scanf("%d", &matrix1[i][j]);
-            }
-        }
 
-        printf("Enter the number of rows and columns of second matrix\n");
-        scanf("%d%d", &p, &q);
-        
-        if ( n != p ){
-          printf("No. of columns in Matrix1 is not equal to the No. of rows in Matrix2.\n");
-          return 0;
-        }
-        
-        printf("Enter Matrix2\n");
-        for ( int i = 0 ; i < p ; i++ ){
-            for ( int j = 0 ; j < q ; j++ ){
-                scanf("%d", &matrix2[i][j]);
-            }
-        }
-        
-        for ( int i = 0 ; i < m ; i++ ){
-            for ( int j = 0 ; j < q ; j++ ){
-              int sum = 0;
-              for ( int l = 0 ; l < p ; l++ ){
-                  sum = sum + matrix1[i][l]*matrix2[l][j];
-              }
-              result[i][j] = sum;
-            }
+    int m, n, p, q;
 
+    cout << "Enter the number of rows and columns of first matrix\n";
+    cin >> m >> n;
+    cout << "Enter Matrix1\n";
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cin >> matrix1[i][j];
         }
-     
-        printf("Product:-\n");
-        for ( int i = 0 ; i < m ; i++ ){
-            for ( int j = 0 ; j < q ; j++ ){
-                printf("%d\t", result[i][j]);
+    }
+
+    cout << "Enter the number of rows and columns of second matrix\n";
+    cin >> p >> q;
+
+    if (n != p)
+    {
+        cout << "No. of columns in Matrix1 is not equal to the No. of rows in Matrix2.\n";
+        return 0;
+    }
+
+    cout << "Enter Matrix2\n";
+    for (int i = 0; i < p; i++)
+    {
+        for (int j = 0; j < q; j++)
+        {
+            cin >> matrix2[i][j];
+        }
+    }
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < q; j++)
+        {
+            int sum = 0;
+            for (int l = 0; l < p; l++)
+            {
+                sum = sum + matrix1[i][l] * matrix2[l][j];
             }
-            printf("\n");
+            result[i][j] = sum;
         }
-     
-      return 0;
+    }
+
+    cout << "Product:-\n";
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < q; j++)
+        {
+            cout << result[i][j] << "\t";
+        }
+        cout << endl;
+    }
+
+    return 0;
 }
