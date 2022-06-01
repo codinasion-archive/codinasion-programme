1700ed49aa6497d990fea1961fc3c4ee10da3bf7
ssavi.ict <ssavi.ict@gmail.com>
2022-06-01 08:51:09 +0600

    c and cpp program of maximum of numbers added
    Former-commit-id: 9cf4807210848f3f2432459218980c9504e33b01

---
+++ programme/maximum-of-numbers/min.cpp
@@ -0,0 +1,30 @@
+// Write a program to print the minimum of given numbers.
+
+#include <iostream>
+#include <limits>
+
+int main()
+{
+  int max = std::numeric_limits<int>::min(); // Initializes as minimum value of int.
+  unsigned int count = 0;
+  std::cout << "Enter amount of numbers : ";
+  std::cin >> count;
+
+  while (count > 0)
+  {
+    int n = 0;
+    std::cout << "Enter number : ";
+    std::cin >> n;
+
+    if (n > max)
+    {
+      max = n;
+    }
+
+    --count;
+  }
+
+  std::cout << "\nMaximum : " << max << "\n";
+
+  return 0;
+}
