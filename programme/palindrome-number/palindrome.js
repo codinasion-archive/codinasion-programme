2f6ae991c89907f4b2c4df2816378cab123a7319
Lorenzo Rossi <rossilor95@outlook.it>
2022-06-01 18:26:20 +0200

    Added palindrome.js

---
+++ programme/palindrome-number/palindrome.js
@@ -0,0 +1,37 @@
+// Write a program to check palindrome number
+
+const reverseInt = function (n) {
+    let remainder, reverse = 0;
+    while (n > 0) {
+        remainder = n % 10;
+        reverse = reverse * 10 + remainder;
+        n = Math.floor(n / 10);
+    }
+    return reverse;
+}
+
+const isPalindrome = function (n) {
+    if (n < 0) {
+        return false;
+    }
+    if (n < 10) {
+        return true;
+    }
+    return reverseInt(n) == n;
+}
+
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+rl.question("Input  : ", (n) => {
+    if (isPalindrome(n)) {
+        console.log("Output : Palindrome number");
+        rl.close();
+    } else {
+        console.log("Output : Not palindrome number");
+        rl.close();
+    }
+});
