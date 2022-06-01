37c9419c803c9578040e50b722cd094b2f654a88
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-06-02 02:44:46 +0530

    Update palindrome.js
    Former-commit-id: b02d8d9b2a9a10944f23aeeaf6c5ee370a74e1d6

--- programme/palindrome-number/palindrome.js
+++ programme/palindrome-number/palindrome.js
@@ -1,37 +1,38 @@
 // Write a program to check palindrome number
 
 const reverseInt = function (n) {
-    let remainder, reverse = 0;
-    while (n > 0) {
-        remainder = n % 10;
-        reverse = reverse * 10 + remainder;
-        n = Math.floor(n / 10);
-    }
-    return reverse;
-}
+  let remainder,
+    reverse = 0;
+  while (n > 0) {
+    remainder = n % 10;
+    reverse = reverse * 10 + remainder;
+    n = Math.floor(n / 10);
+  }
+  return reverse;
+};
 
 const isPalindrome = function (n) {
-    if (n < 0) {
-        return false;
-    }
-    if (n < 10) {
-        return true;
-    }
-    return reverseInt(n) == n;
-}
+  if (n < 0) {
+    return false;
+  }
+  if (n < 10) {
+    return true;
+  }
+  return reverseInt(n) == n;
+};
 
-const readline = require('readline');
+const readline = require("readline");
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
+  input: process.stdin,
+  output: process.stdout,
 });
 
 rl.question("Input  : ", (n) => {
-    if (isPalindrome(n)) {
-        console.log("Output : Palindrome number");
-        rl.close();
-    } else {
-        console.log("Output : Not palindrome number");
-        rl.close();
-    }
+  if (isPalindrome(n)) {
+    console.log("Output : Palindrome number");
+    rl.close();
+  } else {
+    console.log("Output : Not palindrome number");
+    rl.close();
+  }
 });
