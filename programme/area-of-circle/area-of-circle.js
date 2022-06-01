fbd8744a27ca0fcc06a94bc2a1a94f85ccc5c3ba
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-06-02 02:36:05 +0530

    Update area-of-circle.js
    Former-commit-id: 8bd7d81f314498f2aea002758a757ac710e32c37

--- programme/area-of-circle/area-of-circle.js
+++ programme/area-of-circle/area-of-circle.js
@@ -1,22 +1,23 @@
 // Write a programme to find the area of circle
+
 const areaOfCircle = function areaOfCircle(r) {
-	return Math.PI * r * r;
+  return Math.PI * r * r;
 };
 
 const readline = require("readline");
 const rl = readline.createInterface({
-	input: process.stdin,
-	output: process.stdout,
+  input: process.stdin,
+  output: process.stdout,
 });
 
 rl.question("Enter radius : ", (userInput) => {
-	const r = Number.parseFloat(userInput);
-	if (!isNaN(r)) {
-		area = areaOfCircle(r);
-		console.log("\nArea : ", area.toPrecision(4));
-		rl.close();
-	} else {
-		console.log("Invalid input: the input is not a number.");
-		rl.close();
-	}
+  const r = Number.parseFloat(userInput);
+  if (!isNaN(r)) {
+    area = areaOfCircle(r);
+    console.log("\nArea : ", area.toPrecision(4));
+    rl.close();
+  } else {
+    console.log("Invalid input: the input is not a number.");
+    rl.close();
+  }
 });
