66e284e51e4763a31fce05cee5edba969c535f13
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-06-04 23:19:49 +0530

    Update minutes-to-seconds.c

--- programme/minutes-to-seconds/minutes-to-seconds.c
+++ programme/minutes-to-seconds/minutes-to-seconds.c
@@ -1,11 +1,13 @@
+// Write a programme to convert minutes to seconds
+
 #include <stdio.h>
 
 int main(void)
 {
-  double minutes;
-  printf("Min : ");
-  scanf("%lf", &minutes);
-  int seconds = minutes * 60;
-  printf("Sec : %d\n", seconds);
-  return 0;
+    double minutes;
+    printf("Min : ");
+    scanf("%lf", &minutes);
+    int seconds = minutes * 60;
+    printf("Sec : %d\n", seconds);
+    return 0;
 }
