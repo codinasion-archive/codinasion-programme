425c406c6b9a99593a3abac3c00ac16843faff7d
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-06-04 23:18:34 +0530

    Update print-even.c
    Former-commit-id: 0d04bdc121900f47956b6c950dd83871d77b8cb1

--- programme/print-even-numbers/print-even.c
+++ programme/print-even-numbers/print-even.c
@@ -1,10 +1,13 @@
+// Write a programme to print first n even numbers
+
 #include <stdio.h>
 
 int main(void)
 {
     int n;
-    printf("Input: ");
+    printf("Input  : ");
     scanf("%d", &n);
+    printf("Output : ");
     for (int i = 0; i < n - 1; i++)
     {
         printf("%d ", 2 * i);
