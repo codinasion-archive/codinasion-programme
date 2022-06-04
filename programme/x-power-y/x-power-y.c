45b0aeca4cfcd19baa08633f8763858970a92ac3
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-06-04 23:17:27 +0530

    Update x-power-y.c

--- programme/x-power-y/x-power-y.c
+++ programme/x-power-y/x-power-y.c
@@ -1,11 +1,13 @@
+// Write a program to find X power Y
+
 #include <stdio.h>
 #include <math.h>
 
 int main(void)
 {
     double x, y;
-    printf("Input:  ");
+    printf("Input  : ");
     scanf("%lf %lf", &x, &y);
-    printf("Output: %d\n", (int)pow(x, y));
+    printf("Output : %d\n", (int)pow(x, y));
     return 0;
 }
