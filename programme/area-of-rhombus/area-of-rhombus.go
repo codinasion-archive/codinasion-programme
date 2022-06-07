096483a75eae1dc6d8070a288636e914f38fac4b
Madhu S Gowda <79962713+MadhuS-1605@users.noreply.github.com>
2022-06-07 11:38:08 +0530

    Create area-f-rhombus.go

---
+++ programme/area-of-rhombus/area-of-rhombus.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var a, b float64
+	fmt.Print("Enter the value of a: ")
+	fmt.Scan(&a)
+	fmt.Print("Enter the value of b: ")
+	fmt.Scan(&b)
+	fmt.Println("The area of the rhombus is: ", a*b/2)
+}
