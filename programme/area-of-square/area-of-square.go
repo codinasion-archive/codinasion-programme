437c49e84022d26d860d09a1ef70b032b2f915c2
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-06-07 14:22:47 +0530

    Update area-of-square.go

--- programme/area-of-square/area-of-square.go
+++ programme/area-of-square/area-of-square.go
@@ -1,11 +1,12 @@
+// Write a program to find the area of a square
+
 package main
 
 import "fmt"
 
-//area of `square`
 func main() {
 	var a float64
-	fmt.Print("Enter the value of a: ")
+	fmt.Print("Enter the value of a : ")
 	fmt.Scan(&a)
-	fmt.Println("The area of the square is: ", a*a)
+	fmt.Println("\nThe area of the square is :", a*a)
 }
