85251d12944540d575ac05e2d9dba43145b83fd9
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-06-07 10:12:51 +0530

    Update square-of-a-num.js
    Former-commit-id: 6ed6025926eaf8dfb1a22e2c5852b7b3beb8dba3

--- programme/square-of-number/square-of-a-num.js
+++ programme/square-of-number/square-of-a-num.js
@@ -1,11 +1,14 @@
 /* 
-Write a JavaScript Programme to print square of a number.
+Write a Programme to print square of a number
 
 Input  : 5
 Output : 25
 */
 
-
 function squareNum(num) {
-  return num * num
+  return num * num;
 }
+
+const input = 5;
+console.log("Input  :", input);
+console.log("output :", squareNum(input));
