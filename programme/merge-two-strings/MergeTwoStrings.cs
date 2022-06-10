9f7baf5cf8d6cdeedd0e8d6a911e61a783a455f5
Hans <48442650+hansleykowlessur@users.noreply.github.com>
2022-06-10 11:47:57 +0400

    Added merge-two-strings for C#
    Former-commit-id: 7d9b44a2a2d09f2d8af7611b377ba436c607c562

---
+++ programme/merge-two-strings/MergeTwoStrings.cs
@@ -0,0 +1,29 @@
+class MergeTwoStrings
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                // Initialisation 
+                string firstStr = string.Empty;
+                string secondStr = string.Empty;
+
+                // Read values from keyboard
+                Console.Write("Input1 : ");
+                firstStr = Console.ReadLine();
+
+                Console.Write("Input2 : ");
+                secondStr = Console.ReadLine();
+
+                // Display output
+                // Note: Use of string interpolation ($ operation) to concat/merge two variables
+                Console.WriteLine($"Output : {firstStr}{secondStr}");
+                Console.ReadKey();
+            }
+            // Handle other exceptions
+            catch(Exception ex)
+            {
+                Console.WriteLine($"Errors => {ex.Message}{Environment.NewLine}{ex.StackTrace}");
+            }
+        }
+    }
