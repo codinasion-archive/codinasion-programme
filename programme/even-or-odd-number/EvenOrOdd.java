c52b6ee19d1a88af2cc79eb8a609c6e66c14be66
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-06-10 08:29:03 +0530

    Update EvenOrOdd.java
    Former-commit-id: 4e67d07274a8df560b0fc9d8719081e0b99085a4

--- programme/even-or-odd-number/EvenOrOdd.java
+++ programme/even-or-odd-number/EvenOrOdd.java
@@ -1,21 +1,20 @@
-	// A program to check if the entered digit is even or odd
+// A program to check if the entered digit is even or odd
 
-	import java.util.Scanner;
+import java.util.Scanner;
 
-	public class EvenOrOdd {
-	    public static void main(String[] args) {
-	        Scanner sc = new Scanner(System.in);
-	        int num;
+public class EvenOrOdd {
 
-	        System.out.print("Enter the number : ");
-	        num = sc.nextInt();
-	        
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int num;
 
-	        if(num % 2 == 0)
-	        	System.out.println("\nOutput : Even Number");
-	        else
-	        	System.out.println("\nOutput : Odd Number");
+    System.out.print("Enter the number : ");
+    num = sc.nextInt();
 
-	        sc.close();
-	    }
-	}
+    if (num % 2 == 0) System.out.println(
+      "\nOutput : Even Number"
+    ); else System.out.println("\nOutput : Odd Number");
+
+    sc.close();
+  }
+}
