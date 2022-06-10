0ae9856bd12ea2b0f74cd8cfa8ef4f63013db765
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-06-10 17:40:01 +0530

    Update MergeTwoStrings.cs

--- programme/merge-two-strings/MergeTwoStrings.cs
+++ programme/merge-two-strings/MergeTwoStrings.cs
@@ -1,29 +1,32 @@
+// Write a programme to merge two strings
+using System;
+
 class MergeTwoStrings
+{
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        try
         {
-            try
-            {
-                // Initialisation 
-                string firstStr = string.Empty;
-                string secondStr = string.Empty;
+            // Initialisation
+            string firstStr = string.Empty;
+            string secondStr = string.Empty;
 
-                // Read values from keyboard
-                Console.Write("Input1 : ");
-                firstStr = Console.ReadLine();
+            // Read values from keyboard
+            Console.Write("Input1 : ");
+            firstStr = Console.ReadLine();
 
-                Console.Write("Input2 : ");
-                secondStr = Console.ReadLine();
+            Console.Write("Input2 : ");
+            secondStr = Console.ReadLine();
 
-                // Display output
-                // Note: Use of string interpolation ($ operation) to concat/merge two variables
-                Console.WriteLine($"Output : {firstStr}{secondStr}");
-                Console.ReadKey();
-            }
-            // Handle other exceptions
-            catch(Exception ex)
-            {
-                Console.WriteLine($"Errors => {ex.Message}{Environment.NewLine}{ex.StackTrace}");
-            }
+            // Display output
+            // Note: Use of string interpolation ($ operation) to concat/merge two variables
+            Console.WriteLine($"Output : {firstStr}{secondStr}");
+        }
+        // Handle other exceptions
+        catch (Exception ex)
+        {
+            Console
+                .WriteLine($"Errors => {ex.Message}{Environment.NewLine}{ex.StackTrace}");
         }
     }
+}
