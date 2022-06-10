01d8cf5923e285f304c1ab969444846126417866
Hans <48442650+hansleykowlessur@users.noreply.github.com>
2022-06-10 14:47:04 +0400

    Added swap-wtihout-temp.cs

---
+++ programme/swap-two-numbers-without-using-third-variable/swap-without-temp.cs
@@ -0,0 +1,27 @@
+    class SwapTwoNumbersWithoutTemp
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                // Initialisation
+                int a = 10;
+                int b = 20;
+
+                // Swapping the values where:
+                // - b is assigned the value of a
+                // - a is assigned the value of b
+                // Note: The operation is possible using tuples
+                (b, a) = (a, b);
+
+                // Display result
+                Console.WriteLine($"a={a} b={b}");
+            }
+            // Handle other exceptions
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Errors => {ex.Message}{Environment.NewLine}{ex.StackTrace}");
+            }
+           
+        }
+    }
