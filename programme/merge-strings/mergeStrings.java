f6cc11780eee45bc80c704dba0e9f0bc5da9cb6c
Alenn Wright <bowserko123@gmail.com>
2022-06-09 23:26:48 -0700

    Added merge-strings
    Added the Java, JS, Python, C and C++ versions
    
    
    Former-commit-id: 4b3aaac66fcf2628aa30607d7ff1e4985a1a00a8

---
+++ programme/merge-strings/mergeStrings.java
@@ -0,0 +1,14 @@
+
+public class mergeStrings {
+    public static void main(String[] args)
+    {
+        String String1 = "Hello";
+        String String2 = "_World";
+
+        System.out.println("String 1: " + String1);
+        System.out.println("String 2: " + String2);
+        String mergedString = String1 + String2;
+        System.out.println("Merged String: " + mergedString);
+
+    }
+}
---
+++ programme/merge-strings/mergeStrings.py
@@ -0,0 +1,10 @@
+
+def mergeStrings(String1, String2):
+    return String1 + String2
+
+strIn1 = "Hello"
+strIn2 = " World"
+
+print("String 1: " + strIn1)
+print("String 2: " + strIn2)
+print("Merged String : " + mergeStrings("Hello"," World"))
