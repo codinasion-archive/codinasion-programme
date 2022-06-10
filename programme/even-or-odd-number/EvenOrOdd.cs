cc38016350b70557cf6627faa67450cd121697da
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-06-10 17:18:04 +0530

    Update EvenOrOdd.cs

--- programme/even-or-odd-number/EvenOrOdd.cs
+++ programme/even-or-odd-number/EvenOrOdd.cs
@@ -1,40 +1,44 @@
- class EvenOrOdd
+// Write a programme to check a number even or odd
+using System;
+
+class EvenOrOdd
+{
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        try
         {
+            // Initialisation
+            string firstNumStr = string.Empty;
+            const string ODD_NUMBER_MSG = "Odd number";
+            const string EVEN_NUMBER_MSG = "Even number";
 
-            try
+            // Read value from keyboard
+            Console.Write("Input  : ");
+            firstNumStr = Console.ReadLine();
+
+            // Parse string to number
+            int numericValue = int.Parse(firstNumStr);
+
+            // Use the modulus operator to check
+            // if the remainder is zero for even number
+            Console.Write("Output : ");
+            if (numericValue % 2 == 0)
             {
-                // Initialisation 
-                string firstNumStr = string.Empty;
-                const string ODD_NUMBER_MSG = "Odd number";
-                const string EVEN_NUMBER_MSG = "Even number";
-
-                // Read value from keyboard
-                Console.Write("Input : ");
-                firstNumStr = Console.ReadLine();
-
-                // Parse string to number
-                int numericValue = int.Parse(firstNumStr);
-
-                // Use the modulus operator to check 
-                // if the remainder is zero for even number
-                if (numericValue % 2 == 0)
-                {
-                    // Even message
-                    Console.Write(EVEN_NUMBER_MSG);
-                }else
-                {
-                    // Odd message
-                    Console.Write(ODD_NUMBER_MSG);
-                }
-                Console.ReadKey();
+                // Even message
+                Console.WriteLine (EVEN_NUMBER_MSG);
             }
-            // Handle other exceptions
-            catch (Exception ex)
+            else
             {
-                Console.WriteLine($"Errors => {ex.Message}{Environment.NewLine}{ex.StackTrace}");
+                // Odd message
+                Console.WriteLine (ODD_NUMBER_MSG);
             }
-            
+        }
+        // Handle other exceptions
+        catch (Exception ex)
+        {
+            Console
+                .WriteLine($"Errors => {ex.Message}{Environment.NewLine}
+                    ex.StackTrace}")
         }
     }
+}
