bdc713cb6725b0deffe2abd564c1b59fa668013a
Harsh Raj <harshraj8843pro@gmail.com>
2022-06-10 17:28:57 +0530

    update

--- programme/check-leap-year/CheckLeapYear.java
+++ programme/check-leap-year/CheckLeapYear.java
@@ -4,19 +4,27 @@ import java.util.Scanner;
 
 public class CheckLeapYear {
 
-    public static String checkLeapYear(int year) {
-
-        if (year % 4 == 0)
-            return "Leap year";
-        return "Not a leap year";
+  public static String checkLeapYear(int year) {
+    if (year % 4 == 0) {
+      if (year % 100 == 0) {
+        if (year % 400 == 0) {
+          return "The year is a leap year";
+        } else {
+          return "The year is not a leap year";
+        }
+      } else {
+        return "The year is a leap year";
+      }
+    } else {
+      return "The year is not a leap year";
     }
+  }
 
-    public static void main(String[] args) {
-
-        Scanner scan = new Scanner(System.in);
-        System.out.print("Input  : ");
-        int year = scan.nextInt();
-        System.out.println("Output : " + checkLeapYear(year));
-        scan.close();
-    }
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    System.out.print("Input  : ");
+    int year = scan.nextInt();
+    System.out.println("Output : " + checkLeapYear(year));
+    scan.close();
+  }
 }
