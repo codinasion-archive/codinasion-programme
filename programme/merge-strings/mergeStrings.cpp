4b3aaac66fcf2628aa30607d7ff1e4985a1a00a8
Alenn Wright <bowserko123@gmail.com>
2022-06-09 23:26:48 -0700

    Added merge-strings
    Added the Java, JS, Python, C and C++ versions

---
+++ programme/merge-strings/mergeStrings.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string str1, str2;
+
+    cout << "Enter String 1: ";
+    cin >> str1;
+    cout << "Enter String 2: ";
+    cin >> str2;
+
+    string mergedString = str1 + str2;
+    cout << "\nMerged String: " << mergedString << endl;
+    return 0;
+}
