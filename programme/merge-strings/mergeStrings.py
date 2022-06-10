3361fe81aacc8c40d85cd934e12abb837e8de9ca
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-06-10 16:35:03 +0530

    Update mergeStrings.py

--- programme/merge-strings/mergeStrings.py
+++ programme/merge-strings/mergeStrings.py
@@ -1,10 +1,12 @@
+# Write a programme to merge two strings
 
 def mergeStrings(String1, String2):
     return String1 + String2
 
-strIn1 = "Hello"
-strIn2 = " World"
 
-print("String 1: " + strIn1)
-print("String 2: " + strIn2)
-print("Merged String : " + mergeStrings("Hello"," World"))
+strIn1 = "Hello"
+strIn2 = "_World"
+
+print("String 1 : " + strIn1)
+print("String 2 : " + strIn2)
+print("\nMerged String : " + mergeStrings(strIn1, strIn2))
