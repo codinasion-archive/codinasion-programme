5fd18af0f7c893941d172136555c98ab29c00b07
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-06-10 16:34:56 +0530

    Update mergeStrings.c
    Former-commit-id: 1c3b4921412ebec103c2f0219580835c8e131970

--- programme/merge-strings/mergeStrings.c
+++ programme/merge-strings/mergeStrings.c
@@ -1,22 +1,23 @@
+// Write a programme to merge two strings
 
 #include <stdio.h>
 #include <string.h>
 
 int main()
 {
-   /* Define a temporary variable */
-   char mergedString[100], str1[50], str2[50];
+    /* Define a temporary variable */
+    char mergedString[100], str1[50], str2[50];
 
-   printf("Enter String 1: ");
-   scanf("%s", str1);
-   printf("Enter String 2: ");
-   scanf("%s", str2);
+    printf("Enter String 1 : ");
+    scanf("%s", str1);
+    printf("Enter String 2 : ");
+    scanf("%s", str2);
 
-   strcpy(mergedString, str1);
-   strcat(mergedString, str2);
+    strcpy(mergedString, str1);
+    strcat(mergedString, str2);
 
-   /* Display the concatenated strings */
-   printf("%s\n", mergedString);
+    /* Display the concatenated strings */
+    printf("\nOutput : %s\n", mergedString);
 
-   return 0;
+    return 0;
 }
