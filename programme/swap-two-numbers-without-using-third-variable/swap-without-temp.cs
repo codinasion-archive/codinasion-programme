5bd0a68eb805fe59b6c686b34a8652a30e5cb60d
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-06-10 17:48:21 +0530

    Update swap-without-temp.cs

--- programme/swap-two-numbers-without-using-third-variable/swap-without-temp.cs
+++ programme/swap-two-numbers-without-using-third-variable/swap-without-temp.cs
@@ -1,27 +1,31 @@
-    class SwapTwoNumbersWithoutTemp
+// Write a program to swap two numbers without using third variable
+using System;
+
+class SwapTwoNumbersWithoutTemp
+{
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        try
         {
-            try
-            {
-                // Initialisation
-                int a = 10;
-                int b = 20;
+            // Initialisation
+            int a = 10;
+            int b = 20;
+            Console.WriteLine($"a={a} b={b}");
 
-                // Swapping the values where:
-                // - b is assigned the value of a
-                // - a is assigned the value of b
-                // Note: The operation is possible using tuples
-                (b, a) = (a, b);
+            // Swapping the values where:
+            // - b is assigned the value of a
+            // - a is assigned the value of b
+            // Note: The operation is possible using tuples
+            (b, a) = (a, b);
 
-                // Display result
-                Console.WriteLine($"a={a} b={b}");
-            }
-            // Handle other exceptions
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Errors => {ex.Message}{Environment.NewLine}{ex.StackTrace}");
-            }
-           
+            // Display result
+            Console.WriteLine($"a={a} b={b}");
+        }
+        // Handle other exceptions
+        catch (Exception ex)
+        {
+            Console
+                .WriteLine($"Errors => {ex.Message}{Environment.NewLine}{ex.StackTrace}");
         }
     }
+}
