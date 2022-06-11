62a9cbc8c77b0afe226181595323642fa7e71b6c
ClasherKasten <cfb1470@gmail.com>
2022-06-11 20:30:47 +0200

    Added JS Program for 'Inches to Feet'
    Former-commit-id: 45e873e3bb3113c6473fef4d00cd5375e5f18a30

---
+++ programme/inches-to-feet/inches-to-feet.js
@@ -0,0 +1,21 @@
+// Write a program to add two numbers
+// Execute: node inches-to-feet.js
+
+"use strict"
+
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question("Input  : ", (inches) => {
+	const result = parseInt(inches) / 12;
+	if (!isNaN(result)) {
+		console.log(`Output : ${result}`)
+	} else {
+		console.log("An Error occured");
+	}
+	rl.close();
+});
