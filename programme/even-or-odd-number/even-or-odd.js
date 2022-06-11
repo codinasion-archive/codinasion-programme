779c4d99da1573432a456df264ad548ecf59bb3b
Jon Rutter <contact@jonrutter.io>
2022-06-11 02:46:06 -0400

    Implement even-or-odd in JavaScript

---
+++ programme/even-or-odd-number/even-or-odd.js
@@ -0,0 +1,13 @@
+/* Write a program to check whether a number is even or odd
+
+Example:
+Input  : 3
+Output : Odd Number
+
+*/
+
+const evenOrOdd = (n) => {
+  return n % 2 === 0 ? 'Even number' : 'Odd number';
+};
+
+console.log(evenOrOdd(3));
