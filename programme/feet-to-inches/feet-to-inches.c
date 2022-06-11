ebfb4a65d176c018de5609de2679652f86fd4bd5
ClasherKasten <cfb1470@gmail.com>
2022-06-11 16:37:08 +0200

    Setup 'Feet to Inches'. Added C Program for 'Feet to Inches'

---
+++ programme/feet-to-inches/feet-to-inches.c
@@ -0,0 +1,12 @@
+// Write a programme to convert feet to inches
+
+#include <stdio.h>
+
+int main(void)
+{
+  int feet;
+  printf("Input  : ");
+  scanf("%d", &feet);
+  printf("Output : %d\n", feet * 12);
+  return 0;
+}
