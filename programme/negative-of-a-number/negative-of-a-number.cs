8d3505a56bc5e084bee5fc8778378ae51e2e895e
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-06-11 16:45:07 +0530

    Update negative-of-a-number.cs
    Former-commit-id: 3b66b7ff4d9cfe3deb200a29a30eca8d28957009

--- programme/negative-of-a-number/negative-of-a-number.cs
+++ programme/negative-of-a-number/negative-of-a-number.cs
@@ -1,30 +1,34 @@
-    class NegativeOfANumber
+// Write a program to convert number to strings
+using System;
+
+class NegativeOfANumber
+{
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        try
         {
-            try
-            {
-                // Initialisation
-                string numberStr = string.Empty;
-                int negativeNumber = 0;
+            // Initialisation
+            string numberStr = string.Empty;
+            int negativeNumber = 0;
 
-                // Read value from keyboard
-                Console.Write("Input : ");
-                numberStr = Console.ReadLine();
+            // Read value from keyboard
+            Console.Write("Input  : ");
+            numberStr = Console.ReadLine();
 
-                // Convert string to int
-                int numConverted = int.Parse(numberStr);
+            // Convert string to int
+            int numConverted = int.Parse(numberStr);
 
-                // If the number is already negative no conversion else substract 0 from the +ve number to convert it to -ve
-                negativeNumber = (numConverted <= 0) ? numConverted : 0 - numConverted;
+            // If the number is already negative no conversion else substract 0 from the +ve number to convert it to -ve
+            negativeNumber =
+                (numConverted <= 0) ? numConverted : 0 - numConverted;
 
-                // Display result
-                Console.WriteLine($"Output : {negativeNumber}");
-            }
-            // Handle other exceptions
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Errors => {ex.Message}{Environment.NewLine}{ex.StackTrace}");
-            }
+            // Display result
+            Console.WriteLine($"Output : {negativeNumber}");
+        }
+        // Handle other exceptions
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Errors => {ex.Message}{Environment.NewLine}{ex.StackTrace}");
         }
     }
+}
