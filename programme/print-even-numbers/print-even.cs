ba5aadb9a57364213eda3a1949ff1234e9accc5d
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-06-11 22:19:40 +0530

    Update print-even.cs

--- programme/print-even-numbers/print-even.cs
+++ programme/print-even-numbers/print-even.cs
@@ -1,33 +1,36 @@
-    class PrintEvenNumber
+// Write a programme to print first n even numbers
+using System;
+using System.Linq;
+
+class PrintEvenNumber
+{
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        try
         {
-            try
+            // Initialisation
+            string numStr = string.Empty;
+            string evenNumberStr = string.Empty;
+
+            // Read value from keyboard
+            Console.Write("Input  :  ");
+            numStr = Console.ReadLine();
+
+            // Loop n-th time based on value input
+            for (int eachNum = 0; eachNum < int.Parse(numStr); eachNum++)
             {
-                // Initialisation
-                string numStr = string.Empty;
-                string evenNumberStr = string.Empty;
-
-                // Read value from keyboard
-                Console.Write("Input : ");
-                numStr = Console.ReadLine();
-
-                // Loop n-th time based on value input
-                for(int eachNum = 0; eachNum < int.Parse(numStr); eachNum++)
-                {
-                    // Check if even number and concat with previous string
-                    if (eachNum % 2 == 0)
-                        evenNumberStr = evenNumberStr + " " + eachNum;
-                        
-                }
-
-                // Display result
-                Console.WriteLine($"Output : {evenNumberStr}");
+                // Check if even number and concat with previous string
+                if (eachNum % 2 == 0)
+                    evenNumberStr = evenNumberStr + " " + eachNum;
             }
-            // Handle other exceptions
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Errors => {ex.Message}{Environment.NewLine}{ex.StackTrace}");
-            }
+
+            // Display result
+            Console.WriteLine($"Output : {evenNumberStr}");
+        }
+        // Handle other exceptions
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Errors => {ex.Message}{Environment.NewLine}{ex.StackTrace}");
         }
     }
+}
