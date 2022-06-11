8e5d087ad366daab223d7848a17da9f4f91986a1
ClasherKasten <cfb1470@gmail.com>
2022-06-11 20:29:08 +0200

    Added Java Program for 'Inches to Feet'

---
+++ programme/inches-to-feet/InchesToFeet.java
@@ -0,0 +1,16 @@
+// Write a programme to convert inches to feet
+
+import java.util.Scanner;
+
+class InchesToFeet {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Input  : ");
+            int inches = sc.nextInt();
+            System.out.print("Output : ");
+            System.out.println(inches / 12.0);
+        } catch (Exception e) {
+            System.err.println("An Error occured");
+        }
+    }
+}
