8ac10df4d706705f044947d94ea300fe8fbb1bfc
IrumiDesu7 <ikalam89@gmail.com>
2022-06-11 20:37:06 +0800

    add celsius-to-fahrenheit.js

---
+++ programme/celsius-to-fahrenheit/celsius-to-fahrenheit.js
@@ -0,0 +1,6 @@
+// Write a programme to convert celsius to fahrenheit
+
+const celc = Number(prompt('Input temperature in celsius : '));
+
+const fahrenheit = (celc * 9) / 5 + 32;
+console.log(fahrenheit);
