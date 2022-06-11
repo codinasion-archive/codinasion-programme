32bc8ad79bcfb060cfe7a9900ee0a26584f641e2
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-06-11 22:31:17 +0530

    Update even-or-odd.c

--- programme/even-or-odd-number/even-or-odd.c
+++ programme/even-or-odd-number/even-or-odd.c
@@ -4,9 +4,9 @@
 
 int main(void)
 {
-  unsigned long long number;
-  printf("Input  : ");
-  scanf("%llud", &number);
-  printf("Output : %s Number\n", number % 2 == 0 ? "Even" : "Odd");
-  return 0;
+    unsigned long long number;
+    printf("Input  : ");
+    scanf("%llud", &number);
+    printf("Output : %s Number\n", number % 2 == 0 ? "Even" : "Odd");
+    return 0;
 }
