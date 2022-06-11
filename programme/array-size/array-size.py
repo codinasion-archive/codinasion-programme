2ccb3e9140e0dd42e31c39d13adfe9c96cc84396
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-06-11 18:21:33 +0530

    Update array-size.py

--- programme/array-size/array-size.py
+++ programme/array-size/array-size.py
@@ -1,8 +1,9 @@
 # Write a programme to check size of an array
 
-array_list = ['array1','array2','array3']
+array_list = ['array1', 'array2', 'array3']
+
+print(array_list)
 
 array_size = len(array_list)
 
-print("Array size : ", array_size)
-
+print("\nArray size : ", array_size)
