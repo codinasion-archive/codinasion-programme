0ff1e7584d565fdeba4e57357b07128816bbad80
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-06-11 15:02:10 +0530

    Update number-to-string.cs

--- programme/number-to-string/number-to-string.cs
+++ programme/number-to-string/number-to-string.cs
@@ -1,3 +1,4 @@
+// Write a program to convert number to strings
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,61 +9,61 @@ namespace Number_to_Strings
     {
         static void Main(string[] args)
         {
-
             try
             {
                 // Initialisation
                 string numStr = string.Empty;
 
                 // Contains the mapping of numeric value to their string equivalent
-                IDictionary<int, string> dicNumToStr = new Dictionary<int, string>()
-                {
-                    {0, "zero" },
-                    {1, "one" },
-                    {2, "two" },
-                    {3, "three" },
-                    {4, "four" },
-                    {5, "five" },
-                    {6, "six" },
-                    {7, "seven" },
-                    {8, "eight" },
-                    {9, "nine" }
-                };
+                IDictionary<int, string> dicNumToStr =
+                    new Dictionary<int, string>()
+                    {
+                        { 0, "zero" },
+                        { 1, "one" },
+                        { 2, "two" },
+                        { 3, "three" },
+                        { 4, "four" },
+                        { 5, "five" },
+                        { 6, "six" },
+                        { 7, "seven" },
+                        { 8, "eight" },
+                        { 9, "nine" }
+                    };
 
                 // Read value from keyboard
-                Console.Write("Input: ");
+                Console.Write("Input  : ");
                 numStr = Console.ReadLine();
                 Console.WriteLine();
 
                 // Convert to List of integer
                 // ===========================
-                List<int> arrOfNumericValues = numStr.
-                                               ToCharArray(). // <= Convert string to array of char. E.g. "123" => ['1','2','3']
-                                               ToList().      // <= Convert array to List in order to use Linq to be able to convert each char to integer
-                                               ConvertAll(
-                                                   // 1. Convert each char to string. E.g. '1' => "1"
-                                                   // 2. Convert each string to integer. E.g. "1" => 1
-                                                   eachChar => int.Parse(eachChar.ToString())
-                                               );
+                List<int> arrOfNumericValues =
+                    numStr
+                        .ToCharArray()                  // <= Convert string to array of char. E.g. "123" => ['1','2','3']
+                        .ToList()                       // <= Convert array to List in order to use Linq to be able to convert each char to integer
+                        .ConvertAll(                    
+                        // 1. Convert each char to string. E.g. '1' => "1"
+                        // 2. Convert each string to integer. E.g. "1" => 1
+                        eachChar => int.Parse(eachChar.ToString()));
 
                 // Convert numbers to string
                 // =========================
-                string resultStr = string.Join(
-                                    " ", // <= White space separator
-                                    arrOfNumericValues.
-                                        Select(
-                                            // Map each number against their respective value in the dictionary. E.g. 1 => "one"
-                                            eachNumber => dicNumToStr[eachNumber]
-                                        ) 
-                                   );
+                string resultStr =
+                    string
+                        .Join(" ",                      // <= White space separator
+                        arrOfNumericValues
+                            .Select(
+                            // Map each number against their respective value in the dictionary. E.g. 1 => "one"
+                            eachNumber => dicNumToStr[eachNumber]));
 
                 // Display result
-                Console.Write($"output : {resultStr}");
+                Console.WriteLine($"output : {resultStr}");
             }
             // Handle other exceptions
             catch (Exception ex)
             {
-                Console.WriteLine($"Errors => {ex.Message}{Environment.NewLine}{ex.StackTrace}");
+                Console
+                    .WriteLine($"Errors => {ex.Message}{Environment.NewLine}{ex.StackTrace}");
             }
         }
     }
