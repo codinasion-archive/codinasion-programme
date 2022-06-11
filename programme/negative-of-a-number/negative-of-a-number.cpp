7525fbb6c3e4575525fa5489e17099c99fc6836e
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-06-11 17:43:57 +0530

    Update negative-of-a-number.cpp

--- programme/negative-of-a-number/negative-of-a-number.cpp
+++ programme/negative-of-a-number/negative-of-a-number.cpp
@@ -1,9 +1,11 @@
+// Write a programme to find negative of a number
+
 #include <iostream>
 
 int main()
 {
     int input;
-    std::cout << "Input : ";
+    std::cout << "Input  : ";
     std::cin >> input;
-    std::cout << "Output : " << -input;
+    std::cout << "Output : " << -input << "\n";
 }
