d79115d3fb0605f659279af74d878f71c9038376
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-06-11 17:25:34 +0530

    Update check-leap-year.js
    Former-commit-id: 45f76440a99f783c4622eb5a703f7c0f149ea728

--- programme/check-leap-year/check-leap-year.js
+++ programme/check-leap-year/check-leap-year.js
@@ -1,5 +1,7 @@
-const year = Number(prompt('Input year : '));
+// Write a programme to check leap year
+
+const year = Number(prompt("Input year : "));
 
 year % 4 === 0 && (year % 400 === 0 || year % 100 !== 0)
-  ? console.log('leap year')
-  : console.log('not leap year');
+  ? console.log("leap year")
+  : console.log("not leap year");
