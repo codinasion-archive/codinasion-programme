951dafcf1e6e55ae2cd1052e1d2ce22f9357a597
Prajwal Borkar <sunnyborkar7777@gmail.com>
2022-06-12 12:22:12 +0530

    Added array-size.java file
    Former-commit-id: c904eacb6cde768f895908f91bd41ee9a8cec592

---
+++ programme/array-size/array-size.java
@@ -0,0 +1,11 @@
+// Write a programme to check size of an array
+
+class ArraySize
+{
+	public static void main (String[] args)
+	{
+		String[] arr = {"array1", "array2", "array3"};
+		int arrlength = arr.length;
+		System.out.println(arrlength);
+	}
+}
