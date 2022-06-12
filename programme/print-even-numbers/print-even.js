6e977d75ac65b5b8d33cfb4f90e889dc524765fe
Lorenzo Rossi <rossilor95@outlook.it>
2022-06-13 00:01:12 +0200

    Added print-even.js
    Former-commit-id: 681ee6dc3bd5cd0cf39f5bdcd6c40b13144d2529

---
+++ programme/print-even-numbers/print-even.js
@@ -0,0 +1,24 @@
+const isEven = function (number) {
+    return number % 2 == 0;
+};
+
+const readline = require("readline");
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question("Input  : ", (answer) => {
+    process.stdout.write("Output : ");
+    let counter = 0;
+    let number = 0;
+    while (counter < answer) {
+        if (isEven(number)) {
+            process.stdout.write(String(number) + " ");
+            counter++;
+        }
+        number++;
+    }
+    rl.close();
+});
