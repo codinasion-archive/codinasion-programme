6ad731d1cace0aad35cad63200090856f5331edc
Harsh Raj <harshraj8843pro@gmail.com>
2022-06-14 01:14:55 +0530

    update
    Former-commit-id: 8dcaf70164bc82d8432b941c8f698ad654c7fcfa

--- programme/compound-interest-calculator/compound-interest-calculator.go
+++ programme/compound-interest-calculator/compound-interest-calculator.go
@@ -1,3 +1,5 @@
+// Write a programme to calculate compound interest
+
 package main
 
 import (
@@ -8,14 +10,16 @@ import (
 func main() {
 	var priciple, term, rate float64
 	var amount float64
-	fmt.Println("Enter the Principle Amount:")
+	fmt.Print("Enter the Principle Amount : ")
 	fmt.Scan(&priciple)
-	fmt.Println("Enter the Term in Years:")
+	fmt.Print("Enter the Term in Years : ")
 	fmt.Scan(&term)
-	fmt.Println("Enter the Intrest Rate:")
+	fmt.Print("Enter the Intrest Rate : ")
 	fmt.Scan(&rate)
+
 	amount = math.Pow(1+rate/100, term) * priciple
-	fmt.Println("The amount is:", amount)
+	fmt.Println("\nThe Amount is :", math.Floor(amount*100)/100)
+
 	intrest := amount - priciple
-	fmt.Println("The Compound Intrest is:", intrest)
+	fmt.Println("The Compound Intrest is :", math.Floor(intrest*100)/100)
 }
