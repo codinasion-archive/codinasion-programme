59d9f983818ed285720a0f9e780e02cd7bbb74c4
Umaxyon <umaxyon@gmail.com>
2022-06-13 15:35:59 +0900

    Added negative-of-a-number.go
    Former-commit-id: afbf377de8429418542827ceac505007735a1d11

---
+++ programme/negative-of-a-number/negative-of-a-number.go
@@ -0,0 +1,27 @@
+// Write a programme to find negative of a number
+
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	var str string
+
+	fmt.Print("Input Number : ")
+	fmt.Scan(&str)
+
+	num, err := strconv.Atoi(str)
+	if err != nil {
+		fmt.Printf("Input error: %s", str)
+		return
+	}
+	
+	if num > 0 {
+		num = -num
+	}
+
+	fmt.Printf("\nOutput: %d\n", num)
+}
