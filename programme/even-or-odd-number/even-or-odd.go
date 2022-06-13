5eeef4cb1982e505d3af63a7384f682d01f0ecb3
Umaxyon <umaxyon@gmail.com>
2022-06-13 17:01:43 +0900

    Added even-or-odd.go

---
+++ programme/even-or-odd-number/even-or-odd.go
@@ -0,0 +1,30 @@
+// Write a programme to check a number even or odd
+
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	var str string
+
+	fmt.Print("Input Number : ")
+	fmt.Scan(&str)
+
+	num, err := strconv.Atoi(str)
+	if err != nil {
+		fmt.Printf("Input error: %s", str)
+		return
+	}
+
+	var evenOrOdd string
+	if num%2 == 0 {
+		evenOrOdd = "Even"
+	} else {
+		evenOrOdd = "Odd"
+	}
+
+	fmt.Printf("\nOutput : %s Number", evenOrOdd)
+}
