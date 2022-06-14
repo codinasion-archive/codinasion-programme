40d69f27d5b8a1590e06536cbf3fa66bb91e525a
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-06-14 23:00:19 +0530

    Update array-size.java
    Former-commit-id: 01baa6d2b1ce65c5318c080889fc58b93ec9ecd0

--- programme/array-size/array-size.java
+++ programme/array-size/array-size.java
@@ -1,11 +1,14 @@
 // Write a programme to check size of an array
 
-class ArraySize
-{
-	public static void main (String[] args)
-	{
-		String[] arr = {"array1", "array2", "array3"};
-		int arrlength = arr.length;
-		System.out.println(arrlength);
-	}
+import java.util.Arrays;
+
+class ArraySize {
+
+  public static void main(String[] args) {
+    String[] arr = { "element1", "element2", "element3" };
+    System.out.println("Input  : " + Arrays.toString(arr));
+
+    int arrlength = arr.length;
+    System.out.println("Output : " + arrlength);
+  }
 }
