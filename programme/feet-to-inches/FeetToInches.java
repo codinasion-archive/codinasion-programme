489860cdd78824ba03627d34cbc362e6a3913a3b
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-06-14 23:32:16 +0530

    Update FeetToInches.java
    Former-commit-id: df83e5262f8e2d015beb3fe1402fbd972ef2543d

--- programme/feet-to-inches/FeetToInches.java
+++ programme/feet-to-inches/FeetToInches.java
@@ -3,14 +3,15 @@
 import java.util.Scanner;
 
 class FeetToInches {
-    public static void main(String[] args) {
-        try (Scanner sc = new Scanner(System.in)) {
-            System.out.print("Input  : ");
-            int feet = sc.nextInt();
-            System.out.print("Output : ");
-            System.out.println(feet * 12);
-        } catch (Exception e) {
-            System.err.println("An Error occured");
-        }
+
+  public static void main(String[] args) {
+    try (Scanner sc = new Scanner(System.in)) {
+      System.out.print("Input  : ");
+      int feet = sc.nextInt();
+      System.out.print("Output : ");
+      System.out.println(feet * 12);
+    } catch (Exception e) {
+      System.err.println("An Error occured");
     }
+  }
 }
