709789a042e3faaecd21703a092356c8bf0d6d84
yilmaz98 <mohamedyilmaz98@gmail.com>
2022-06-15 00:55:55 -0700

    Resolved a typo in code

--- programme/array-size/array-size.go
+++ programme/array-size/array-size.go
@@ -2,7 +2,7 @@ package main
 import "fmt"
 
 func main() {
-    array := []int{"element1", "element2", "element3"}
+    array := []string{"element1", "element2", "element3"}
 
 	length = len(array)
 	
