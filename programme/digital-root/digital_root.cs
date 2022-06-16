73d46bd0611adfdc187d26fbfcf94c9aa34edb3b
Umaxyon <umaxyon@gmail.com>
2022-06-16 14:44:32 +0900

    Added digital_root.cs

---
+++ programme/digital-root/digital_root.cs
@@ -0,0 +1,27 @@
+// Write a programme for Digital Root of a number
+
+using System;
+
+public class DigitalRoot
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Input : ");
+        int num;
+        if (int.TryParse(Console.ReadLine(), out num))
+        {
+            Console.WriteLine("\nOutput : {0}", Convert(num));
+        }
+        else
+        {
+            Console.WriteLine("Input Err");
+        }
+    }
+
+    public static int Convert(int n)
+    {
+        if (n == 0) return 0;
+
+        return 1 + ((n - 1) % 9);
+    }
+}
