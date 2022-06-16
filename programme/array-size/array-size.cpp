77048ba551b420186707683f8855b33c5c8ca086
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-06-16 09:50:44 +0530

    Update array-size.cpp
    Former-commit-id: 9e6bbd16be0f3c32e7c9fdd1639cacf3a67ec0a7

--- programme/array-size/array-size.cpp
+++ programme/array-size/array-size.cpp
@@ -1,19 +1,49 @@
+// Write a programme to find the length of an array
+
 #include <iostream>
 #include <vector>
 using namespace std;
-template <typename T> int array_size_2(vector<T> array){
+template <typename T>
+int array_size(vector<T> array)
+{
     return array.size();
 }
-int main(){
-    char arraytest1[0];                        // length == 0
-    int arraytest2[5]={0,1,2,3,4};             // length == 5
 
-    cout << "test 1 : " << (*(&arraytest1+1)-arraytest1==0) << endl;
-    cout << "test 2 : " << (sizeof(arraytest2)/sizeof(arraytest2[0])==5) << endl;
+int main()
+{
+    // using 'sizeof' function
+    int array[] = {0, 1, 2, 3, 4};
+    int lenOfArray = sizeof(array) / sizeof(array[0]);
+    cout << "Input  : [ ";
+    for (int i = 0; i < lenOfArray; i++)
+    {
+        if (i == lenOfArray - 1)
+        {
+            cout << array[i] << " ";
+        }
+        else
+        {
+            cout << array[i] << ", ";
+        }
+    }
+    cout << "]" << endl;
+    cout << "Output : " << lenOfArray << endl;
 
-    vector<char> arrayOfChars = {'4','3','c'}; // length == 3
-    vector<int> arrayOfIntegers = {1,3,5,2,3}; // length == 5
-    
-    cout << "test 3 : " << (array_size_2(arrayOfChars) == 3) << endl;
-    cout << "test 4 : " << (array_size_2(arrayOfIntegers) == 5) << endl;
+    // using 'vector'
+    vector<int> array1 = {1, 3, 5, 2, 3};
+    int lenOfArray1 = array_size(array1);
+    cout << "Input  : [ ";
+    for (int i = 0; i < lenOfArray1; i++)
+    {
+        if (i == lenOfArray1 - 1)
+        {
+            cout << array1[i] << " ";
+        }
+        else
+        {
+            cout << array1[i] << ", ";
+        }
+    }
+    cout << "]" << endl;
+    cout << "Output : " << lenOfArray1 << endl;
 }
