a344858724391fdbe970195ab2aa79285eb38212
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-06-16 10:21:04 +0530

    Update array-size.go

--- programme/array-size/array-size.go
+++ programme/array-size/array-size.go
@@ -1,10 +1,14 @@
+// Write a programme to find the length of an array
+
 package main
+
 import "fmt"
 
 func main() {
-    array := []string{"element1", "element2", "element3"}
+	array := []string{"element1", "element2", "element3"}
 
-	length = len(array)
-	
-    fmt.Println("Array Length:", length)
+	length := len(array)
+
+	fmt.Println("Array :", array)
+	fmt.Println("Array Length :", length)
 }
