077b81bd46cc07814dfa1eb8712af598c7014da3
Umaxyon <umaxyon@gmail.com>
2022-06-16 14:52:38 +0900

    Added hours-to-seconds.cs

---
+++ programme/hours-to-seconds/hours-to-seconds.cs
@@ -0,0 +1,20 @@
+// Write a C programme to convert hours to seconds.
+
+using System;
+
+public class HoursToSeconds
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Hours : ");
+        int num;
+        if (int.TryParse(Console.ReadLine(), out num))
+        {
+            Console.WriteLine("\nSeconds : {0}", num * 3600);
+        }
+        else
+        {
+            Console.WriteLine("Input Err");
+        }
+    }
+}
