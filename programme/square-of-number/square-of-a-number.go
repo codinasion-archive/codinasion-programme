8e2818e26a9ccd41cffeee609f38e6f2eb1e810c
Umaxyon <umaxyon@gmail.com>
2022-06-16 14:04:50 +0900

    Added square-of-a-number.go

---
+++ programme/square-of-number/square-of-a-number.go
@@ -0,0 +1,23 @@
+// Write a programme to find the square of a number
+
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	var str string
+
+	fmt.Print("Input : ")
+	fmt.Scan(&str)
+
+	num, err := strconv.Atoi(str)
+	if err != nil {
+		fmt.Printf("Input error: %s", str)
+		return
+	}
+
+	fmt.Printf("Output : %d\n", num*num)
+}
