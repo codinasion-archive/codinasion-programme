c0474860b7fcf221b7f37cef7a9e034fd41ae1a0
Lorenzo Rossi <rossilor95@outlook.it>
2022-06-17 11:36:13 +0200

    Add Palindrome.cs
    Former-commit-id: 0ab6843743b8c281ebc1fb9916b65a69cff47124

---
+++ programme/palindrome-number/Palindrome.cs
@@ -0,0 +1,62 @@
+﻿// Write a program to check palindrome number
+
+namespace SpecialNumbers
+{
+    public class Palindrome
+    {
+
+        public static double Reverse(double number)
+        {
+            double remainder, reverse = 0;
+            while (number > 0)
+            {
+                remainder = number % 10;
+                reverse = reverse * 10 + remainder;
+                number = Math.Floor(number / 10);
+            }
+            return reverse;
+        }
+
+        public static bool IsPalindrome(double number)
+        {
+            if (number < 0)
+            {
+                return false;
+            }
+            if (number < 10)
+            {
+                return true;
+            }
+            return number == Reverse(number);
+        }
+
+        public static void PalindromeConsoleUtility(double number)
+        {
+            if (IsPalindrome(number))
+            {
+                Console.WriteLine("Output : Palindrome number");
+            }
+            else
+            {
+                Console.WriteLine("Output : Not palindrome number");
+            }
+        }
+
+
+        public static void Main()
+        {
+            Console.Write("Input  : ");
+
+            double number;
+            if (double.TryParse(Console.ReadLine(), out number))
+            {
+                PalindromeConsoleUtility(number);
+            }
+            else
+            {
+                Console.WriteLine("Invalid Input");
+            }
+        }
+
+    }
+}
