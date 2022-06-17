557e6fc989e9802ad17abd31e94072b831c1c764
Lorenzo Rossi <rossilor95@outlook.it>
2022-06-17 11:03:54 +0200

    Add InchesToFeet.cs
    Former-commit-id: 0622253e03b5f93b0c2a4aeeb721690ba50c221f

---
+++ programme/inches-to-feet/InchesToFeet.cs
@@ -0,0 +1,25 @@
+﻿// Write a programme to convert inches to feet
+
+namespace UnitConversion
+{
+    public class InchesToFeet
+    {
+        public static double ConvertInchesToFeet(double inches) => inches / 12.0;
+
+        public static void Main()
+        {
+            Console.Write("Input  : ");
+
+            double inches;
+            if (double.TryParse(Console.ReadLine(), out inches))
+            {
+                double feet = ConvertInchesToFeet(inches);
+                Console.WriteLine("Output : " + Math.Round(feet, 2));
+            }
+            else
+            {
+                Console.WriteLine("Invalid Input");
+            }
+        }
+    }
+}
