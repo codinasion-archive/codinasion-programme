1436327492f688113537ebf46989bbb64b5dd344
Lorenzo Rossi <rossilor95@outlook.it>
2022-06-17 11:13:53 +0200

    Add FeetToInches.cs

---
+++ programme/feet-to-inches/FeetToInches.cs
@@ -0,0 +1,25 @@
+﻿// Write a programme to convert feet to inches
+
+namespace UnitConversion
+{
+    public class FeetToInches
+    {
+        public static double ConvertFeetToInches(double feet) => feet * 12.0;
+
+        public static void Main()
+        {
+            Console.Write("Input  : ");
+
+            double feet;
+            if (double.TryParse(Console.ReadLine(), out feet))
+            {
+                double inches = ConvertFeetToInches(feet);
+                Console.WriteLine("Output : " + Math.Round(inches, 2));
+            }
+            else
+            {
+                Console.WriteLine("Invalid Input");
+            }
+        }
+    }
+}
