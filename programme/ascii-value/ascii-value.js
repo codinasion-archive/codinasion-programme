20e689275fd7675c2bb6026ba3cb56626e6ea267
12533 <12533@LAPTOP-0TQKCDVO>
2022-06-17 11:32:13 -0700

    Added ascii-value.js' which returns the value of a character in ASCII value
    Former-commit-id: af3caef5e2d0840b51b44caef4604546c9a5f6a3

---
+++ programme/ascii-value/ascii-value.js
@@ -0,0 +1,9 @@
+console.log(ASCII value of a Character);
+
+function character_to_ascii( n )
+{
+    return n.charCodeAt(0);
+}
+
+//  insert the character below
+console.log( character_to_ascii( Z)  );
