7b002c17e2c835c9014287e7790af74b6306b2ff
Umaxyon <umaxyon@gmail.com>
2022-06-17 23:09:26 +0900

    Added table.go

---
+++ programme/multiplication-table/table.go
@@ -0,0 +1,24 @@
+// Write a program to output the multiplication table
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	var str string
+
+	fmt.Print("Input : ")
+	fmt.Scan(&str)
+
+	num, err := strconv.Atoi(str)
+	if err != nil {
+		fmt.Printf("Input error: %s", str)
+		return
+	}
+
+	for i := 1; i <= 10; i++ {
+		fmt.Printf("%d * %d = %d\n", num, i, num*i)
+	}
+}
