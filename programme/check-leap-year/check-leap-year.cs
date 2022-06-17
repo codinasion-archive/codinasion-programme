2b74f962c1e144c73c109da98261806be98494a0
Umaxyon <umaxyon@gmail.com>
2022-06-17 21:47:04 +0900

    Added check-leap-year.cs
    Former-commit-id: d94e5367c2992f3321da70626b89048a7b37e48e

---
+++ programme/check-leap-year/check-leap-year.cs
@@ -0,0 +1,18 @@
+// Write a programme to check leap year
+using System;
+
+public class CheckLeapYear
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Input  : ");
+        if (int.TryParse(Console.ReadLine(), out int num))
+        {
+            Console.WriteLine("Output :{0} Leap Year", DateTime.IsLeapYear(num) ? "" : " Not a");
+        }
+        else
+        {
+            Console.WriteLine("Input Err");
+        }
+    }
+}
