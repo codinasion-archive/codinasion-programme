f4d8af9d3f1ce83702d5b7f0a9452fc9967deb73
d-coder111 <82580017+d-coder111@users.noreply.github.com>
2022-06-18 23:34:04 +0530

    Create BetterEvenOrOdd.java
    Former-commit-id: 5048e313aeff54c9f7775dee6dc17bd0ef25a3e5

---
+++ programme/even-or-odd-number/BetterEvenOrOdd.java
@@ -0,0 +1,23 @@
+// A program to check if the entered digit is even or odd using bitwise AND operator(Optimized method)
+
+import java.util.Scanner;
+
+public class EvenOrOdd {
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int num;
+
+    System.out.print("Enter the number : ");
+    num = sc.nextInt();
+    
+    if ((num & 1) == 1) {
+            System.out.println("\nOutput : Even Number");
+        }
+    
+    else {
+            System.out.println("\nOutput : Odd Number");
+        }
+    sc.close();
+  }
+}
