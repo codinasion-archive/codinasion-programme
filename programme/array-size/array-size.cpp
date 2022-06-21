dc0cedd315fe58effed81fec10fb501434448581
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-06-21 12:34:29 +0530

    Update array-size.cpp

--- programme/array-size/array-size.cpp
+++ programme/array-size/array-size.cpp
@@ -1,10 +1,24 @@
-//C++ program to get length of int array
+// write a program to get length of int array
 
 #include <iostream>
 using namespace std;
-int main() {
-   int arr[5] = {4, 1, 8, 2, 9};
-   int len = sizeof(arr)/sizeof(arr[0]);
-   cout << "The length of the array is: " << len;
-   return 0;
+int main()
+{
+    int arr[5] = {4, 1, 8, 2, 9};
+    int len = sizeof(arr) / sizeof(arr[0]);
+    cout << "Array : { ";
+    for (int i = 0; i < len; i++)
+    {
+        if (i == len - 1)
+        {
+            cout << arr[i] << " }" << endl;
+        }
+        else
+        {
+            cout << arr[i] << ", ";
+        }
+    }
+
+    cout << "\nArray length : " << len << endl;
+    return 0;
 }
