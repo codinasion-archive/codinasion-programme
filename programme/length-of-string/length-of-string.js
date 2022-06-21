69127f0ddef81f5399a93ffcd98c0890c206a0e0
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-06-21 12:46:23 +0530

    Update length-of-string.js

--- programme/length-of-string/length-of-string.js
+++ programme/length-of-string/length-of-string.js
@@ -1,11 +1,13 @@
+// Write a program to print the length of a string
+
 const readline = require("readline");
 
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
+  input: process.stdin,
+  output: process.stdout,
 });
 
 rl.question("Input  : ", (inputString) => {
-    console.log("Output :", inputString.length);
-    rl.close();
+  console.log("Output :", inputString.length);
+  rl.close();
 });
