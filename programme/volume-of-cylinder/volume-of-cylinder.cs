7dba6344ed2ee4238b85d0474bfccc8396fe0f70
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-06-21 18:29:58 +0530

    Update volume-of-cylinder.cs
    Former-commit-id: 20be651a5ca33d53692da0df764714002ab9122b

--- programme/volume-of-cylinder/volume-of-cylinder.cs
+++ programme/volume-of-cylinder/volume-of-cylinder.cs
@@ -1,22 +1,21 @@
 // Write a program to compute the volume of a Cylinder
-
 using System;
 
 public class VolumeOfCylinder
 {
-	public static void Main()
-	{
-        	// taking radius of cylinder
-		Console.Write("Enter the radius: ");
-		int radius = int.Parse(Console.ReadLine());
+    public static void Main()
+    {
+        // taking radius of cylinder
+        Console.Write("Enter the radius : ");
+        int radius = int.Parse(Console.ReadLine());
 
-        	// taking height of cylinder
-		Console.Write("Enter the height: ");
-		int height = int.Parse(Console.ReadLine());
+        // taking height of cylinder
+        Console.Write("Enter the height : ");
+        int height = int.Parse(Console.ReadLine());
 
-        	// calculating Volume of cylinder
-		double volume = Math.PI * radius * radius * height;
+        // calculating Volume of cylinder
+        double volume = Math.PI * radius * radius * height;
 
-		Console.WriteLine($"Volume of cylinder : {Math.Round(volume, 2)}");
-	}
+        Console.WriteLine($"\nVolume of cylinder : {Math.Round(volume, 2)}");
+    }
 }
