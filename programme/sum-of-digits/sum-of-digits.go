d52e4236ffdc7552e9a0a2cd955a0894742010d6
Umaxyon <umaxyon@gmail.com>
2022-06-22 19:06:18 +0900

    Added sum-of-digits.go
    Former-commit-id: 2d2a54bbdfb12639ec8c5e3cc1c7b83fc83deb86

---
+++ programme/sum-of-digits/sum-of-digits.go
@@ -0,0 +1,29 @@
+// Write a program to print sum of digits.
+
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	var str string
+
+	fmt.Print("Input : ")
+	fmt.Scan(&str)
+
+	num, err := strconv.Atoi(str)
+	if err != nil {
+		fmt.Printf("Input error: %s", str)
+		return
+	}
+
+	var sum, d int
+	for num != 0 {
+		d = num % 10
+		sum = sum + d
+		num = num / 10
+	}
+	fmt.Printf("Sum of the digits : %d\n", sum)
+}
