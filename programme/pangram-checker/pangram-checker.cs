bd983cabde1c82654e89d82a4a0853ecd5745318
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-06-25 10:03:36 +0530

    Update pangram-checker.cs
    Former-commit-id: 8b0291de5f524fe87f90d18a9d523fe781a4f435

--- programme/pangram-checker/pangram-checker.cs
+++ programme/pangram-checker/pangram-checker.cs
@@ -1,5 +1,6 @@
 // Write a programme to check Pangram sentence
 using System;
+using System.Collections.Generic;
 
 public class PangramChecker
 {
@@ -18,12 +19,14 @@ public class PangramChecker
 
         for (int i = 0; i < chars.Length; i++)
         {
-            int c = (int)chars[i];
+            int c = (int) chars[i];
 
             // 97 == 'a', 122 == 'z' (ascii code)
             if (97 <= c && c <= 122) hs.Add(chars[i]);
         }
 
-        Console.WriteLine("Output : {0}Pangram Sentence", hs.Count == 26 ? "" : "Not a ");
+        Console
+            .WriteLine("Output : {0}Pangram Sentence",
+            hs.Count == 26 ? "" : "Not a ");
     }
 }
