ab93b6f7a124cff84b1ec1a371b43db03b85dc08
0xRenegade <therenegadecoding@gmail.com>
2022-06-25 13:47:27 -0500

    issue #919 area of right triangle

---
+++ programme/area-of-right-angle-triangle/area-of-right-angle-triangle.php
@@ -0,0 +1,21 @@
+<?
+// Write a programme to find the area of a right angle triangle
+
+// EQUATION: area = 1/2 * base * height
+
+function calculate_area(int $base, int $height) {
+
+    $area = ($base * $height) / 2;
+
+    return $area;
+
+}
+
+$base = readline('Enter the base: ');
+$height = readline('Enter the height: ');
+$area = calculate_area($base, $height);
+
+print "\nBase : $base";
+print "\nHeight: $height";
+print "\nCalculated Area: $area";
+?>
