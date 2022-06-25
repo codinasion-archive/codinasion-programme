2b23098cd48150c4a6de590028be00ee233b7fb5
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-06-25 11:10:17 +0530

    Update array-elements-slice.js
    Former-commit-id: bcf8e13be807cd9f534360cc152714c73eb1e0c4

--- programme/array-elements-slice/array-elements-slice.js
+++ programme/array-elements-slice/array-elements-slice.js
@@ -1,27 +1,31 @@
-// Array element slice function
+// Write a programme to generate new array with sliced elements of old array
 
-array_list = ['abcd', 'efgh', 'ijklmn','opq','rstuvw','xyz']
-// console.log(array_list)
+// define array
+array_list = ["abcd", "efgh", "ijklmn", "opq", "rstuvw", "xyz"];
+console.log("Array :", array_list);
 
 // function arguments (arrayList) (start slice from -number) (end slice with -number)
-function arrayLetterRange(a_list, start=0, end=2){
-    
-    // store slice value in result
-    let result = []
-    for (let i = 0; i < a_list.length; i++) {
-        const element = a_list[i];    
-        let sliced_val = element.slice(start-1,end)
+function arrayLetterRange(a_list, start = 0, end = 2) {
+  // store slice value in result
+  let result = [];
+  for (let i = 0; i < a_list.length; i++) {
+    const element = a_list[i];
+    let sliced_val = element.slice(start - 1, end);
 
-        // check slice value if not empty 
-        if(sliced_val != ''){
-            result.push(sliced_val)
-        }
+    // check slice value if not empty
+    if (sliced_val != "") {
+      result.push(sliced_val);
     }
-    return result
+  }
+  return result;
 }
 
+// define range
+let start = 2;
+let end = 3;
 
 // call the function
-let array_result = arrayLetterRange(array_list, 2,5)
-console.log(val)
-
+let array_result = arrayLetterRange(array_list, start, end);
+console.log("Start :", start);
+console.log("End   :", end);
+console.log("\nSliced Array :", array_result);
