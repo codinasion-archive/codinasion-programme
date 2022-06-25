a4748adaa9e3f8e8d2bd6686d62b085ee6bed447
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-06-25 11:32:25 +0530

    Update find-negative-number.js

--- programme/negative-of-a-number/find-negative-number.js
+++ programme/negative-of-a-number/find-negative-number.js
@@ -1,28 +1,24 @@
-// find the negative number of a number
+// Write a programme to find negative of a number
 // quick def (9 = -9 and -9 = 9 and string = not a number)
 
-
-
-// let number = "9"
-let number = 89.09
+let number = 89.09;
+console.log("Input  :", number);
 
 function findNegativeNum(num) {
-    // this keep you float value in float
-    if (typeof num === 'number') {
-        let negative = num - (num*2)
-        return negative;
+  // this keep you float value in float
+  if (typeof num === "number") {
+    let negative = num - num * 2;
+    return negative;
+  } else {
+    // try to convert string into number if possible ("9") != number
+    if (parseInt(num)) {
+      let negative = num - num * 2;
+      return negative;
+    } else {
+      return "Can not convert into number\nNot a Number";
     }
-    else{
-        // try to convert string into number if possible ("9") != number
-        if (parseInt(num)) {
-            let negative = num - (num*2)
-            return negative;
-        }
-        else{
-            return "Can not convert into number\nNot a Number"
-        }
-    }
+  }
 }
 
-let result = findNegativeNum(number)
-console.log(result)
+let result = findNegativeNum(number);
+console.log("Output :", result);
