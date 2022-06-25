0277ce01133d2841791cc6411b806e6dcc6d158e
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-06-25 14:04:51 +0530

    Update array-elements-slice.go
    Former-commit-id: d99445b247aca02e525fce73cc58422256afd5de

--- programme/array-elements-slice/array-elements-slice.go
+++ programme/array-elements-slice/array-elements-slice.go
@@ -11,16 +11,16 @@ func main() {
 	var start int = 2
 	var end int = 3
 
-	fmt.Printf("Orignal List: ")
+	fmt.Printf("Orignal List : ")
 	fmt.Println(list)
 
-/*
-        1. "range x" will return index, value pair or iterable entity 'x'
-        2. expression "variable_name[x:y]" returns a list of element within range having  index >= x and index < y
-*/
+	/*
+	   1. "range x" will return index, value pair or iterable entity 'x'
+	   2. expression "variable_name[x:y]" returns a list of element within range having  index >= x and index < y
+	*/
 	for index, value := range list {
-		list [index] = value [start-1 : end]
+		list[index] = value[start-1 : end]
 	}
-	fmt.Printf("Modified List: ")
+	fmt.Printf("Sliced List  : ")
 	fmt.Println(list)
 }
