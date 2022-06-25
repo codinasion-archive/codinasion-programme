a4120f4a411a93ee677acdcb1851a553ea452b3b
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-06-25 11:10:23 +0530

    Update array-elements-slice.py
    Former-commit-id: 40588379da7b4afcfd370db2d006f647fe2ce1e1

--- programme/array-elements-slice/array-elements-slice.py
+++ programme/array-elements-slice/array-elements-slice.py
@@ -1,7 +1,8 @@
-# Array element slice function
+# Write a programme to generate new array with sliced elements of old array
 
-array_list = ['abcd', 'efgh', 'ijklmn','opq','rstuvw','xyz']
-print(array_list)
+# define array
+array_list = ['abcd', 'efgh', 'ijklmn', 'opq', 'rstuvw', 'xyz']
+print("Array :", array_list)
 
 # function arguments (arrayList) (start slice from -number) (end slice with -number)
 def arrayLetterRange(a_list, start=5, end=1):
@@ -11,13 +12,20 @@ def arrayLetterRange(a_list, start=5, end=1):
     for i in a_list:
         sliced_val = i[start-1: end]
 
-        # check slice value if not empty 
+        # check slice value if not empty
         if i != '':
             result.append(sliced_val)
 
     return result
 
 
+# define range
+range_start = 2
+range_end = 3
+print("Start :", range_start)
+print("End   :", range_end)
+print()
+
 # call the function
-array_result = arrayLetterRange(array_list, 2,3)
-print(array_result)
+array_result = arrayLetterRange(array_list, range_start, range_end)
+print("Sliced Array :", array_result)
