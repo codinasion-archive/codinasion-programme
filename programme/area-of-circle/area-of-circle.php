703f13f9874eccf3821a132995cd720abddd7a9e
0xRenegade <therenegadecoding@gmail.com>
2022-06-26 11:46:23 -0500

    issue #933 area of circle

---
+++ programme/area-of-circle/area-of-circle.php
@@ -0,0 +1,25 @@
+<?php
+
+// Write a programme to find the area of circle
+
+function calculate_area($r) {
+
+    if (!is_numeric($r)) {
+
+        return 'Invalid input: the input is not a number.';
+
+    } else {
+
+        return pi() * $r * $r;
+
+    }
+
+}
+
+$r = readline("Enter radius: ");
+$area = calculate_area($r);
+
+print "\nRadius: {$r}";
+print "\nArea: {$area}";
+
+?>
