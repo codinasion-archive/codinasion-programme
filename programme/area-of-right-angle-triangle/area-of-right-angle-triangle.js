439ec3e544cf892f13c177a8fbcfaabe46ea9f1d
Jason Chan <jlc893@gmail.com>
2022-06-25 21:59:40 -0400

    Area of right angle triangle js

---
+++ programme/area-of-right-angle-triangle/area-of-right-angle-triangle.js
@@ -0,0 +1,4 @@
+const base = prompt('Enter the base of the triangle: ');
+const height = prompt('Enter the height of the triangle: ');
+
+console.log(`The area of the triangle is: ${(base * height) / 2}`);
