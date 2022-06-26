c41656a42742254e7b605ce850cd364d176a1b92
0xRenegade <therenegadecoding@gmail.com>
2022-06-25 20:15:05 -0500

    fix area returning as NAN (negative number)

--- programme/area-of-triangle/area-of-triangle.php
+++ programme/area-of-triangle/area-of-triangle.php
@@ -1,4 +1,4 @@
-<?
+<?php
 // Write a PHP programme to find area of triangle.
 
 // Formula: 
@@ -9,9 +9,22 @@
 function calculate_area(int|float $a, int|float $b, int|float $c) {
 
     $s = ($a + $b + $c) / 2;
-    $area = sqrt($s * ($s - $a) * ($s - $b) * ($s - $c));
+    // return $s;
+    $a = ($s - $a);
+    $b = ($s - $b);
+    $c = ($s - $c);
 
-    return $area;
+    if ($a < 0 || $b < 0 || $c < 0) {
+
+        // No negative numbers for formula
+        return false;
+
+    } else {
+
+        $area = $s * $a * $b * $c;
+        return sqrt($area);
+
+    }
 
 }
 
@@ -20,9 +33,17 @@ $b = readline("Enter the length of the second side of the triangle: ");
 $c = readline("Enter the length of the third side of the triangle: ");
 $area = calculate_area($a, $b, $c);
 
-print "\nFirst side length: {$a}";
-print "\nSecond side length: {$b}";
-print "\nThird side length: {$c}";
-print "\nCalculated Area: {$area}";
+if (!$area) {
+
+    print "\nNegative number detected, please try again with new values";
+
+} else {
+
+    print "\nFirst side length: {$a}";
+    print "\nSecond side length: {$b}";
+    print "\nThird side length: {$c}";
+    print "\nCalculated Area: {$area}";
+
+}
 
 ?>
