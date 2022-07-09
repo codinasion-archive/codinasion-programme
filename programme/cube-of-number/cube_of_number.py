aea062556a0d1b69e5dfd58257408f31d47e7c6f
Seraph★776 <72005563+seraph776@users.noreply.github.com>
2022-07-09 13:26:49 -0400

    Create cube_of_number.py

---
+++ programme/cube-of-number/cube_of_number.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+
+def cube_of_number()-> None:
+  """Returns the cube of a number"""
+    n = int(input("Input  : "))
+    print("Output :", pow(n, 3))
+
+
+if __name__ == '__main__':
+    cube_of_number()
