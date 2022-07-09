b9af7ef3b8229d7e8a04ebaaa3c1b9e56fdf3096
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-07-10 04:47:53 +0530

    Update cube_of_number.py

--- programme/cube-of-number/cube_of_number.py
+++ programme/cube-of-number/cube_of_number.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
+# Write a programme to find cube of a number
 
 
-def cube_of_number()-> None:
-  """Returns the cube of a number"""
+def cube_of_number() -> None:
+    """Returns the cube of a number"""
     n = int(input("Input  : "))
     print("Output :", pow(n, 3))
 
