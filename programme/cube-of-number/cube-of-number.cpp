27757421fb8b610b4a8d2f821bfa92436da24dda
Krish Shah <shahkrish2016@gmail.com>
2022-07-10 19:43:51 -0400

    added cube-of-number.cpp

---
+++ programme/cube-of-number/cube-of-number.cpp
@@ -0,0 +1,18 @@
+/*
+Name: Krish Shah
+*/
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main() {
+    double number;
+
+    cout << "Input  : ";
+    cin >> number;
+
+    cout << "Output : " << pow(number, 3);
+
+    return 0;
+}
