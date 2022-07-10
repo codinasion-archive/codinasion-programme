b02fdad3a6a5bc3daf798c4e81709b5e9bb7a624
Seraph★776 <72005563+seraph776@users.noreply.github.com>
2022-07-10 09:02:46 -0400

    Create volume_of_cone.py

---
+++ programme/volume-of-cone/volume_of_cone.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# Write a programme to find cube of a number
+from math import pi
+
+
+def volume_of_cone() -> None:
+    """Returns the area of a cone"""
+    ht = {0: 'radius', 1: 'height'}
+    radius, height = [int(input(f"Input  {ht.get(i)}: ")) for i in range(2)]
+    print("Output :", f'{1 / 3 * pi * pow(radius, 2) * height:.5f}')
+
+
+if __name__ == '__main__':
+    volume_of_cone()
