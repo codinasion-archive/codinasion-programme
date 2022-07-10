d4223d33a32f94651369ba91f3efca2744bf3c7b
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-07-10 07:17:00 +0530

    Update VolumeOfCube.java

--- programme/volume-of-cube/VolumeOfCube.java
+++ programme/volume-of-cube/VolumeOfCube.java
@@ -1,15 +1,16 @@
+// Write a programme to find volume of a cube
 import java.util.Scanner;
 
 class VolumeOfCube {
-  
+
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    System.out.println("Input : ");
+    System.out.print("Input  : ");
     double input = sc.nextDouble();
     System.out.println("Output : " + volumeOfCube(input));
     sc.close();
   }
-  
+
   private static double volumeOfCube(double num) {
     return Math.pow(num, 3);
   }
