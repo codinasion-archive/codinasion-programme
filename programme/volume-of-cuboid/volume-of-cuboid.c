458bf11bad44703d40ce1ef9421a91aeef8a35ed
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-07-10 10:31:49 +0530

    Update volume-of-cuboid.c

--- programme/volume-of-cuboid/volume-of-cuboid.c
+++ programme/volume-of-cuboid/volume-of-cuboid.c
@@ -1,10 +1,22 @@
+// Write a programme to find volume of a cuboid
+
 #include <stdio.h>
 
-int main(){
-  int length,width,height,Volume;
-  printf("Enter length,width and height of Cuboid.\n");
-  scanf("%d%d%d", &length, &width, &height);
-  Volume = length*width*height;
-  printf("Volume:%d",Volume);
-  return 0;
+int main()
+{
+    int length, width, height, Volume;
+
+    // Input
+    printf("Enter the length of cuboid : ");
+    scanf("%d", &length);
+    printf("Enter the width  of cuboid : ");
+    scanf("%d", &width);
+    printf("Enter the height of cuboid : ");
+    scanf("%d", &height);
+
+    // calculate and print volume
+    Volume = length * width * height;
+    printf("\nVolume of Cuboid : %d\n", Volume);
+
+    return 0;
 }
