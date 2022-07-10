369e1adb1dc0f9cf3fbc3818dccd7a14c5309460
PaoloFer <paolo.ferrari.vr.05@gmail.com>
2022-07-10 15:22:36 +0200

    Volume of cone

---
+++ programme/volume-of-cone/volume-of-cone.py
@@ -0,0 +1,19 @@
+# Write a program to compute the volume of a Cone
+import math
+
+'''
+    Author: PaoloFer
+    Date: 10/05/2022
+'''
+
+# Get radius from user
+enter_radius = float(input('Radius : '))
+
+# Get height from user
+enter_height = float(input('Height : '))
+
+# Calculate volume
+volume =  1/3 * math.pi * (enter_radius)**2 * enter_height 
+
+# Print result
+print("\nVolume : {:0.2f}".format((volume)))
