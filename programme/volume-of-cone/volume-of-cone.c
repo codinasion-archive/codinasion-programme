665f57dbb9528d4fe23ccfce16cbeae04ee86a41
Bryant Boatright <bryantboatright@icloud.com>
2022-07-10 14:37:21 -0500

    edit output

--- programme/volume-of-cone/volume-of-cone.c
+++ programme/volume-of-cone/volume-of-cone.c
@@ -4,13 +4,16 @@ int main(int argc, char *argv[])
 {
 	double pi = 3.14159;
 	double radius,height,volume;
-
-	scanf("enter radius of cone: %lf", &radius);
-	scanf("enter height of cone: %lf", &radius);
+	printf("enter radius : ");
+	scanf("%lf", &radius);
+	printf("enter height : ");
+	scanf("%lf", &height);
 
 	volume = pi * radius * radius * height / 3.0;
 
-	printf("volume of cone: %lf", volume);
+	printf("radius : %lf\n", radius);
+	printf("height : %lf\n\n", height);
+	printf("volume of cone: %lf\n", volume);
     return 0;
 }
 
