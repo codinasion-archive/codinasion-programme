5b5650241b42b9eb57fd211a0aa06a37c1371bb1
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-07-10 15:35:44 +0530

    Update volume-of-cube.c

--- programme/volume-of-cube/volume-of-cube.c
+++ programme/volume-of-cube/volume-of-cube.c
@@ -1,13 +1,19 @@
+// Write a programme to find volume of a cube
 #include <stdio.h>
-int Vol_cube(int s){
-  int Volume = s*s*s;
+
+int Vol_cube(int s)
+{
+  int Volume = s * s * s;
   return Volume;
 }
 
-int main(){
+int main()
+{
   int side;
-  printf("Enter cube side :");
+  printf("Enter cube side : ");
   scanf("%d", &side);
-  printf("Volume:%d", Vol_cube(side));
+
+  printf("\nVolume of cube : %d\n", Vol_cube(side));
+
   return 0;
 }
