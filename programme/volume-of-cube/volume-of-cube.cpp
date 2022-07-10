a667453c227bf27af0a8da63afd471f0123709e2
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-07-10 15:35:46 +0530

    Update volume-of-cube.cpp

--- programme/volume-of-cube/volume-of-cube.cpp
+++ programme/volume-of-cube/volume-of-cube.cpp
@@ -1,15 +1,21 @@
+// Write a programme to find volume of a cube
 #include <iostream>
+
 using namespace std;
 
-int Vol_cube(int s){
-  int Vol = s*s*s;
-  return Vol;
+int Vol_cube(int s)
+{
+    int Vol = s * s * s;
+    return Vol;
 }
 
-int main(){
-  int side;
-  cout<<"Enter cube side :"<<endl;
-  cin>>side;
-  cout<<"Volume:"<<Vol_cube(side);
-  return 0;
+int main()
+{
+    int side;
+    cout << "Enter cube side : ";
+    cin >> side;
+
+    cout << "\nVolume of cube  : " << Vol_cube(side) << endl;
+
+    return 0;
 }
