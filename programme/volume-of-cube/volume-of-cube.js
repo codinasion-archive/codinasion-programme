67b5fcda87e6ea2d6ef3ff634023a65d1a85f9a9
PaoloFer <paolo.ferrari.vr.05@gmail.com>
2022-07-10 17:24:19 +0200

    javascript cube volume

---
+++ programme/volume-of-cube/volume-of-cube.js
@@ -0,0 +1,8 @@
+// Write a program to compute the volume of a Cube
+
+function volumeCube(cubeSide) {
+    return cubeSide ** 3;
+  }
+  
+  const cubeSide = 2;
+  console.log(`The area of square for ${cubeSide} is ${volumeCube(cubeSide)}`);
