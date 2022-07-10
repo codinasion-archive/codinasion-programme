3b535f88036105d6837cb2bd60e13eb4a58c8cf2
Seraph★776 <72005563+seraph776@users.noreply.github.com>
2022-07-10 18:53:05 -0400

    Update volume_of_cone.py

--- programme/volume-of-cone/volume_of_cone.py
+++ programme/volume-of-cone/volume_of_cone.py
@@ -4,7 +4,7 @@ from math import pi
 
 
 def volume_of_cone() -> None:
-    """Returns the area of a cone"""
+    """Returns the volume of a cone"""
     ht = {0: 'radius', 1: 'height'}
     radius, height = [int(input(f"Input  {ht.get(i)}: ")) for i in range(2)]
     print("Output :", f'{1 / 3 * pi * pow(radius, 2) * height:.5f}')
