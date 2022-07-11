1c3b60be3d124421861b2e68bd69e6aac27fa41a
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-07-11 09:21:48 +0530

    Update volume-of-cube.js

--- programme/volume-of-cube/volume-of-cube.js
+++ programme/volume-of-cube/volume-of-cube.js
@@ -1,8 +1,9 @@
 // Write a program to compute the volume of a Cube
 
 function volumeCube(cubeSide) {
-    return cubeSide ** 3;
-  }
-  
-  const cubeSide = 2;
-  console.log(`The area of Cube for ${cubeSide} is ${volumeCube(cubeSide)}`);
+  return cubeSide ** 3;
+}
+
+const cubeSide = 2;
+console.log("Side   of Cube : " + cubeSide);
+console.log("Volume of Cube : " + volumeCube(cubeSide));
