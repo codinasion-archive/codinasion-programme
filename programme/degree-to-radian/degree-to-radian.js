3d5d198db626b80b1b79aa9d81ffae5d44222d9d
PaoloFer <paolo.ferrari.vr.05@gmail.com>
2022-07-11 15:46:22 +0200

    new file:   programme/degree-to-radian/degree-to-radian.js 	new file:   programme/radian-to-degree/radian-to-degree.js

---
+++ programme/degree-to-radian/degree-to-radian.js
@@ -0,0 +1,7 @@
+//Write a programme to convert degree to radian
+function degrees_to_radians(degrees){
+  var pi = Math.PI;
+  return degrees * (pi/180);
+}
+
+console.log(degrees_to_radians(1));//0.017453292519943295
---
+++ programme/radian-to-degree/radian-to-degree.js
@@ -0,0 +1,7 @@
+//Write a programme to convert radian to degree
+function radian_to_degree(degrees){
+    var pi = Math.PI;
+    return degrees * (180/pi);
+  }
+  
+  console.log(radian_to_degree(1));//57.29577951308232
