a897d804c4ef046d39fe8c04956d4b3379541742
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-07-12 02:00:01 +0530

    Update radian-to-degree.js

--- programme/radian-to-degree/radian-to-degree.js
+++ programme/radian-to-degree/radian-to-degree.js
@@ -1,7 +1,10 @@
-//Write a programme to convert radian to degree
-function radian_to_degree(degrees){
-    var pi = Math.PI;
-    return degrees * (180/pi);
-  }
-  
-  console.log(radian_to_degree(1));//57.29577951308232
+// Write a programme to convert radian to degree
+
+function radian_to_degree(degrees) {
+  var pi = Math.PI;
+  return degrees * (180 / pi);
+}
+
+radian = 1;
+console.log("Input  : " + radian);
+console.log("Output : " + radian_to_degree(radian).toFixed(5));
