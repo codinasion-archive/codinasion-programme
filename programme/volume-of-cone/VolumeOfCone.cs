b85d49c896a44234d4238bdc5ee0ba15f85c3546
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-07-11 09:11:07 +0530

    Update VolumeOfCone.cs

--- programme/volume-of-cone/VolumeOfCone.cs
+++ programme/volume-of-cone/VolumeOfCone.cs
@@ -1,4 +1,6 @@
 // Write a program to find the volume of a cone
+using System;
+
 namespace Codinasion.Programme
 {
     internal class VolumeOfCone
@@ -8,8 +10,12 @@ namespace Codinasion.Programme
             double radius = GetRadius();
             double height = GetHeight();
             Cone cone = Cone.Create(height, radius);
-            Console.WriteLine($"\nA cone with a radius of {cone.Radius} and a height of {cone.Height} has a volume of {cone.Volume}");
+            Console
+                .WriteLine($"\nA cone with a radius of {
+                    cone.Radius} and a height of {cone.Height} has a volume of {
+                    string.Format("{0:0.00000}", cone.Volume)}");
         }
+
         private static double GetHeight()
         {
             return GetConsoleInput("Height");
@@ -27,7 +33,7 @@ namespace Codinasion.Programme
             while (input == null)
             {
                 Console.Write($"Enter {dimension} : ");
-                string? consoleInput = Console.ReadLine();
+                string consoleInput = Console.ReadLine();
                 if (double.TryParse(consoleInput, out double parsedInput))
                 {
                     if (parsedInput > 0)
@@ -38,28 +44,32 @@ namespace Codinasion.Programme
                     {
                         Console.WriteLine("Input must be greater than 0");
                     }
-
                 }
                 else
                 {
                     Console.WriteLine($"Unable to parse input {consoleInput}");
-                };
+                }
             }
             return input.Value;
         }
     }
+
     internal class Cone
     {
         public double Height { get; protected set; }
+
         public double Radius { get; protected set; }
+
         // (1/3) must be cast to a double or else it'll treat it as an int and return 0
-        public double Volume => (double)1 / 3 * Math.PI * Math.Pow(Radius, 2) * Height;
+        public double Volume =>
+            (double) 1 / 3 * Math.PI * Math.Pow(Radius, 2) * Height;
 
         private Cone(double height, double radius)
         {
             Height = height;
             Radius = radius;
         }
+
         public static Cone Create(double height, double radius)
         {
             return new Cone(height, radius);
