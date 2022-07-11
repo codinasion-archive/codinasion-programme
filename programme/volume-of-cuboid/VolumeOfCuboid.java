4df2bf823cad007216578b84aeac96b593a5f4d7
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-07-11 11:02:02 +0530

    Update and rename volume-of-cuboid.java to VolumeOfCuboid.java

--- programme/volume-of-cuboid/VolumeOfCuboid.java
+++ programme/volume-of-cuboid/VolumeOfCuboid.java
@@ -4,19 +4,24 @@ import java.util.Scanner;
 public class VolumeOfCuboid {
 
   private static double computeCuboitVolume(double l, double h, double w) {
-    return l*h*w;
+    return l * h * w;
   }
 
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    System.out.println("Length : ");
+
+    System.out.print("Length : ");
     double l = sc.nextDouble();
-    System.out.println("Width : ");
+
+    System.out.print("Width  : ");
     double w = sc.nextDouble();
-    System.out.println("Height : ");
+
+    System.out.print("Height : ");
     double h = sc.nextDouble();
-    double vol = computeCuboitVolume(l,h,w);
+
+    double vol = computeCuboitVolume(l, h, w);
     System.out.printf("\nVolume : %.2f\n", vol);
+
     sc.close();
   }
 }
