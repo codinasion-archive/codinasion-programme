821ef05c88ace4f06ad7c7babd9f626b71b6ac54
Green <career@haydentimms.co.uk>
2022-07-11 22:53:31 +0100

    #1159 added csharp code for counting capital letters in string

---
+++ programme/count-capital-letters-in-string/CountCapitalLetters.cs
@@ -0,0 +1,34 @@
+// write a programme to get the number of capital letters in a string
+namespace Codinasion.Programme
+{
+    internal class CountCapitalLetters
+    {
+
+        public static void Main()
+        {
+
+            string? input = GetInput();
+            var numberOfCapitals = input.Count(c => char.IsUpper(c));
+            Console.WriteLine($"{input} has {numberOfCapitals} capital letters");
+        }
+
+        private static int CountNumberOfCapitalLetters(string stringToCount)
+        {
+            return stringToCount.Count(c => char.IsUpper(c));
+        }
+        private static string GetInput()
+        {
+            string? input = null;
+            while (input == null)
+            {
+                Console.Write("Please enter a string to count the capital letters of : ");
+                var consoleInput = Console.ReadLine();
+                if (!string.IsNullOrEmpty(consoleInput))
+                {
+                    input = consoleInput;
+                }
+            }
+            return input;
+        }
+    }
+}
