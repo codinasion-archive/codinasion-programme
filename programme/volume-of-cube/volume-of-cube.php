ecc0c7d1eb8ea5783a300305ce137c77d3d9e2ef
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-07-11 08:43:42 +0530

    Update volume-of-cube.php

--- programme/volume-of-cube/volume-of-cube.php
+++ programme/volume-of-cube/volume-of-cube.php
@@ -1,28 +1,27 @@
-<?php 
+<?php
 // Write a program to find volume of a cube
 
-function cube($number) {
+function cube($number)
+{
 
     if (!is_numeric($number)) {
 
-        return 'Invalid input: Input is not a number';
-    
-    }elseif ($number < 0) {
+        return 'Invalid input : Input is not a number';
 
-        return "Invalid input: Input cant' be negative";
+    } elseif ($number < 0) {
 
-    }elseif ($number == 0) {
+        return "Invalid input : Input cant' be negative";
 
-        return "Invalid input: Input cant' be 0";
+    } elseif ($number == 0) {
+
+        return "Invalid input : Input cant' be 0";
     }
 
-    return pow($number,3);
+    return pow($number, 3);
 }
 
 $number = readline("Enter cube side  : ");
 $cube = cube($number);
 
-print "\nSide : {$number}";
-print "\nCube  : {$cube}";
-
-?>
+print "\nSide   of Cube : {$number}";
+print "\nVolume of Cube : {$cube}\n";
