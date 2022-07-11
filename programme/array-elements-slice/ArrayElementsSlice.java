8e8d691130fc7ff3b558413da4547a7b80c75757
aukokyong <75824065+aukokyong@users.noreply.github.com>
2022-07-11 12:19:55 +0800

    Add solution for array elements slice

---
+++ programme/array-elements-slice/ArrayElementsSlice.java
@@ -0,0 +1,22 @@
+// Write a Java programme to generate new array with sliced elements of old array.
+
+import java.util.Arrays;
+
+public class ArrayElementsSlice {
+  
+  public static void main(String[] args) {
+    String[] array = new String[]{"abcd", "efgh", "ijklmn", "opq", "rstuvw", "xyz"};
+    int start = 2;
+    int end = 3;
+    System.out.println("Start index : " + start);
+    System.out.println("End index : " + end);
+    System.out.println(Arrays.toString(arrayElementsSlice(array, start, end)));
+  }
+  
+  private static String[] arrayElementsSlice(String[] array, int start, int end) {
+    for(int i = 0; i < array.length; i++) {
+      array[i] = array[i].substring(start - 1, end);
+    }
+    return array;
+  }
+}
