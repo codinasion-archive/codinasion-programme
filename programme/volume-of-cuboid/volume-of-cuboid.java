8fa620b436a687936121ec25be9e0841f0eb513d
irvinavelino <103976767+irvinavelino@users.noreply.github.com>
2022-07-10 23:45:31 -0500

    Create volume-of-cuboid.java

---
+++ programme/volume-of-cuboid/volume-of-cuboid.java
@@ -0,0 +1,22 @@
+// Write a program to compute the volume of a Sphere
+import java.util.Scanner;
+
+public class VolumeOfCuboid {
+
+  private static double computeCuboitVolume(double l, double h, double w) {
+    return l*h*w;
+  }
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Length : ");
+    double l = sc.nextDouble();
+    System.out.println("Width : ");
+    double w = sc.nextDouble();
+    System.out.println("Height : ");
+    double h = sc.nextDouble();
+    double vol = computeCuboitVolume(l,h,w);
+    System.out.printf("\nVolume : %.2f\n", vol);
+    sc.close();
+  }
+}
