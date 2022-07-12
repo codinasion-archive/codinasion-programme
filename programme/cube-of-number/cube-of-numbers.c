35b8a1e9581f27a21e9ba903d27145885ba41752
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-07-12 16:18:21 +0530

    Update cube-of-numbers.c

--- programme/cube-of-number/cube-of-numbers.c
+++ programme/cube-of-number/cube-of-numbers.c
@@ -1,15 +1,15 @@
-// Write a programme to find cube of a number.
+// Write a programme to find cube of a number
 #include <stdio.h>
 
 int main()
 {
-    int number = 0;
-    printf("Enter an integer: ");
-    scanf("%d", &number);
-    
-    int result = number * number * number;
+  int number = 0;
+  printf("Enter an integer: ");
+  scanf("%d", &number);
 
-    printf("Input  : %d", number);
-    printf("Output : %d", result);
-    return 0;
+  int result = number * number * number;
+
+  printf("\nInput  : %d", number);
+  printf("\nOutput : %d\n", result);
+  return 0;
 }
