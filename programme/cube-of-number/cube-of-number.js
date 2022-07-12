ce54635d2acf0e7ef00d65809197127c766f6ed1
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-07-12 16:18:24 +0530

    Update cube-of-number.js

--- programme/cube-of-number/cube-of-number.js
+++ programme/cube-of-number/cube-of-number.js
@@ -1,9 +1,9 @@
-// Write a programme to find cube of a number.
+// Write a programme to find cube of a number
 
 function cube(number) {
-    return Math.pow(number, 3);
+  return Math.pow(number, 3);
 }
 
 const x = 2;
-console.log('Input  :', x);
-console.log('Output :', cube(x));
+console.log("Input  :", x);
+console.log("Output :", cube(x));
