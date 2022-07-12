7eb3651c67f30d9bcd09bb67955be76f35fb23ed
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-07-12 16:29:35 +0530

    Update check-even-or-odd-string.c

--- programme/check-even-or-odd-string/check-even-or-odd-string.c
+++ programme/check-even-or-odd-string/check-even-or-odd-string.c
@@ -3,20 +3,24 @@
 #include <stdio.h>
 #include <string.h>
 
-void check_even(char *input){
-    if (strlen(input) % 2 == 0){
-        printf("Even String\n");
-    } else {
-        printf("Odd String\n");
-    }
+void check_even(char *input)
+{
+  if (strlen(input) % 2 == 0)
+  {
+    printf("Even String\n");
+  }
+  else
+  {
+    printf("Odd String\n");
+  }
 }
 
-int main() {
-    char *first = "hiithere";
-    check_even(first);
+int main()
+{
+  char *first = "hiithere";
+  printf("Input  : %s\n", first);
+  printf("Output : ");
+  check_even(first);
 
-    char *second = "hii";
-    check_even(second);
-
-    return 0;
+  return 0;
 }
