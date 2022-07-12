9f0fa8a11333354a27c5475815a0ff2e67d30dec
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-07-12 16:29:40 +0530

    Update check-even-or-odd-string.py

--- programme/check-even-or-odd-string/check-even-or-odd-string.py
+++ programme/check-even-or-odd-string/check-even-or-odd-string.py
@@ -1,7 +1,9 @@
 # Write a programme to check even or odd string
 
-given_string = input("Input your string: ")
+given_string = input("Input your string : ")
 
+print("\nInput  :", given_string)
+print("Output : ", end="")
 if (len(given_string) % 2 == 0):
     print("Even String")
 else:
