49ce4e5eaa942f9cd2a273da2e4688737115136a
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-07-12 16:40:16 +0530

    Update countCapitalLetters.java

--- programme/count-capital-letters/countCapitalLetters.java
+++ programme/count-capital-letters/countCapitalLetters.java
@@ -1,21 +1,22 @@
+// Write a programme to count capital letters in a string
 import java.util.Scanner;
-public class countCapitalLetters 
-{
-    public static void main(String args[])
-    {
-        String str;
-        int upper=0;
-        Scanner scan=new Scanner(System.in); 
-        System.out.println("Enter the String  ");
-        str=scan.nextLine();
-        for(int i=0; i<str.length(); i++)
-        {
-            char ch=str.charAt(i);
-            if(ch>='A' && ch<='Z')
-            {
-                upper++;
-            }
-        }
-        System.out.println("uppercase letters: "+upper);
+
+public class countCapitalLetters {
+
+  public static void main(String args[]) {
+    String str;
+    int upper = 0;
+    Scanner scan = new Scanner(System.in);
+    System.out.print("Enter the String  : ");
+    str = scan.nextLine();
+    for (int i = 0; i < str.length(); i++) {
+      char ch = str.charAt(i);
+      if (ch >= 'A' && ch <= 'Z') {
+        upper++;
+      }
     }
+    System.out.println("uppercase letters : " + upper);
+
+    scan.close();
+  }
 }
