64653061d58bfdc9ea5c923a030046585daf36d5
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-07-12 16:33:37 +0530

    Update degree-to-radian.cs

--- programme/degree-to-radian/degree-to-radian.cs
+++ programme/degree-to-radian/degree-to-radian.cs
@@ -1,4 +1,5 @@
 // Write a programme to convert degree to radian
+using System;
 
 public class DegreeToRadian
 {
@@ -8,11 +9,10 @@ public class DegreeToRadian
 
         double degrees;
         while (!double.TryParse(Console.ReadLine(), out degrees))
-            Console.WriteLine("Invalid Input!!");
+        Console.WriteLine("Invalid Input!!");
 
         double radians = degrees * Math.PI / 180;
         Console
             .WriteLine($"\n{degrees} Degrees is {Math.Round(radians, 5)} Radians.");
-
     }
 }
