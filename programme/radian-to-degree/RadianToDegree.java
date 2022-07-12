8ce106629a0cd5925192176d9e0b0c41ceee512f
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-07-12 16:05:39 +0530

    Update RadianToDegree.java

--- programme/radian-to-degree/RadianToDegree.java
+++ programme/radian-to-degree/RadianToDegree.java
@@ -3,19 +3,19 @@ import java.util.Scanner;
 
 public class RadianToDegree {
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
 
-        // taking radians input
-        System.out.print("Enter the amount of Radians  : ");
-        int radian = sc.nextInt();
+    // taking radians input
+    System.out.print("Enter the amount of Radians  : ");
+    int radian = sc.nextInt();
 
-        // calculating radian to degree
-        double degree = radian * (180/(Math.PI));
+    // calculating radian to degree
+    double degree = radian * (180 / (Math.PI));
 
-        // printing the final answer
-        System.out.println("\nAmount in Degrees = " + degree);
+    // printing the final answer
+    System.out.printf("\nAmount in Degrees = %.5f\n", degree);
 
-        sc.close();
-    }
+    sc.close();
+  }
 }
