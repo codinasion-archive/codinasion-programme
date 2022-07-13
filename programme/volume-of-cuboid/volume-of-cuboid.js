2ac0742f4619a167d39a015364e80e019ce3c562
Anas Tazir <atazir2018@gmail.com>
2022-07-14 00:12:48 +0530

    add js code to calculate volume of a cuboid

---
+++ programme/volume-of-cuboid/volume-of-cuboid.js
@@ -0,0 +1,14 @@
+// Write a programme to find volume of a cuboid
+
+
+// Input: length, width, height
+const l = 2
+const w = 3
+const h = 4
+
+function volumeCuboid(l, w, h) {
+    return l*w*h;
+}
+
+console.log("Volume of Cuboid is : " + volumeCuboid(l, w, h));
+  
