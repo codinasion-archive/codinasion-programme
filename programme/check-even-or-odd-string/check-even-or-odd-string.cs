d47f6cb8e03f8bf6f414388b176e963010a6a47e
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-07-13 20:25:42 +0530

    Update check-even-or-odd-string.cs

--- programme/check-even-or-odd-string/check-even-or-odd-string.cs
+++ programme/check-even-or-odd-string/check-even-or-odd-string.cs
@@ -1,20 +1,24 @@
+// Write a programme to check even or odd string
 using System;
+
 namespace Codinasion.Programme
 {
     class EvenOrOddString
     {
         static void Main(string[] args)
         {
-            Console.Write("Input : ");
+            Console.Write("Input  : ");
             String input = Console.ReadLine();
+
+            Console.Write("Output : ");
             int length = input.Length;
             if (length % 2 == 0)
             {
-                Console.WriteLine("Even length");
+                Console.WriteLine("Even string");
             }
             else
             {
-                Console.WriteLine("Odd length");
+                Console.WriteLine("Odd string");
             }
         }
     }
