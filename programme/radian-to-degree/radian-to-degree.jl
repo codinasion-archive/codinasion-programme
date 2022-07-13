ba5d82e06b4dd9f70a8e9a9f7aafbaa38548b99b
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-07-13 19:04:16 +0530

    Update radian-to-degree.jl

--- programme/radian-to-degree/radian-to-degree.jl
+++ programme/radian-to-degree/radian-to-degree.jl
@@ -1,8 +1,7 @@
-#we can use the rad2deg() method. The rad2deg() is an inbuilt function in Julia that converts the specified radian values to degree values.
+# Write a program to convert Radian to Degree
+# The rad2deg() is an inbuilt function in Julia that converts 
+# the specified radian values to degree values.
 
-#For Getting the values in degree, we can use this code : println(rad2deg(value in rad))
-
-#if we input the value of radian as 1 and run this code given below 
-println(rad2deg(1))
-
-#we will get an output i.e 57.29577951308232
+radian = 1
+println("Input  : ", radian)
+println("Output : ", rad2deg(radian))
