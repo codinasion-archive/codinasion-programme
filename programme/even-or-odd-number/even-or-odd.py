fe39b5fa86b397c4f7df0cbb5e5f4e04697e9148
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-07-13 19:20:21 +0530

    Update even-or-odd.py

--- programme/even-or-odd-number/even-or-odd.py
+++ programme/even-or-odd-number/even-or-odd.py
@@ -1,3 +1,9 @@
+# Write a programme to check a number even or odd
+# n = 3 -> Odd Number, n=2 -> Even Number
+
 def even_or_odd(num):
-    return "Even" if num%2==0 else "Odd"
-    
+    return "Even" if num % 2 == 0 else "Odd"
+
+
+n = int(input("Input  : "))
+print("Output :", even_or_odd(n))
