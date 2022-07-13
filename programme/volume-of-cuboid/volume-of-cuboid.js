1ad25549d5ce5c69cb1e3fe9370b51f62ca1f264
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-07-14 00:40:20 +0530

    Update volume-of-cuboid.js

--- programme/volume-of-cuboid/volume-of-cuboid.js
+++ programme/volume-of-cuboid/volume-of-cuboid.js
@@ -1,14 +1,16 @@
 // Write a programme to find volume of a cuboid
 
-
 // Input: length, width, height
-const l = 2
-const w = 3
-const h = 4
+const l = 2;
+const w = 3;
+const h = 4;
+
+console.log("Length of Cuboid : " + l);
+console.log("Width  of Cuboid : " + w);
+console.log("Height of Cuboid : " + h);
 
 function volumeCuboid(l, w, h) {
-    return l*w*h;
+  return l * w * h;
 }
 
-console.log("Volume of Cuboid is : " + volumeCuboid(l, w, h));
-  
+console.log("\nVolume of Cuboid : " + volumeCuboid(l, w, h));
