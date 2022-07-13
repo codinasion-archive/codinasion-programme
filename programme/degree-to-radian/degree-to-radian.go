e11fb1cefd638f8bf7755a0ee155cca92f1595fe
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-07-14 00:16:56 +0530

    Update degree-to-radian.go

--- programme/degree-to-radian/degree-to-radian.go
+++ programme/degree-to-radian/degree-to-radian.go
@@ -1,13 +1,14 @@
+// Write a programme to convert degree to radian
 package main
 
 import (
 	"fmt"
-	"math/big"
+	"math"
 )
 
 func main() {
 	var degree float64
-	fmt.Println("Enter a degree: ")
+	fmt.Print("Enter a degree : ")
 	fmt.Scan(&degree)
-	fmt.Println("Radian of", degree, "is", degree*(math.Pi/180))
+	fmt.Println("Radian of", degree, "degree is", math.Round((degree*(math.Pi/180))*100000)/100000)
 }
