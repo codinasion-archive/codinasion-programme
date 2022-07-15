9f45c2e83084a1e6e2bd569d08334c8b86be8efd
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-07-15 08:47:13 +0530

    Update degree-to-radian.c

--- programme/degree-to-radian/degree-to-radian.c
+++ programme/degree-to-radian/degree-to-radian.c
@@ -1,9 +1,13 @@
 // Write a programme to convert degree to radian
 #include <stdio.h>
-int main() {
-    float degree;
-    printf("Degree: ");
-    scanf("%f", &degree);
-    printf("Radian: %f", 3.141592653589793*degree/180);
-    return 0;
+
+int main()
+{
+  float degree;
+  printf("Degree : ");
+  scanf("%f", &degree);
+
+  printf("Radian : %f\n", 3.141592653589793 * degree / 180);
+
+  return 0;
 }
