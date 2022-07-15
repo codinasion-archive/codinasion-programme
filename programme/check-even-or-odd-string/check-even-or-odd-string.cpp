e17e9b4c10b1f898316718663a7a83916cd16d9a
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-07-15 08:42:44 +0530

    Update check-even-or-odd-string.cpp

--- programme/check-even-or-odd-string/check-even-or-odd-string.cpp
+++ programme/check-even-or-odd-string/check-even-or-odd-string.cpp
@@ -1,11 +1,19 @@
 // Write a programme to check even or odd string
 #include <iostream>
+
 using namespace std;
-int main() {
-	string ask;
-	cout << "String: ";
-	cin >> ask; 
-	if (ask.length() & 1) cout << "Odd String " << "\n";
-	else cout << "Even String" << "\n";
-	return 0;
+
+int main()
+{
+    string ask;
+    cout << "Input  : ";
+    cin >> ask;
+    cout << "Output : ";
+    if (ask.length() & 1)
+        cout << "Odd String "
+             << "\n";
+    else
+        cout << "Even String"
+             << "\n";
+    return 0;
 }
