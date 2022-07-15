ac110aad5d8f13c5a5850de79a7cb8301898f753
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-07-15 15:35:45 +0530

    Update count-capital-letters.php

--- programme/count-capital-letters/count-capital-letters.php
+++ programme/count-capital-letters/count-capital-letters.php
@@ -1,11 +1,12 @@
 <?php
 // Write a programme to count capital letters in a string
 
-function countCapitalLetters(string $text) {
+function countCapitalLetters(string $text)
+{
     $count = 0;
 
     foreach (str_split($text) as $char) {
-        if(ctype_upper($char)) {
+        if (ctype_upper($char)) {
             $count++;
         }
     }
@@ -17,7 +18,5 @@ $str = "HiiThere";
 
 $capital_letters = countCapitalLetters("HiiThere");
 
-print "\nInput : {$str}";
+print "\nInput  : {$str}";
 print "\nOutput : {$capital_letters}\n";
-
-?>
