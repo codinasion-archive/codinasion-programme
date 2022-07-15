cb973e148c24664f88967771f03a31afa03a1418
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-07-15 09:25:46 +0530

    Update lowercase-to-uppercase.cpp

--- programme/lowercase-to-uppercase/lowercase-to-uppercase.cpp
+++ programme/lowercase-to-uppercase/lowercase-to-uppercase.cpp
@@ -1,15 +1,22 @@
 // WAP to convert lowercase to uppercase
 #include <iostream>
+
 using namespace std;
-int main() {
-	string ask; 
-	cout << "String: ";
-	getline(cin, ask);
-	int store; 
-	for (auto i : ask) {
-		store = i;
-		if (store>=97 && store<=122) store-=32;
-		cout << (char)store;
-	}
-	return 0;
+
+int main()
+{
+    string ask;
+    cout << "Input  : ";
+    getline(cin, ask);
+    int store;
+    cout << "Output : ";
+    for (auto i : ask)
+    {
+        store = i;
+        if (store >= 97 && store <= 122)
+            store -= 32;
+        cout << (char)store;
+    }
+    cout << endl;
+    return 0;
 }
