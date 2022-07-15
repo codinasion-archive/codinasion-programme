c170f8d393bffbfb299cbfb85a5b198d1a11ff9c
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-07-15 15:24:42 +0530

    Update square-root-of-number.cs

--- programme/square-root-of-number/square-root-of-number.cs
+++ programme/square-root-of-number/square-root-of-number.cs
@@ -1,14 +1,16 @@
+// Write a programme to find square root of a number
 using System;
+
 namespace Codinasion.Programme
 {
     public class SquareRootOfNumber
     {
         public static void Main()
         {
-            Console.Write("Input : ");
+            Console.Write("Input  : ");
             String input = Console.ReadLine();
             int x = Int32.Parse(input);
-            Console.WriteLine(Math.Sqrt(x));
+            Console.WriteLine("Output : " + Math.Sqrt(x));
         }
     }
 }
