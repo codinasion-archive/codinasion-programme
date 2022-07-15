511dfc5f101d54dce791b77206de3f837faad08d
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-07-15 09:27:37 +0530

    Update uppercase-to-lowercase.cpp

--- programme/uppercase-to-lowercase/uppercase-to-lowercase.cpp
+++ programme/uppercase-to-lowercase/uppercase-to-lowercase.cpp
@@ -1,15 +1,22 @@
-// WAP to convert uppercase string to lowercase
+// Write a programme to convert uppercase string to lowercase
 #include <iostream>
+
 using namespace std;
-int main() {
-	string ask; 
-	cout << "String: ";
-	getline(cin, ask);
-	int store; 
-	for (auto i : ask) {
-		store = i;
-		if (store>=65 && store<=90) store+=32;
-		cout << (char)store;
-	}
-	return 0;
+
+int main()
+{
+    string ask;
+    cout << "Input  : ";
+    getline(cin, ask);
+    int store;
+    cout << "Output : ";
+    for (auto i : ask)
+    {
+        store = i;
+        if (store >= 65 && store <= 90)
+            store += 32;
+        cout << (char)store;
+    }
+    cout << endl;
+    return 0;
 }
