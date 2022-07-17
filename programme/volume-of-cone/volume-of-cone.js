799e78a41980475cedb756bb1c044b54ca43e01c
Anas Tazir <atazir2018@gmail.com>
2022-07-17 21:33:08 +0530

    add js code to compute the volume of a cone

---
+++ programme/volume-of-cone/volume-of-cone.js
@@ -0,0 +1,12 @@
+// Write a program to compute the volume of a Cone
+
+const radius = 2;
+const height = 3;
+
+function volumeOfCone(radius, height) {
+    const area = Math.PI * radius * radius * height * (1/3);
+    return area.toFixed(5);
+}
+
+console.log("Radius : ", radius);
+console.log("Volume : ", volumeOfCone(radius, height));
