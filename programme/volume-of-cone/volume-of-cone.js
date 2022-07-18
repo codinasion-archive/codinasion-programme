a67a846e3d7ca11f11cf73d28f6df680027542ba
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-07-18 10:03:31 +0530

    Update volume-of-cone.js

--- programme/volume-of-cone/volume-of-cone.js
+++ programme/volume-of-cone/volume-of-cone.js
@@ -4,8 +4,8 @@ const radius = 2;
 const height = 3;
 
 function volumeOfCone(radius, height) {
-    const area = Math.PI * radius * radius * height * (1/3);
-    return area.toFixed(5);
+  const area = Math.PI * radius * radius * height * (1 / 3);
+  return area.toFixed(5);
 }
 
 console.log("Radius : ", radius);
