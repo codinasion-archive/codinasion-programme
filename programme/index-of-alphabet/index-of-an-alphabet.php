125b82ff5a7b608985ef2d2b5d136e468a6bbddc
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-07-19 17:59:30 +0530

    Update index-of-an-alphabet.php

--- programme/index-of-alphabet/index-of-an-alphabet.php
+++ programme/index-of-alphabet/index-of-an-alphabet.php
@@ -1,12 +1,11 @@
 <?php
+// Write a programme to find index of an alphabet
 
-$char = readline("Input: ");
+$char = readline("Input  : ");
 
 if (ctype_upper($char)) {
-   echo "Output: ".(ord($char) - 64);
+    echo "Output : " . (ord($char) - 64);
 } else {
-   echo "Output: ".(ord($char) - 96);
+    echo "Output : " . (ord($char) - 96);
 }
-
-?>
-
+echo "\n";
