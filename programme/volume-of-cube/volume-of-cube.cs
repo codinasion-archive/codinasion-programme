656b3da288b470601d185a62d1f8b4193f057a94
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-07-20 22:35:48 +0530

    Update volume-of-cube.cs

--- programme/volume-of-cube/volume-of-cube.cs
+++ programme/volume-of-cube/volume-of-cube.cs
@@ -1,15 +1,16 @@
+// Write a programme to find volume of a cube
 using System;
+
 namespace Codinasion.Programme
 {
     public class VolumeOfCube
     {
         public static void Main()
         {
-            Console.Write("Enter Side: ");
+            Console.Write("Enter Side : ");
             decimal Side = Convert.ToDecimal(Console.ReadLine());
             decimal Volume = Side * Side * Side;
-            Console.WriteLine("Volume of cube=" + Volume);
-            Console.ReadKey();
+            Console.WriteLine("\nVolume of cube : " + Volume);
         }
     }
 }
