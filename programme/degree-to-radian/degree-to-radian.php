2dfbc21332009b791cee7b313171e3d53f0ab850
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-07-20 22:39:14 +0530

    Update degree-to-radian.php

--- programme/degree-to-radian/degree-to-radian.php
+++ programme/degree-to-radian/degree-to-radian.php
@@ -1,8 +1,6 @@
 <?php
+// Write a programme to convert degree to radian
 
-$deg = readline("Input: ");
+$deg = readline("Input  : ");
 
-echo "Output: ".round(($deg * pi()/180), 5);
-
-?>
-
+echo "Output : " . round(($deg * pi() / 180), 5) . "\n";
