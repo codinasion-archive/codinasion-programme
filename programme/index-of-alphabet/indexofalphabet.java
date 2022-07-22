654fdafd7352a7cac1692d15d41022e8be8cc06e
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-07-23 00:14:12 +0530

    Update indexofalphabet.java

--- programme/index-of-alphabet/indexofalphabet.java
+++ programme/index-of-alphabet/indexofalphabet.java
@@ -1,15 +1,18 @@
+// Write a programme to find index of an alphabet
 import java.util.*;
 
-class indexofalphabet{
-    public static void main(String args[]) {
-        System.out.print("Input: ");
-        Scanner sc = new Scanner(System.in);
-        char c = sc.next().charAt(0);
-        if(Character.isLowerCase(c)){
-            System.out.println("Output: " + (c-96));
-        }
-        else{
-            System.out.println("Output: " + (c-64));
-        }
+class IndexOfAlphabet {
+
+  public static void main(String args[]) {
+    System.out.print("Input  : ");
+    Scanner sc = new Scanner(System.in);
+    char c = sc.next().charAt(0);
+    if (Character.isLowerCase(c)) {
+      System.out.println("Output : " + (c - 96));
+    } else {
+      System.out.println("Output : " + (c - 64));
     }
+
+    sc.close();
+  }
 }
