e8589190e44f85bcc8683fedfc46ca71b73c1015
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-07-22 18:49:23 +0530

    Update random-even-number.js

--- programme/random-even-number/random-even-number.js
+++ programme/random-even-number/random-even-number.js
@@ -1,10 +1,10 @@
 // Write a programme to generate a random even number in range n1-n2
 
-function generateRandomEvenNumber(n1, n2){
-  while (true){
+function generateRandomEvenNumber(n1, n2) {
+  while (true) {
     let randomNumber = Math.floor(Math.random() * (n2 - n1 + 1)) + n1;
-    if (randomNumber % 2 == 0){
-        return randomNumber;
+    if (randomNumber % 2 == 0) {
+      return randomNumber;
     }
   }
 }
@@ -15,4 +15,7 @@ const range_end = 100;
 console.log("Start range :", range_start);
 console.log("End   range :", range_end);
 
-console.log("Random even number :", generateRandomEvenNumber(range_start, range_end));
+console.log(
+  "\nRandom even number :",
+  generateRandomEvenNumber(range_start, range_end)
+);
