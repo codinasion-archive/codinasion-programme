3f3f46dcf3ead571b56b43f980ebf7a92ca4f8af
Anas Tazir <atazir2018@gmail.com>
2022-07-23 20:24:36 +0530

    add js code to generate a random number in range n1, n2

---
+++ programme/random-number/random-number.js
@@ -0,0 +1,12 @@
+// Write a programme to generate a random number in range n1, n2
+
+const n1 = 1;
+const n2 = 100;
+
+function getRandomNumberBetween(n1,n2){
+    return Math.floor(Math.random()*(n2-n1+1)+n1);
+}
+
+console.log("Start range   :", n1);
+console.log("End   range   :", n2);
+console.log("Random number :", getRandomNumberBetween(n1,n2));
