336e201c8e22c4b0c659e4b6f41eb77e0a004032
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-07-23 07:57:46 +0530

    Update random-number.c

--- programme/random-number/random-number.c
+++ programme/random-number/random-number.c
@@ -1,33 +1,26 @@
-/*
+// Write a programme to generate a random number in range n1-n2
 
+/*
 Programm take as given that inputs are always lower<upper
 Normally we would need a input control to confirm that the uper argument is always bigger
-
 */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
 int main()
 {
-    int lower, upper, num;
+  int lower, upper, num;
 
-    srand(time(0));
-    printf ("Enter lower limit: ") ;
-    scanf ("%d", &lower);
-    printf ("Enter upper limit: ") ;
-    scanf ("%d", &upper);
+  srand(time(0));
+  printf("Enter lower limit : ");
+  scanf("%d", &lower);
+  printf("Enter upper limit : ");
+  scanf("%d", &upper);
 
-    
-    int check = 2 % 2;
+  num = (rand() % (upper - lower + 1)) + lower;
+  printf("\nThe random number is : %d\n", num);
 
-    printf("The random numbers are: ");
-    
-    while (check == 0){
-        num = (rand() % (upper - lower + 1)) + lower;
-        check = num % 2;
-    }
-    printf("%d ", num);
-
-    return 0;
+  return 0;
 }
