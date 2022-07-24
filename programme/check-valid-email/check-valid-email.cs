1938e1f8330ba302da1ad003cf09f0f4414267df
fadyfayek11 <fayekfady24@gmail.com>
2022-07-24 21:24:33 +0200

    issue #1262

---
+++ programme/check-valid-email/check-valid-email.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class EmailValidator
+{
+    private static bool IsValid(string email)
+    {
+        string regex = @"^[^@\s]+@[^@\s]+\.(com|net|org|gov)$";
+
+        return Regex.IsMatch(email, regex, RegexOptions.IgnoreCase);
+    }
+}
+
