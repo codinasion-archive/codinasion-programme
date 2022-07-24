1b4d4342009aae089a26cef49a634753162c0491
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-07-24 22:11:33 +0530

    Update random-number.js

--- programme/random-number/random-number.js
+++ programme/random-number/random-number.js
@@ -3,10 +3,10 @@
 const n1 = 1;
 const n2 = 100;
 
-function getRandomNumberBetween(n1,n2){
-    return Math.floor(Math.random()*(n2-n1+1)+n1);
+function getRandomNumberBetween(n1, n2) {
+  return Math.floor(Math.random() * (n2 - n1 + 1) + n1);
 }
 
 console.log("Start range   :", n1);
 console.log("End   range   :", n2);
-console.log("Random number :", getRandomNumberBetween(n1,n2));
+console.log("Random number :", getRandomNumberBetween(n1, n2));
