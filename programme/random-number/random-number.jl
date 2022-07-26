c6eb362685a6eb6c81016e2d1d5cf849b58ac256
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-07-26 16:52:27 +0530

    Update random-number.jl

--- programme/random-number/random-number.jl
+++ programme/random-number/random-number.jl
@@ -1,11 +1,12 @@
+# Write a programme to generate a random number in range n1-n2
 function output_in_range()
-  println("Enter start range")
+  print("Enter start range : ")
   start_range = readline()
-  
-  println("Enter end range")
+
+  print("Enter end   range : ")
   end_range = readline()
-  
-  println(rand((parse(Int64, start_range)):parse(Int64, end_range))) #We use parsing in order to turn a string input into an integer because rand works only with ints
+
+  println("\nOutput : ", rand((parse(Int64, start_range)):parse(Int64, end_range))) #We use parsing in order to turn a string input into an integer because rand works only with ints
 end
 
 output_in_range()
