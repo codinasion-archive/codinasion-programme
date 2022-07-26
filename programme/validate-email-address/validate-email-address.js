9e659e59d703093d6f5c4fdb5b4d5909b57ee5a2
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-07-26 08:51:32 +0530

    Update validate-email-address.js

--- programme/validate-email-address/validate-email-address.js
+++ programme/validate-email-address/validate-email-address.js
@@ -1,14 +1,14 @@
 // Write a programme to check a valid email address
 
-function isValidEmail(email){
+function isValidEmail(email) {
   const re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-  if(email.match(re)) {
-    return true; 
+  if (email.match(re)) {
+    return true;
   }
   return false;
 }
- 
+
 const email = "test@gmail.com";
 
-console.log("Input  : ", email);
-console.log("Output : ", isValidEmail(email));
+console.log("Input  :", email);
+console.log("Output :", isValidEmail(email) ? "valid" : "invalid");
