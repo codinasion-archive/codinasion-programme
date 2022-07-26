02276134bc9b574162adf1a4ab515ac202d4e136
Harsh Raj <harshraj8843pro@gmail.com>
2022-07-26 09:17:19 +0530

    update #1271
    Co-authored-by: Anas Tazir <74285556+anastazir@users.noreply.github.com>
    
    Co-authored-by: fadyfayek11 <fayekfady24@gmail.com>

--- programme/check-valid-email/check-valid-email.cs
+++ programme/check-valid-email/check-valid-email.cs
@@ -1,12 +1,22 @@
-﻿using System;
+﻿// Write a programme to check a valid email address
+using System;
+using System.Text.RegularExpressions;
 
 public class EmailValidator
 {
-    private static bool IsValid(string email)
+    public static bool IsValid(string email)
     {
-        string regex = @"^[^@\s]+@[^@\s]+\.(com|net|org|gov)$";
+        string regex =
+            @"^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([azA-Z]{2,4}|[0-9]{1,3})(\]?)$";
 
         return Regex.IsMatch(email, regex, RegexOptions.IgnoreCase);
     }
-}
 
+    public static void Main()
+    {
+        string email = "abc@mail.com";
+        Console.WriteLine("Input  : {0}", email);
+
+        Console.WriteLine("Output : {0}", IsValid(email) ? "Valid" : "Invalid");
+    }
+}
