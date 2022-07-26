f0dc97680cce3e6fd09043a68826569e920430d4
Lorenzo Rossi <rossilor95@outlook.it>
2022-07-26 15:48:28 +0200

    Add CheckValidEmailAddress.java

---
+++ programme/check-valid-email-address/CheckValidEmailAddress.java
@@ -0,0 +1,29 @@
+// Write a programme to check a valid email address
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+class CheckValidEmailAddress {
+
+    public static boolean isValidEmail(String emailAddress) {
+        Pattern pattern = Pattern.compile("^[A-Z\\d._%+-]+@[A-Z\\d.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(emailAddress);
+        return matcher.find();
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Please, insert the email address to validate: ");
+        String emailAddress = scanner.nextLine();
+
+        if (isValidEmail(emailAddress)) {
+            System.out.println("The email address provided is valid.");
+        } else {
+            System.out.println("The email address provided is invalid.");
+        }
+
+    }
+
+}
