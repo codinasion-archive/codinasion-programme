916cfb16ec6e380680c3e4c815829d03fe916691
Spiros Arkoudelis <sp.arkoudelis@gmail.com>
2022-07-27 22:06:46 +0300

    Create lowercase-to-uppercase.c

---
+++ programme/lowercase-to-uppercase/lowercase-to-uppercase.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+int main() {
+   char input[100];
+   int i;
+   printf("\nEnter a string : ");
+   gets(input);
+   for (i = 0; input[i]!='\0'; i++) {
+      if(input[i] >= 'a' && input[i] <= 'z') {
+         input[i] = input[i] -32;
+      }
+   }
+   printf("\nString in Upper Case = %s", input);
+   return 0;
+}
