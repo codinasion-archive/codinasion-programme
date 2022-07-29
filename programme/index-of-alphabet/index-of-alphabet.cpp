23fa5b35abd5d927ac2c66fc1c27fe1c9e14373d
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-07-29 18:30:35 +0530

    Update index-of-alphabet.cpp

--- programme/index-of-alphabet/index-of-alphabet.cpp
+++ programme/index-of-alphabet/index-of-alphabet.cpp
@@ -1,19 +1,24 @@
+// Write a programme to find index of an alphabet
 #include <iostream>
 
 using namespace std;
 
 int findIndex(char input);
 
-int findIndex(char input){
-    int ascii_value =(int)input;
-    if (ascii_value > 95) return (ascii_value - 96);
-    else return (ascii_value-64);
-
-
+int findIndex(char input)
+{
+    int ascii_value = (int)input;
+    if (ascii_value > 95)
+        return (ascii_value - 96);
+    else
+        return (ascii_value - 64);
 }
 
-int main() {
+int main()
+{
+    cout << "Input  : ";
     char input;
     cin >> input;
-    cout << findIndex(input);
+
+    cout << "Output : " << findIndex(input) << endl;
 }
