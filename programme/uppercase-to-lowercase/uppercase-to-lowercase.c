da74704bb626ae538118c717465267a349bd3ce7
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-07-29 07:09:55 +0530

    Update uppercase-to-lowercase.c

--- programme/uppercase-to-lowercase/uppercase-to-lowercase.c
+++ programme/uppercase-to-lowercase/uppercase-to-lowercase.c
@@ -1,17 +1,20 @@
-#uppercase to lowercase String in C
-
+// Write a programme to convert uppercase string to lowercase
 #include <stdio.h>
 #include <string.h>
-int main() {
-   char input[100];
-   int i;
-   printf("\nEnter a string : ");
-   gets(input);
-   for (i = 0; input[i]!='\0'; i++) {
-      if(input[i] >= 'A' && input[i] <= 'Z') {
-         input[i] = input[i] +32;
-      }
-   }
-   printf("\nString in Upper Case = %s", input);
-   return 0;
+
+int main()
+{
+  char input[100];
+  int i;
+  printf("Enter a string : ");
+  gets(input);
+  for (i = 0; input[i] != '\0'; i++)
+  {
+    if (input[i] >= 'A' && input[i] <= 'Z')
+    {
+      input[i] = input[i] + 32;
+    }
+  }
+  printf("\nString in Upper Case : %s\n", input);
+  return 0;
 }
