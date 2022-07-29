e740009e6036fbd872c72f3e2daefcd1e47c2470
Spiros Arkoudelis <sp.arkoudelis@gmail.com>
2022-07-29 22:28:34 +0300

    Create index-of-alphabet.c

---
+++ programme/index-of-alphabet/index-of-alphabet.c
@@ -0,0 +1,24 @@
+//Program that accepts a char as an input and returns it's position in alphabet
+
+#include <stdio.h>
+
+int main()
+{   
+    int position;
+    char input;
+    printf("Enter char that you want to learn his alphabet position:\n");
+    scanf("%c", &input);
+    if (input >= 97){
+        
+        position = input - 'a' + 1;
+        
+    }
+    else{
+    
+        position = input - 'A' + 1;
+    
+    }
+    
+    printf("%d", position);
+    
+}
