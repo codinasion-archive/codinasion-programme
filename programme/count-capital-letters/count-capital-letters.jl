1cd70051a15f87bed65d17e479d51843ac753734
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-07-29 07:28:07 +0530

    Update count-capital-letters.jl

--- programme/count-capital-letters/count-capital-letters.jl
+++ programme/count-capital-letters/count-capital-letters.jl
@@ -1,15 +1,15 @@
-## Write a programme to count how many capital letters there are in a String
+# Write a programme to count capital letters in a string
 
 function capitals()
   counter = 0
-  input = readlines()
+  print("Input  : ")
+  input = readline()
   for character in input
     if all(isuppercase, character)
-      
       counter = counter + 1
     end
   end
-  print(counter)
+  println("Output : ", counter)
 end
 
 capitals()
