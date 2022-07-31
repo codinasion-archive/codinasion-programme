5702b4cc1a479a32660ad6b00b4e4ef8ec5556a1
Spiros Arkoudelis <sp.arkoudelis@gmail.com>
2022-07-31 21:22:09 +0300

    Create random-number.java

---
+++ programme/random-number/random-number.java
@@ -0,0 +1,20 @@
+//Programme to generate random number in range using Java
+
+import java.util.Random;
+import java.util.*;
+
+class HelloWorld {
+    public static void main(String[] args) {
+        Scanner s_name = new Scanner(System.in);
+        
+        System.out.println("Enter start range:\n");
+        int startRange = s_name.nextInt();
+        System.out.println("Enter end range:");
+        int endRange = s_name.nextInt();
+        
+        System.out.println("Random value in int from "+startRange+" to "+endRange+ ":\n");
+        int random_int = (int)Math.floor(Math.random()*(endRange-startRange+1)+startRange);
+     
+        System.out.println(random_int);
+    }
+}
