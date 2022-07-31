ffb74067d8fef347e3766fb1e2c614f9dc3f88a1
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-07-31 13:30:44 +0530

    Update square-root-of-number.jl

--- programme/square-root-of-number/square-root-of-number.jl
+++ programme/square-root-of-number/square-root-of-number.jl
@@ -1,7 +1,10 @@
-#Programme to calculate the square root of a given input
+# Write a programme to find square root of a number
 
 function root(input)
   println(sqrt(input))
 end
 
+input = 25
+println("Input  : ", input)
+print("Output : ")
 root(25)
