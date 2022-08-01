60f40723cc6f4d18073833ca743f8c56cf0c3822
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-08-01 07:44:19 +0530

    Update random-even-number.java

--- programme/random-even-number/random-even-number.java
+++ programme/random-even-number/random-even-number.java
@@ -1,22 +1,30 @@
-//Programme to generate random even number in range using Java
-
-import java.util.Random;
+// Write a programme to generate a random even number in range n1-n2
 import java.util.*;
 
-class HelloWorld {
-    public static void main(String[] args) {
-        Scanner s_name = new Scanner(System.in);
-        
-        System.out.println("Enter start range:\n");
-        int startRange = s_name.nextInt();
-        System.out.println("Enter end range:");
-        int endRange = s_name.nextInt();
-        
-        System.out.println("Random value in int from " + startRange + " to " + endRange + ":\n");
-        int random_int = (int) Math.floor(Math.random() * (endRange - startRange + 1) + startRange);
-        while (random_int % 2 != 0){
-            random_int = (int)Math.floor(Math.random()*(endRange-startRange+1)+startRange);
-        }
-        System.out.println(random_int);
+class RandomEvenNumber {
+
+  public static void main(String[] args) {
+    Scanner s_name = new Scanner(System.in);
+
+    System.out.print("Enter start range : ");
+    int startRange = s_name.nextInt();
+    System.out.print("Enter end   range : ");
+    int endRange = s_name.nextInt();
+
+    System.out.print(
+      "\nRandom value in int from " + startRange + " to " + endRange + " : "
+    );
+    int random_int = (int) Math.floor(
+      Math.random() * (endRange - startRange + 1) + startRange
+    );
+    while (random_int % 2 != 0) {
+      random_int =
+        (int) Math.floor(
+          Math.random() * (endRange - startRange + 1) + startRange
+        );
     }
+    System.out.println(random_int);
+
+    s_name.close();
+  }
 }
