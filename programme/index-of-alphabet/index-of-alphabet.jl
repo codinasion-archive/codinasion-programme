553cae0444fdd108ce2113e4eeac930166fd315b
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-08-06 17:28:12 +0530

    Update index-of-alphabet.jl

--- programme/index-of-alphabet/index-of-alphabet.jl
+++ programme/index-of-alphabet/index-of-alphabet.jl
@@ -1,19 +1,19 @@
-#Programme to calculate index of an alphabet
+# Write a programme to find index of an alphabet
 
 function chars(char)
   char = Int(char)
   
   if char >= 97
-    
     small = Int('a')
     println(char - small + 1)
-
   else
-    
     capital = Int('A')
     println(char - capital + 1)
   end
   
 end
 
+input = 'C'
+println("Input  : ", input)
+print("Output : ")
 chars('C')
