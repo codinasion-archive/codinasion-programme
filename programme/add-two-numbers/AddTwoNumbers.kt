eb1de6f2bb5fc8f2a5a75e4871302a8ab171ec43
Spiros Arkoudelis <sp.arkoudelis@gmail.com>
2022-08-07 20:44:08 +0300

    Update AddTwoNumbers.kt
    was the only way i managed to run this on https://www.tutorialspoint.com/compile_kotlin_online.php

--- programme/add-two-numbers/AddTwoNumbers.kt
+++ programme/add-two-numbers/AddTwoNumbers.kt
@@ -2,17 +2,12 @@
 
 import java.util.Scanner
 
-class AddTwoNumbers {
-
-    fun main() {
-        val sc = Scanner(System.`in`)
-
-        print("Enter two numbers to add : ")
-        val a = sc.nextInt()
-        val b = sc.nextInt()
-
-        val sum = a + b
-        println("\nThe sum of numbers is : $sum")
-        sc.close()
-    }
+fun main() {
+    val sc = Scanner(System.`in`)
+    print("Enter two numbers to add : ")
+    val a = sc.nextInt()
+    val b = sc.nextInt()
+    val sum = a + b
+    println("\nThe sum of numbers is : $sum")
+    sc.close()
 }
