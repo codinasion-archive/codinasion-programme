6cad826c1d0a7e7baabef89646d110a942e982e7
KikiManjaro <kylian.meulin@gmail.com>
2022-08-07 17:34:02 +0200

    Add AlphabetTrianglePattern for Kotlin

---
+++ programme/alphabet-triangle-pattern/AlphabetTrianglePattern.kt
@@ -0,0 +1,33 @@
+// Write a programme to print alphabet triangle pattern
+
+import java.util.Scanner;
+
+object AlphaTrianglePattern {
+    private fun pattern(height: Int) {
+        print("Output :\n")
+        for (i in 1..height) {
+            var c = 65
+            for (k in height downTo i + 1) {
+                print(" ")
+            }
+            for (j in 1 until 2 * i) {
+                if (j < i) {
+                    print(c.toChar())
+                    c++
+                } else {
+                    print(c.toChar())
+                    c--
+                }
+            }
+            print("\n")
+        }
+    }
+
+    fun main() {
+        val sc = Scanner(System.`in`)
+        print("Input  : ")
+        val height = sc.nextInt()
+        pattern(height)
+        sc.close()
+    }
+}
