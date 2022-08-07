a20b6652796239a72801b77be2a0bdcdea305e7a
Spiros Arkoudelis <sp.arkoudelis@gmail.com>
2022-08-08 02:04:36 +0300

    Removed Class declaration
    Didn't worked otherwise. Compiler used: https://www.tutorialspoint.com/compile_kotlin_online.php

--- programme/area-of-rhombus/AreaOfRhombus.kt
+++ programme/area-of-rhombus/AreaOfRhombus.kt
@@ -2,19 +2,17 @@
 
 import java.util.Scanner
 
-class AreaOfRhombus {
-    fun main() {
-        val sc = Scanner(System.`in`)
+fun main() {
+    val sc = Scanner(System.`in`)
 
-        print("Diagonal 1 : ")
-        val diag1 = sc.nextInt().toFloat()
+    print("Diagonal 1 : ")
+    val diag1 = sc.nextInt().toFloat()
 
-        print("Diagonal 2 : ")
-        val diag2 = sc.nextInt().toFloat()
+    print("Diagonal 2 : ")
+    val diag2 = sc.nextInt().toFloat()
 
-        val area = diag1 * diag2 / 2
-        println("Area : $area")
+    val area = diag1 * diag2 / 2
+    println("Area : $area")
 
-        sc.close()
+    sc.close()
     }
-}
