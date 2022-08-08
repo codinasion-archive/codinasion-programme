cc93521cea9df6156d205a6a1025bb68e3dfdb22
Spiros Arkoudelis <sp.arkoudelis@gmail.com>
2022-08-08 12:56:09 +0300

    Update programme/random-odd-number/random-odd-number.go
    Co-authored-by: Kylian Meulin <59285425+KikiManjaro@users.noreply.github.com>

--- programme/random-odd-number/random-odd-number.go
+++ programme/random-odd-number/random-odd-number.go
@@ -7,16 +7,10 @@ import (
 )
 
 func main() {
-
-
 	rand.Seed(time.Now().UnixNano())
 	var x1 = rand.Intn(100)
-	for x1 % 2 == 0{
-
+	for x1 % 2 == 0 {
 	    x1 = rand.Intn(100)
-
 	}
-
 	fmt.Println(x1)
-
 }
