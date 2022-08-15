b2ac2ddfe74e9f0eabcdf1b2d0f5c8f1df77c63e
Spiros Arkoudelis <sp.arkoudelis@gmail.com>
2022-08-15 20:45:04 +0300

    Created PHP file for sum of numbers

---
+++ programme/sum-of-numbers/sum-of-numbers.php
@@ -0,0 +1,8 @@
+<?php  
+$input1 = (int)readline('Enter first integer: ');  
+$input2 = (int)readline('Enter second integer: ');
+$input3 = (int)readline('Enter third integer: ');
+$input4 = (int)readline('Enter fourth integer: ');
+$sum = $input1 + $input2 + $input3 +$input4;  
+echo "Sum of Inputs : ". $sum;
+?>
