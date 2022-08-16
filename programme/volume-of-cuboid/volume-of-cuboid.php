197198991e6929012e4f062d911c5d2c6ab2354d
Spiros Arkoudelis <sp.arkoudelis@gmail.com>
2022-08-16 06:22:46 +0300

    Create volume-of-cuboid.php

---
+++ programme/volume-of-cuboid/volume-of-cuboid.php
@@ -0,0 +1,10 @@
+// Write a programme to calculate volume of cuboid
+
+<?php  
+$length = (int)readline('Enter length: '); 
+$width = (int)readline('Enter width: ');  
+$height = (int)readline('Enter height: ');  
+
+$volume = $length*$width*$height;  
+echo "Volume of Cuboid : ". $volume;
+?>
